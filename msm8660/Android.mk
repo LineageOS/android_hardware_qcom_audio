903543538c636526da01a7fdc06ba2b81fe90f19
Steve Kondik <shade@chemlab.org>
2012-06-19 08:56:35 -0700

    msm8660: Add support for camcorder mic
     * Some devices have a mic dedicated to the camcorder. Enable it.
    
    Change-Id: I75fb7b2e36c37505a603f2aaed7362199535055a

--- msm8660/Android.mk
+++ msm8660/Android.mk
@@ -54,6 +54,14 @@ LOCAL_C_INCLUDES += hardware/libhardware_legacy/include
 LOCAL_C_INCLUDES += frameworks/base/include
 LOCAL_C_INCLUDES += system/core/include
 
+ifeq ($(BOARD_HAVE_SAMSUNG_AUDIO),true)
+LOCAL_CFLAGS += -DSAMSUNG_AUDIO
+endif
+
+ifeq ($(BOARD_HAVE_BACK_MIC_CAMCORDER),true)
+LOCAL_CFLAGS += -DBACK_MIC_CAMCORDER
+endif
+
 include $(BUILD_SHARED_LIBRARY)
 
 ifeq ("x","y") # use default audio policy manager
@@ -84,6 +92,14 @@ ifeq ($(BOARD_HAVE_BLUETOOTH),true)
 endif
 
 LOCAL_C_INCLUDES += hardware/libhardware_legacy/audio
+
+ifeq ($(BOARD_HAVE_SAMSUNG_AUDIO),true)
+LOCAL_CFLAGS += -DSAMSUNG_AUDIO
+endif
+
+ifeq ($(BOARD_HAVE_BACK_MIC_CAMCORDER),true)
+LOCAL_CFLAGS += -DBACK_MIC_CAMCORDER
+endif
 
 include $(BUILD_SHARED_LIBRARY)
 endif
