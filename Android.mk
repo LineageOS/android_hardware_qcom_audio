c54653231e38f944af55d94f89afd94a07fb5115
Y Mehta <ymehta@codeaurora.org>
2012-07-09 16:11:56 +0530

    audio: Conditional compilation for 8660, 8960 and 7627a Audio HALs
    Add conditional compilation for 8660, 8960 and 7627a Audio
    HALs
    
    Change-Id: Idb2d1c88c22eb1a50c74b95b507ca40dccda9220

--- Android.mk
+++ Android.mk
@@ -1,2 +1,15 @@
-AUDIO_ROOT := $(call my-dir)
-include $(call all-subdir-makefiles)
+AUDIO_HW_ROOT := $(call my-dir)
+
+ifeq ($(strip $(BOARD_USES_ALSA_AUDIO)),true)
+    include $(AUDIO_HW_ROOT)/alsa_sound/Android.mk
+endif
+
+ifeq ($(strip $(BOARD_USES_ALSA_AUDIO)),true)
+    include $(AUDIO_HW_ROOT)/libalsa-intf/Android.mk
+endif
+ifeq ($(call is-board-platform,msm7627a),true)
+    include $(AUDIO_HW_ROOT)/msm7627a/Android.mk
+endif
+ifeq ($(call is-board-platform,msm8660),true)
+    include $(AUDIO_HW_ROOT)/msm8660/Android.mk
+endif
