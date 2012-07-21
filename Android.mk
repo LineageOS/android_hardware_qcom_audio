799e0c2919d2f974b48c14696f806a2d3874fc79
Shareef Ali <shareefalis@gmail.com>
2012-07-20 21:42:02 -0500

    qcom-audio: fix compile on msm8960 platform
    Change-Id: Ife3fcd892d3588f5e45089a2b1a4d6370eb950e9

--- Android.mk
+++ Android.mk
@@ -3,9 +3,11 @@ ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
 
 AUDIO_HW_ROOT := $(call my-dir)
 
+ifneq ($(TARGET_BOARD_PLATFORM),msm8960)
 ifeq ($(strip $(BOARD_USES_ALSA_AUDIO)),true)
     include $(AUDIO_HW_ROOT)/alsa_sound/Android.mk
     include $(AUDIO_HW_ROOT)/libalsa-intf/Android.mk
+endif
 endif
 ifeq ($(TARGET_BOARD_PLATFORM),msm7x27a)
     include $(AUDIO_HW_ROOT)/msm7627a/Android.mk
