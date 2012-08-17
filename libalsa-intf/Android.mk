4bb2a8f5b006a3016077a4675fbf176924ed6b15
ehgrace.kim <ehgrace.kim@lge.com>
2012-08-17 16:44:18 -0700

    audio: enable the audio calibration for APQ side
    For audio calibration of Multimedia, add the libacdbloader
    
    Change-Id: If822f486f10aeed33a537710ff4d5075225cba8a

--- libalsa-intf/Android.mk
+++ libalsa-intf/Android.mk
@@ -73,12 +73,10 @@ LOCAL_SRC_FILES:= alsa_mixer.c alsa_pcm.c alsa_ucm.c
 LOCAL_MODULE:= libalsa-intf
 LOCAL_MODULE_TAGS := optional
 LOCAL_SHARED_LIBRARIES:= libc libcutils #libutils #libmedia libhardware_legacy
-LOCAL_CFLAGS := -DCONFIG_DIR=\"/system/etc/snd_soc_msm/\"
-ifeq ($(strip $(QC_PROP)),true)
 LOCAL_CFLAGS := -DQC_PROP -DCONFIG_DIR=\"/system/etc/snd_soc_msm/\"
 LOCAL_SHARED_LIBRARIES += libacdbloader
 LOCAL_C_INCLUDES += $(TARGET_OUT_HEADERS)/mm-audio/audio-acdb-util
-endif
+
 ifeq ($(TARGET_SIMULATOR),true)
  LOCAL_LDLIBS += -ldl
 else
