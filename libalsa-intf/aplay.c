ca2f3aace0d3699322019fd9e1b32d3d7b2bdd7b
Shareef Ali <shareefalis@gmail.com>
2012-08-24 23:08:04 -0500

    audio: fix compile errors
    Change-Id: Ie913f479dbe2e4490c5ed9009c5b07fa924fd7cc

--- libalsa-intf/aplay.c
+++ libalsa-intf/aplay.c
@@ -27,8 +27,6 @@
 #include <getopt.h>
 
 #include <sound/asound.h>
-#include <sound/compress_params.h>
-#include <sound/compress_offload.h>
 #include "alsa_audio.h"
 
 #ifndef ANDROID
@@ -204,6 +202,7 @@ static int play_file(unsigned rate, unsigned channels, int fd,
         return -EBADFD;
     }
 
+#ifdef QCOM_COMPRESSED_AUDIO_ENABLED
     if (compressed) {
        struct snd_compr_caps compr_cap;
        struct snd_compr_params compr_params;
@@ -231,6 +230,7 @@ static int play_file(unsigned rate, unsigned channels, int fd,
           return -errno;
        }
     }
+#endif
     pcm->channels = channels;
     pcm->rate = rate;
     pcm->flags = flags;
@@ -374,6 +374,7 @@ static int play_file(unsigned rate, unsigned channels, int fd,
                  fprintf(stderr, "Aplay:sync_ptr->s.status.hw_ptr %ld  sync_ptr->c.control.appl_ptr %ld\n",
                             pcm->sync_ptr->s.status.hw_ptr,
                             pcm->sync_ptr->c.control.appl_ptr);
+#ifdef QCOM_COMPRESSED_AUDIO_ENABLED
                  if (compressed && start) {
                     struct snd_compr_tstamp tstamp;
 		    if (ioctl(pcm->fd, SNDRV_COMPRESS_TSTAMP, &tstamp))
@@ -381,6 +382,7 @@ static int play_file(unsigned rate, unsigned channels, int fd,
                     else
 	                fprintf(stderr, "timestamp = %lld\n", tstamp.timestamp);
 		}
+#endif
              }
              /*
               * If we have reached start threshold of buffer prefill,
