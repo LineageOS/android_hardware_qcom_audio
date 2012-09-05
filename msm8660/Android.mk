ab626088ce792eb1f73b0c6dad6261ee3193dec0
Ethan Chen <intervigil@gmail.com>
2012-09-05 00:15:10 -0700

    audio/msm8660: enable QCOM_ACDB_ENABLED CFLAG for QCOM_HARDWARE
    Change-Id: Ibe54b96e042e3476ce62151a45bfc0bd16e3ae66

--- msm8660/Android.mk
+++ msm8660/Android.mk
@@ -58,6 +58,10 @@ LOCAL_C_INCLUDES += hardware/libhardware_legacy/include
 LOCAL_C_INCLUDES += frameworks/base/include
 LOCAL_C_INCLUDES += system/core/include
 
+ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
+LOCAL_CFLAGS += -DQCOM_ACDB_ENABLED
+endif
+
 ifeq ($(BOARD_HAVE_SAMSUNG_AUDIO),true)
 LOCAL_CFLAGS += -DSAMSUNG_AUDIO
 endif
