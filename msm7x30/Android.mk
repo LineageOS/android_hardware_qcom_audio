c86da26e39e36a02d77b260fb7000150f6681dd3
Arne Coucheron <arco68@gmail.com>
2012-09-15 07:31:38 +0200

    audio/msm7x30: Merge with CAF
    Change-Id: I53341f0be1bc3e4a1b8ec65e85b49809b724c4f1

--- msm7x30/Android.mk
+++ msm7x30/Android.mk
@@ -1,45 +1,41 @@
-# Copyright 2011 The Android Open Source Project
+ifneq ($(BUILD_TINY_ANDROID),true)
 
 #AUDIO_POLICY_TEST := true
 #ENABLE_AUDIO_DUMP := true
 
 LOCAL_PATH := $(call my-dir)
-
 include $(CLEAR_VARS)
-
-LOCAL_ARM_MODE := arm
-LOCAL_CFLAGS := -D_POSIX_SOURCE
 
 LOCAL_SRC_FILES := \
     AudioHardware.cpp \
     audio_hw_hal.cpp
 
 ifeq ($(BOARD_HAVE_BLUETOOTH),true)
-    LOCAL_CFLAGS += -DWITH_A2DP
+  LOCAL_CFLAGS += -DWITH_A2DP
 endif
 
 ifeq ($(BOARD_HAVE_QCOM_FM),true)
-    LOCAL_CFLAGS += -DQCOM_FM_ENABLED
+  LOCAL_CFLAGS += -DWITH_QCOM_FM
 endif
 
 ifeq ($(BOARD_USES_QCOM_AUDIO_LPA),true)
-    LOCAL_CFLAGS += -DQCOM_TUNNEL_LPA_ENABLED
+  LOCAL_CFLAGS += -DQCOM_TUNNEL_LPA_ENABLED
 endif
 
 ifeq ($(BOARD_USES_QCOM_AUDIO_SPEECH),true)
-    LOCAL_CFLAGS += -DWITH_QCOM_SPEECH
+  LOCAL_CFLAGS += -DWITH_QCOM_SPEECH
 endif
 
 ifeq ($(BOARD_USES_QCOM_AUDIO_VOIPMUTE),true)
-    LOCAL_CFLAGS += -DWITH_QCOM_VOIPMUTE
+  LOCAL_CFLAGS += -DWITH_QCOM_VOIPMUTE
 endif
 
 ifeq ($(BOARD_USES_QCOM_AUDIO_RESETALL),true)
-    LOCAL_CFLAGS += -DWITH_QCOM_RESETALL
+  LOCAL_CFLAGS += -DWITH_QCOM_RESETALL
 endif
 
 ifeq ($(BOARD_USES_STEREO_HW_SPEAKER),true)
-    LOCAL_CFLAGS += -DWITH_STEREO_HW_SPEAKER
+  LOCAL_CFLAGS += -DWITH_STEREO_HW_SPEAKER
 endif
 
 LOCAL_SHARED_LIBRARIES := \
@@ -53,18 +49,17 @@ $(shell mkdir -p $(OUT)/obj/SHARED_LIBRARIES/libaudioalsa_intermediates/)
 $(shell touch $(OUT)/obj/SHARED_LIBRARIES/libaudioalsa_intermediates/export_includes)
 
 ifeq ($(BOARD_USES_QCOM_AUDIO_CALIBRATION),true)
-    LOCAL_SHARED_LIBRARIES += libaudcal
-    LOCAL_CFLAGS += -DWITH_QCOM_CALIBRATION
+  LOCAL_SHARED_LIBRARIES += libaudcal
+  LOCAL_CFLAGS += -DWITH_QCOM_CALIBRATION
 endif
 
 ifneq ($(TARGET_SIMULATOR),true)
-    LOCAL_SHARED_LIBRARIES += libdl
+LOCAL_SHARED_LIBRARIES += libdl
 endif
 
 LOCAL_STATIC_LIBRARIES := \
     libmedia_helper \
-    libaudiohw_legacy \
-    libaudiopolicy_legacy
+    libaudiohw_legacy
 
 LOCAL_MODULE := audio.primary.$(TARGET_BOARD_PLATFORM)
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
@@ -74,7 +69,7 @@ LOCAL_CFLAGS += -fno-short-enums
 
 LOCAL_C_INCLUDES := $(TARGET_OUT_HEADERS)/mm-audio/audio-alsa
 ifeq ($(BOARD_USES_QCOM_AUDIO_CALIBRATION),true)
-    LOCAL_C_INCLUDES := $(TARGET_OUT_HEADERS)/mm-audio/audcal
+  LOCAL_C_INCLUDES += $(TARGET_OUT_HEADERS)/mm-audio/audcal
 endif
 LOCAL_C_INCLUDES += hardware/libhardware/include
 LOCAL_C_INCLUDES += hardware/libhardware_legacy/include
@@ -82,6 +77,7 @@ LOCAL_C_INCLUDES += frameworks/base/include
 LOCAL_C_INCLUDES += system/core/include
 
 include $(BUILD_SHARED_LIBRARY)
+
 
 # The audio policy is implemented on top of legacy policy code
 include $(CLEAR_VARS)
@@ -96,23 +92,24 @@ LOCAL_SHARED_LIBRARIES := \
     libmedia
 
 LOCAL_STATIC_LIBRARIES := \
-    libaudiohw_legacy \
     libmedia_helper \
     libaudiopolicy_legacy
 
 LOCAL_MODULE := audio_policy.$(TARGET_BOARD_PLATFORM)
+
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 LOCAL_MODULE_TAGS := optional
 
 ifeq ($(BOARD_HAVE_BLUETOOTH),true)
-    LOCAL_CFLAGS += -DWITH_A2DP
+  LOCAL_CFLAGS += -DWITH_A2DP
 endif
 
 ifeq ($(BOARD_USES_QCOM_AUDIO_LPA),true)
-    LOCAL_CFLAGS += -DQCOM_TUNNEL_LPA_ENABLED
+  LOCAL_CFLAGS += -DQCOM_TUNNEL_LPA_ENABLED
 endif
 
 LOCAL_C_INCLUDES := hardware/libhardware_legacy/audio
+
 
 include $(BUILD_SHARED_LIBRARY)
 
@@ -121,6 +118,9 @@ include $(CLEAR_VARS)
 LOCAL_MODULE       := audio_policy.conf
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)
+LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/
 LOCAL_SRC_FILES    := audio_policy.conf
 include $(BUILD_PREBUILT)
+
+
+endif # not BUILD_TINY_ANDROID
