0ec1fe7ae923b53016d394abb0d9beb6899bbaa4
Giulio Cervera <giulio.cervera@gmail.com>
2012-09-15 12:31:48 +0200

    audio/msm: fix 7x30 and 8660 voice call volume
    Change-Id: Ic47fe957eda23c202ef23fed0a88ffb6f2a95145

--- msm7x30/AudioPolicyManager.cpp
+++ msm7x30/AudioPolicyManager.cpp
@@ -694,7 +694,9 @@ status_t AudioPolicyManager::checkAndSetVolume(int stream, int index, audio_io_h
             // offset value to reflect actual hardware volume that never reaches 0
             // 1% corresponds roughly to first step in VOICE_CALL stream volume setting (see AudioService.java)
             volume = 0.01 + 0.99 * volume;
-            if (stream == AudioSystem::BLUETOOTH_SCO) {
+            if (stream == AudioSystem::VOICE_CALL) {
+                voiceVolume = (float)index/(float)mStreams[stream].mIndexMax;
+            } else if (stream == AudioSystem::BLUETOOTH_SCO) {
                voiceVolume = 1.0;
             }
             if (voiceVolume >= 0 && output == mPrimaryOutput) {
@@ -715,7 +717,6 @@ status_t AudioPolicyManager::checkAndSetVolume(int stream, int index, audio_io_h
         }
         mpClientInterface->setStreamVolume((AudioSystem::stream_type)stream, volume, output, delayMs);
     }
-
     return NO_ERROR;
 }
 
