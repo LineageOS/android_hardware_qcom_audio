73fb11d93274bc1c3675b24e910a4cb87571ffd0
Eric Laurent <elaurent@google.com>
2013-04-09 11:24:13 -0700

    audio: add voice processing effect wrapper
    Added wrapper library to expose Fluence AEC and NS to effect
    framework.
    
    Bug 7241490
    
    Change-Id: I9cec4a5e7dde210e0eb9f4dd3de341b9c83b340d

--- Android.mk
+++ Android.mk
@@ -1,11 +1,12 @@
 ifneq ($(filter msm8960 msm8226 msm8x26 msm8974 msm8x74,$(TARGET_BOARD_PLATFORM)),)
 
-LOCAL_PATH := $(call my-dir)
+MY_LOCAL_PATH := $(call my-dir)
 
 ifeq ($(BOARD_USES_LEGACY_ALSA_AUDIO),true)
-include $(LOCAL_PATH)/legacy/Android.mk
+include $(MY_LOCAL_PATH)/legacy/Android.mk
 else
-include $(LOCAL_PATH)/hal/Android.mk
+include $(MY_LOCAL_PATH)/hal/Android.mk
+include $(MY_LOCAL_PATH)/voice_processing/Android.mk
 endif
 
 endif
