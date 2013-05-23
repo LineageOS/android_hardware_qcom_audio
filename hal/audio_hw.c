f19eb03b30878589973498f9274d93dc339210fc
Daniel Hillenbrand <codeworkx@cyanogenmod.org>
2013-05-23 10:10:00 +0530

    hal: Support the audio amplifier hook
    * Original legacy HAL commit:
      Ib236598a5888b2af19bcfb81e285f644a0e84c0d
    * Example: http://review.cyanogenmod.org/38221
    
     (cyanogen: Refactored to be an audio_extn)
    
    Change-Id: Ic944a9f7059c78b79322dae9c787cdd8bb029cff
    
    audio: add amplifier stream start/standby operations
    
    Change-Id: I5de7ad7a0467e7cf822c9c0870a755c03d05e884
    
    hal: Convert libaudioamp to audio_amplifier HAL
    
    Change-Id: I1d0f63a46989d1792d7a5e08d2bdb6344ebafa31
    
    hal: Notify amplifier of device enable/disable
    
    Change-Id: Ice808c9b55a9e3bc8bafe5ca3ff555377d38dd8f
    
    hal: enable amplifier earlier
    
    Change-Id: Id876e8f836e3ce1ee5f8186ca9c0e6ef5f37182c
    
    hal: only open the amplifier once
    
    Change-Id: Ie9bbff74123e90b71e95809a84dcb3bbe9ba82fe
    
    hal: notify amplifier of parameter changes
    
    Change-Id: Iecc020c0347ae7c43d27183186e06dcefef7a0dd
    
    hal: Clean up audio amplifier usage
    
     * Externalize it into an extension file similar to the rest.
    
    Change-Id: I03de7efa9bab8870099028170fa471dfffe1ce84

--- hal/audio_hw.c
+++ hal/audio_hw.c
@@ -74,6 +74,7 @@
 #include <platform.h>
 #include "audio_extn.h"
 #include "voice_extn.h"
+#include "audio_amplifier.h"
 
 #include "sound/compress_params.h"
 #include "sound/asound.h"
@@ -950,6 +951,7 @@ int enable_snd_device(struct audio_device *adev,
             return -EINVAL;
         }
         audio_extn_dev_arbi_acquire(snd_device);
+        amplifier_enable_devices(snd_device, true);
         audio_route_apply_and_update_path(adev->audio_route, device_name);
 
         if (SND_DEVICE_OUT_HEADPHONES == snd_device &&
@@ -1004,6 +1006,7 @@ int disable_snd_device(struct audio_device *adev,
             }
         } else {
             audio_route_reset_and_update_path(adev->audio_route, device_name);
+            amplifier_enable_devices(snd_device, false);
         }
 
         if (SND_DEVICE_OUT_BT_A2DP == snd_device)
@@ -2026,6 +2029,10 @@ int select_devices(struct audio_device *adev, audio_usecase_t uc_id)
             voice_extn_compress_voip_is_started(adev))
             voice_set_sidetone(adev, out_snd_device, true);
     }
+
+    /* Rely on amplifier_set_devices to distinguish between in/out devices */
+    amplifier_set_input_devices(in_snd_device);
+    amplifier_set_output_devices(out_snd_device);
 
     /* Applicable only on the targets that has external modem.
      * Enable device command should be sent to modem only after
@@ -3084,6 +3091,9 @@ static int out_standby(struct audio_stream *stream)
             stop_compressed_output_l(out);
 
         pthread_mutex_lock(&adev->lock);
+
+        amplifier_output_stream_standby((struct audio_stream_out *) stream);
+
         out->standby = true;
         if (out->usecase == USECASE_COMPRESS_VOIP_CALL) {
             voice_extn_compress_voip_close_output_stream(stream);
@@ -3916,6 +3926,11 @@ static ssize_t out_write(struct audio_stream_out *stream, const void *buffer,
             ret = voice_extn_compress_voip_start_output_stream(out);
         else
             ret = start_output_stream(out);
+
+        if (ret == 0)
+            amplifier_output_stream_start(stream,
+                    is_offload_usecase(out->usecase));
+
         pthread_mutex_unlock(&adev->lock);
         /* ToDo: If use case is compress offload should return 0 */
         if (ret != 0) {
@@ -4631,6 +4646,9 @@ static int in_standby(struct audio_stream *stream)
             adev->adm_deregister_stream(adev->adm_data, in->capture_handle);
 
         pthread_mutex_lock(&adev->lock);
+
+        amplifier_input_stream_standby((struct audio_stream_in *) stream);
+
         in->standby = true;
         if (in->usecase == USECASE_COMPRESS_VOIP_CALL) {
             do_stop = false;
@@ -4846,6 +4864,11 @@ static ssize_t in_read(struct audio_stream_in *stream, void *buffer,
             ret = voice_extn_compress_voip_start_input_stream(in);
         else
             ret = start_input_stream(in);
+
+        if (ret == 0)
+            amplifier_input_stream_start(stream);
+
+
         pthread_mutex_unlock(&adev->lock);
         if (ret != 0) {
             goto exit;
@@ -5979,6 +6002,7 @@ static int adev_set_parameters(struct audio_hw_device *dev, const char *kvpairs)
     }
 
     audio_extn_set_parameters(adev, parms);
+    amplifier_set_parameters(parms);
 done:
     str_parms_destroy(parms);
     pthread_mutex_unlock(&adev->lock);
@@ -6095,6 +6119,8 @@ static int adev_set_mode(struct audio_hw_device *dev, audio_mode_t mode)
     pthread_mutex_lock(&adev->lock);
     if (adev->mode != mode) {
         ALOGD("%s: mode %d\n", __func__, mode);
+        if (amplifier_set_mode(mode) != 0)
+            ALOGE("Failed setting amplifier mode");
         adev->mode = mode;
         if ((mode == AUDIO_MODE_NORMAL) && voice_is_in_call(adev)) {
             voice_stop_call(adev);
@@ -6575,6 +6601,8 @@ static int adev_close(hw_device_t *device)
     pthread_mutex_lock(&adev_init_lock);
 
     if ((--audio_device_ref_count) == 0) {
+        if (amplifier_close() != 0)
+            ALOGE("Amplifier close failed");
         audio_extn_snd_mon_unregister_listener(adev);
         audio_extn_sound_trigger_deinit(adev);
         audio_extn_listen_deinit(adev);
@@ -6920,6 +6948,10 @@ static int adev_open(const hw_module_t *module, const char *name,
     adev->vr_audio_mode_enabled = false;
 
     audio_extn_ds2_enable(adev);
+
+    if (amplifier_open(adev) != 0)
+        ALOGE("Amplifier initialization failed");
+
     *device = &adev->device.common;
 
     audio_extn_utils_update_streams_cfg_lists(adev->platform, adev->mixer,
