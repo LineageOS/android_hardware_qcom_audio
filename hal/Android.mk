d3143a2a13cd69eb8ec8d9b094dda7b666799ac4
Daniel Hillenbrand <codeworkx@cyanogenmod.org>
2013-05-23 10:10:00 +0530

    hal: Support the audio amplifier hook
    * Original legacy HAL commit:
      Ib236598a5888b2af19bcfb81e285f644a0e84c0d
    * Example: http://review.cyanogenmod.org/38221
    
     (cyanogen: Refactored to be an audio_extn)
    
    Change-Id: Ic944a9f7059c78b79322dae9c787cdd8bb029cff
    
    audio: add amplifier stream start/standby operations
    
    Change-Id: I5de7ad7a0467e7cf822c9c0870a755c03d05e884
    
    hal: Convert libaudioamp to audio_amplifier HAL
    
    Change-Id: I1d0f63a46989d1792d7a5e08d2bdb6344ebafa31
    
    hal: Notify amplifier of device enable/disable
    
    Change-Id: Ice808c9b55a9e3bc8bafe5ca3ff555377d38dd8f
    
    hal: enable amplifier earlier
    
    Change-Id: Id876e8f836e3ce1ee5f8186ca9c0e6ef5f37182c
    
    hal: only open the amplifier once
    
    Change-Id: Ie9bbff74123e90b71e95809a84dcb3bbe9ba82fe
    
    hal: notify amplifier of parameter changes
    
    Change-Id: Iecc020c0347ae7c43d27183186e06dcefef7a0dd
    
    hal: Clean up audio amplifier usage
    
     * Externalize it into an extension file similar to the rest.
    
    Change-Id: I03de7efa9bab8870099028170fa471dfffe1ce84

--- hal/Android.mk
+++ hal/Android.mk
@@ -251,6 +251,7 @@ endif
 LOCAL_SHARED_LIBRARIES := \
 	liblog \
 	libcutils \
+	libhardware \
 	libtinyalsa \
 	libtinycompress \
 	libaudioroute \
@@ -264,6 +265,7 @@ LOCAL_C_INCLUDES += \
 	external/tinycompress/include \
 	system/media/audio_utils/include \
 	external/expat/lib \
+	hardware/libhardware/include \
 	$(call include-path-for, audio-route) \
 	$(call include-path-for, audio-effects) \
 	$(LOCAL_PATH)/$(AUDIO_PLATFORM) \
@@ -300,6 +302,11 @@ ifeq ($(strip $(AUDIO_FEATURE_ENABLED_PM_SUPPORT)),true)
     LOCAL_SRC_FILES += audio_extn/pm.c
     LOCAL_C_INCLUDES += $(TARGET_OUT_HEADERS)/libperipheralclient/inc
     LOCAL_SHARED_LIBRARIES += libperipheral_client
+endif
+
+ifneq ($(strip $(AUDIO_FEATURE_ENABLED_EXT_AMPLIFIER)),false)
+    LOCAL_CFLAGS += -DEXT_AMPLIFIER_ENABLED
+    LOCAL_SRC_FILES += audio_extn/audio_amplifier.c
 endif
 
 LOCAL_CFLAGS += -Wall -Werror
