17873959068c81eb4837a9071ff778e3549ee7a7
Daniel Hillenbrand <codeworkx@cyanogenmod.org>
2013-05-23 10:10:00 +0530

    hal: Support the audio amplifier hook
    * Original legacy HAL commit:
      Ib236598a5888b2af19bcfb81e285f644a0e84c0d
    * Example: http://review.cyanogenmod.org/38221
    
     (cyanogen: Refactored to be an audio_extn)
    
    Change-Id: Ic944a9f7059c78b79322dae9c787cdd8bb029cff
    
    audio: add amplifier stream start/standby operations
    
    Change-Id: I5de7ad7a0467e7cf822c9c0870a755c03d05e884
    
    hal: Convert libaudioamp to audio_amplifier HAL
    
    Change-Id: I1d0f63a46989d1792d7a5e08d2bdb6344ebafa31
    
    hal: Notify amplifier of device enable/disable
    
    Change-Id: Ice808c9b55a9e3bc8bafe5ca3ff555377d38dd8f
    
    hal: enable amplifier earlier
    
    Change-Id: Id876e8f836e3ce1ee5f8186ca9c0e6ef5f37182c
    
    hal: only open the amplifier once
    
    Change-Id: Ie9bbff74123e90b71e95809a84dcb3bbe9ba82fe
    
    hal: notify amplifier of parameter changes
    
    Change-Id: Iecc020c0347ae7c43d27183186e06dcefef7a0dd
    
    hal: Clean up audio amplifier usage
    
     * Externalize it into an extension file similar to the rest.
    
    Change-Id: I03de7efa9bab8870099028170fa471dfffe1ce84
    
    audio: add amplifier hooks for stream parameter manipulation
    
    hal: Use log/log.h instead of cutils/log.h
    
    Change-Id: I6e52524cc650eea234fbcf64ed17104dd8a3f001
    
    [Pig]: Clang-format
    
    audio_amplifier: Add new hook for setting amp feedback
    
    Change-Id: I6de5e9c9dbbb6214b166f30628734aeb406c8a2a
    
    Change-Id: I16a76475cb58a710d229648d77024379bfc499d5

--- hal/Android.mk
+++ hal/Android.mk
@@ -380,6 +380,12 @@ ifeq ($(strip $(AUDIO_FEATURE_ENABLED_AHAL_EXT)),true)
     LOCAL_SHARED_LIBRARIES += vendor.qti.hardware.audiohalext@1.0
 endif
 
+ifeq ($(strip $(AUDIO_FEATURE_ENABLED_EXT_AMPLIFIER)),true)
+    LOCAL_CFLAGS += -DEXT_AMPLIFIER_ENABLED
+    LOCAL_SRC_FILES += audio_extn/audio_amplifier.c
+    LOCAL_SHARED_LIBRARIES += libhardware
+endif
+
 LOCAL_CFLAGS += -D_GNU_SOURCE
 LOCAL_CFLAGS += -Wall -Werror
 
--- hal/audio_hw.h
+++ hal/audio_hw.h
@@ -42,6 +42,7 @@
 #include <cutils/str_parms.h>
 #include <cutils/list.h>
 #include <cutils/hashmap.h>
+#include <hardware/audio_amplifier.h>
 #include <hardware/audio.h>
 #include <tinyalsa/asoundlib.h>
 #include <tinycompress/tinycompress.h>
@@ -792,6 +793,8 @@ struct audio_device {
     Hashmap *io_streams_map;
     bool a2dp_started;
     bool ha_proxy_enable;
+
+    amplifier_device_t *amp;
 };
 
 struct audio_patch_record {
