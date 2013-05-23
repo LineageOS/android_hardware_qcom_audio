1da9a45b58399d8e031cb6b1512072a2ba454575
Daniel Hillenbrand <codeworkx@cyanogenmod.org>
2013-05-23 10:10:00 +0530

    hal: Support the audio amplifier hook
    * Original legacy HAL commit:
      Ib236598a5888b2af19bcfb81e285f644a0e84c0d
    * Example: http://review.cyanogenmod.org/38221
    
     (cyanogen: Refactored to be an audio_extn)
    
    Change-Id: Ic944a9f7059c78b79322dae9c787cdd8bb029cff
    
    audio: add amplifier stream start/standby operations
    
    Change-Id: I5de7ad7a0467e7cf822c9c0870a755c03d05e884
    
    hal: Convert libaudioamp to audio_amplifier HAL
    
    Change-Id: I1d0f63a46989d1792d7a5e08d2bdb6344ebafa31
    
    hal: Notify amplifier of device enable/disable
    
    Change-Id: Ice808c9b55a9e3bc8bafe5ca3ff555377d38dd8f
    
    hal: enable amplifier earlier
    
    Change-Id: Id876e8f836e3ce1ee5f8186ca9c0e6ef5f37182c
    
    hal: only open the amplifier once
    
    Change-Id: Ie9bbff74123e90b71e95809a84dcb3bbe9ba82fe
    
    hal: notify amplifier of parameter changes
    
    Change-Id: Iecc020c0347ae7c43d27183186e06dcefef7a0dd
    
    hal: Clean up audio amplifier usage
    
     * Externalize it into an extension file similar to the rest.
    
    Change-Id: I03de7efa9bab8870099028170fa471dfffe1ce84

--- hal/audio_hw.h
+++ hal/audio_hw.h
@@ -40,6 +40,7 @@
 
 #include <stdlib.h>
 #include <cutils/list.h>
+#include <hardware/audio_amplifier.h>
 #include <hardware/audio.h>
 #include <tinyalsa/asoundlib.h>
 #include <tinycompress/tinycompress.h>
@@ -503,6 +504,7 @@ struct audio_device {
     bool bt_sco_on;
     struct audio_device_config_param *device_cfg_params;
     unsigned int interactive_usecase_state;
+    amplifier_device_t *amp;
 };
 
 int select_devices(struct audio_device *adev,
