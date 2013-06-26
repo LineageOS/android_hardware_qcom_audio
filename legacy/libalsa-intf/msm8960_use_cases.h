3e51042a863f5a070ef49d098f2a526707c9e7da
Steve Kondik <shade@chemlab.org>
2013-06-26 00:07:01 -0700

    alsa_sound: Add support for seperated VOIP devices
     * Similar to the VOC devices tuned for voice calls, some new devices
       are using VOIP devices tuned for SIP and Hangouts.
     * Enable with BOARD_USES_SEPERATED_VOIP
    
    Change-Id: I3a49be26ca2ad3e1aafb8b0960935ac8c735fe51

--- legacy/libalsa-intf/msm8960_use_cases.h
+++ legacy/libalsa-intf/msm8960_use_cases.h
@@ -306,6 +306,12 @@ static card_mapping_t card_mapping_list[] = {
 #define SND_USE_CASE_DEV_VOC_LINE           "Voice Line"
 #define SND_USE_CASE_DEV_AANC_LINE          "AANC Line"
 #define SND_USE_CASE_DEV_AANC_DMIC_ENDFIRE  "AANC DMIC Endfire"
+#define SND_USE_CASE_DEV_VOIP_EARPIECE      "VOIP Earpiece"
+#define SND_USE_CASE_DEV_VOIP_HEADPHONE     "VOIP Headphones"
+#define SND_USE_CASE_DEV_VOIP_HEADSET       "VOIP Headset"
+#define SND_USE_CASE_DEV_VOIP_SPEAKER       "VOIP Speaker"
+#define SND_USE_CASE_DEV_VOIP_LINE          "VOIP Line"
+#define SND_USE_CASE_DEV_VOIP_HANDSET       "VOIP Handset"
 
 #define SND_USE_CASE_MOD_PLAY_FM         "Play FM"
 #define SND_USE_CASE_MOD_CAPTURE_FM      "Capture FM"
