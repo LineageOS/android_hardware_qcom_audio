07fab57f61b8b91861ac6a6e37403601aef68356
Mike Grissom <mikeyman77@gmail.com>
2013-08-06 16:38:39 -0700

    msm8660: add missing get_supported_devices in HW HAL
    Change-Id: I1198bb8ca7c8c3c31eaf0af2a2e8a6761392bdd3

--- msm8660/audio_hw_hal.cpp
+++ msm8660/audio_hw_hal.cpp
@@ -488,6 +488,69 @@ static inline const struct qcom_audio_device * to_cladev(const struct audio_hw_d
     return reinterpret_cast<const struct qcom_audio_device *>(dev);
 }
 
+static uint32_t adev_get_supported_devices(const struct audio_hw_device *dev)
+{
+    /* XXX: The old AudioHardwareInterface interface is not smart enough to
+* tell us this, so we'll lie and basically tell AF that we support the
+* below input/output devices and cross our fingers. To do things properly,
+* audio hardware interfaces that need advanced features (like this) should
+* convert to the new HAL interface and not use this wrapper. */
+    return (/* OUT */
+            AUDIO_DEVICE_OUT_EARPIECE |
+            AUDIO_DEVICE_OUT_SPEAKER |
+            AUDIO_DEVICE_OUT_WIRED_HEADSET |
+            AUDIO_DEVICE_OUT_WIRED_HEADPHONE |
+            AUDIO_DEVICE_OUT_BLUETOOTH_SCO |
+            AUDIO_DEVICE_OUT_BLUETOOTH_SCO_HEADSET |
+            AUDIO_DEVICE_OUT_BLUETOOTH_SCO_CARKIT |
+            AUDIO_DEVICE_OUT_BLUETOOTH_A2DP |
+            AUDIO_DEVICE_OUT_BLUETOOTH_A2DP_HEADPHONES |
+            AUDIO_DEVICE_OUT_BLUETOOTH_A2DP_SPEAKER |
+            AUDIO_DEVICE_OUT_AUX_DIGITAL |
+            AUDIO_DEVICE_OUT_ANLG_DOCK_HEADSET |
+            AUDIO_DEVICE_OUT_DGTL_DOCK_HEADSET |
+            AUDIO_DEVICE_OUT_USB_ACCESSORY |
+            AUDIO_DEVICE_OUT_USB_DEVICE |
+            AUDIO_DEVICE_OUT_REMOTE_SUBMIX |
+#ifdef QCOM_ANC_HEADSET_ENABLED
+            AUDIO_DEVICE_OUT_ANC_HEADSET |
+            AUDIO_DEVICE_OUT_ANC_HEADPHONE |
+#endif
+#ifdef QCOM_PROXY_DEVICE_ENABLED
+            AUDIO_DEVICE_OUT_PROXY |
+#endif
+#if defined(QCOM_FM_ENABLED) || defined(STE_FM)
+            AUDIO_DEVICE_OUT_FM |
+            AUDIO_DEVICE_OUT_FM_TX |
+#endif
+            AUDIO_DEVICE_OUT_DEFAULT |
+            /* IN */
+            AUDIO_DEVICE_IN_COMMUNICATION |
+            AUDIO_DEVICE_IN_AMBIENT |
+            AUDIO_DEVICE_IN_BUILTIN_MIC |
+            AUDIO_DEVICE_IN_BLUETOOTH_SCO_HEADSET |
+            AUDIO_DEVICE_IN_WIRED_HEADSET |
+            AUDIO_DEVICE_IN_AUX_DIGITAL |
+            AUDIO_DEVICE_IN_VOICE_CALL |
+            AUDIO_DEVICE_IN_BACK_MIC |
+            AUDIO_DEVICE_IN_REMOTE_SUBMIX |
+            AUDIO_DEVICE_IN_ANLG_DOCK_HEADSET |
+            AUDIO_DEVICE_IN_DGTL_DOCK_HEADSET |
+            AUDIO_DEVICE_IN_USB_ACCESSORY |
+            AUDIO_DEVICE_IN_USB_DEVICE |
+#ifdef QCOM_ANC_HEADSET_ENABLED
+            AUDIO_DEVICE_IN_ANC_HEADSET |
+#endif
+#ifdef QCOM_PROXY_DEVICE_ENABLED
+            AUDIO_DEVICE_IN_PROXY |
+#endif
+#if defined(QCOM_FM_ENABLED) || defined(STE_FM)
+            AUDIO_DEVICE_IN_FM_RX |
+            AUDIO_DEVICE_IN_FM_RX_A2DP |
+#endif
+            AUDIO_DEVICE_IN_DEFAULT);
+}
+
 static int adev_init_check(const struct audio_hw_device *dev)
 {
     const struct qcom_audio_device *qadev = to_cladev(dev);
@@ -742,6 +805,7 @@ static int qcom_adev_open(const hw_module_t* module, const char* name,
     qadev->device.common.module = const_cast<hw_module_t*>(module);
     qadev->device.common.close = qcom_adev_close;
 
+    qadev->device.get_supported_devices = adev_get_supported_devices;
     qadev->device.init_check = adev_init_check;
     qadev->device.set_voice_volume = adev_set_voice_volume;
     qadev->device.set_master_volume = adev_set_master_volume;
