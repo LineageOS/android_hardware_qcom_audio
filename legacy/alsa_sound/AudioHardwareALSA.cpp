0505b7c19d6f9a4a1b10381413f647212df7209e
Preetam Singh Ranawat <apranawat@codeaurora.org>
2013-10-01 19:41:43 +0530

    alsa_sound: Enable support for LPA/Tunnel audio playback
     -add the profile param to get the CompatibleProfile using
      derived function in APMAlsa.
    
    Change-Id: I56573e23b0df40ec853628b32678e55a03262d3f

--- legacy/alsa_sound/AudioHardwareALSA.cpp
+++ legacy/alsa_sound/AudioHardwareALSA.cpp
@@ -1222,7 +1222,7 @@ AudioHardwareALSA::openOutputStream(uint32_t devices,
          devices, *channels, *sampleRate, flags);
 
     status_t err = BAD_VALUE;
-#ifdef QCOM_OUTPUT_LPA_ENABLED
+#ifdef QCOM_TUNNEL_LPA_ENABLED
     if (flags & (AUDIO_OUTPUT_FLAG_LPA | AUDIO_OUTPUT_FLAG_TUNNEL)) {
         int type = !(flags & AUDIO_OUTPUT_FLAG_LPA); //0 for LPA, 1 for tunnel
         AudioSessionOutALSA *out = new AudioSessionOutALSA(this, devices, *format, *channels,
