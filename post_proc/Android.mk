3eedc004e9adf5967f393d65c22b7806d0c63e6c
Subhash Chandra Bose Naripeddy <snariped@codeaurora.org>
2013-11-12 20:45:15 -0800

    post_proc: Support effects in DSP for tunnel mode playback
    Add interface to support configuring equlaizer, bass boost,
    virtualizer and reverb effects in DSP for tunnel mode of
    playback.
    
    Change-Id: I9a93ad6136c4de06df5136967908b4e888dae44f

---
+++ post_proc/Android.mk
@@ -0,0 +1,31 @@
+
+LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES:= \
+	bundle.c \
+	equalizer.c \
+	bass_boost.c \
+	virtualizer.c \
+	reverb.c \
+	effect_api.c
+
+LOCAL_CFLAGS+= -O2 -fvisibility=hidden
+
+LOCAL_SHARED_LIBRARIES := \
+	libcutils \
+	liblog \
+	libtinyalsa
+
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/soundfx
+LOCAL_MODULE:= libqcompostprocbundle
+
+LOCAL_C_INCLUDES := \
+	external/tinyalsa/include \
+	kernel/include/sound \
+	$(call include-path-for, audio-effects)
+
+include $(BUILD_SHARED_LIBRARY)
