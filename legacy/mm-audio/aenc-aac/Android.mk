a94e52965de8d67b7e2aec49222402389d24abb9
AUDITYA BHATTARAM <audityab@codeaurora.org>
2013-11-14 22:10:46 +0530

    Remove AOSP reference to utils.mk
    Remove function calls from AOSP code to utils.mk
    & replace them with actual definition.
    
    Change-Id: Ie0ae58e6eb4a4f54d58e15ac8f1d819dbdac5cd2
    CRs-fixed: 566287

--- legacy/mm-audio/aenc-aac/Android.mk
+++ legacy/mm-audio/aenc-aac/Android.mk
@@ -3,13 +3,13 @@ ifeq ($(TARGET_ARCH),arm)
 
 AENC_AAC_PATH:= $(call my-dir)
 
-ifeq ($(call is-board-platform,msm8660),true)
+ifeq ($(TARGET_BOARD_PLATFORM),msm8660)
 include $(AENC_AAC_PATH)/qdsp6/Android.mk
 endif
-ifeq ($(call is-board-platform,msm8960),true)
+ifeq ($(TARGET_BOARD_PLATFORM),msm8960)
 include $(AENC_AAC_PATH)/qdsp6/Android.mk
 endif
-ifeq ($(call is-board-platform,msm8974),true)
+ifeq ($(TARGET_BOARD_PLATFORM),msm8974)
 include $(AENC_AAC_PATH)/qdsp6/Android.mk
 endif
 
