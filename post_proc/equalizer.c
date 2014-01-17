788431a4316dad36c3d6023802e2b8d20307367f
Amit Shekhar <ashekhar@codeaurora.org>
2014-01-16 16:51:43 -0800

    post_proc: Enable effects param in DSP to start effect
    -During switch to tunnel playback, offload effects-flags remain
    disabled. This stops effects being applied on tunnel playback.
    -Send effects enable params to DSP to start effects during switch.
    
    Change-Id: Ibe1e50b41ca25b51537e4a0ad80dbd5184379133
    CRs-Fixed: 593509

--- post_proc/equalizer.c
+++ post_proc/equalizer.c
@@ -491,6 +491,11 @@ int equalizer_start(effect_context_t *context, output_context_t *output)
 
     ALOGV("%s: %p", __func__, output->ctl);
     eq_ctxt->ctl = output->ctl;
+    if (offload_eq_get_enable_flag(&(eq_ctxt->offload_eq)))
+        if (eq_ctxt->ctl)
+            offload_eq_send_params(eq_ctxt->ctl, eq_ctxt->offload_eq,
+                                   OFFLOAD_SEND_EQ_ENABLE_FLAG |
+                                   OFFLOAD_SEND_EQ_BANDS_LEVEL);
     return 0;
 }
 
