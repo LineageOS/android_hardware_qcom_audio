739e7152580eb7daff0f957c8a15f906c8d008bf
Vimal Puthanveed <vimalp@codeaurora.org>
2014-01-23 15:56:53 -0800

    hal: Check hfp usecase to differenciate between nb and wb
     - Need to check if the hfp session is WB or not before
       setting the device to avoid rerouting.
    
    CRs-Fixed: 605465
    Change-Id: Ice6fd2776b89e5358004071b011ac304f35dbf58

--- hal/audio_hw.c
+++ hal/audio_hw.c
@@ -602,6 +602,7 @@ int select_devices(struct audio_device *adev, audio_usecase_t uc_id)
     struct audio_usecase *vc_usecase = NULL;
     struct audio_usecase *voip_usecase = NULL;
     struct audio_usecase *hfp_usecase = NULL;
+    audio_usecase_t hfp_ucid;
     struct listnode *node;
     int status = 0;
 
@@ -641,7 +642,8 @@ int select_devices(struct audio_device *adev, audio_usecase_t uc_id)
                     out_snd_device = voip_usecase->out_snd_device;
             }
         } else if (audio_extn_hfp_is_active(adev)) {
-            hfp_usecase = get_usecase_from_list(adev, USECASE_AUDIO_HFP_SCO);
+            hfp_ucid = audio_extn_hfp_get_usecase();
+            hfp_usecase = get_usecase_from_list(adev, hfp_ucid);
             if (hfp_usecase->devices & AUDIO_DEVICE_OUT_ALL_CODEC_BACKEND) {
                    in_snd_device = hfp_usecase->in_snd_device;
                    out_snd_device = hfp_usecase->out_snd_device;
