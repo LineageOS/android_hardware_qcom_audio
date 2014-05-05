80e2d861c927f1eb7c26853dadd324ddf8442090
Ravi Kumar Alamanda <ralama@codeaurora.org>
2014-05-05 16:00:33 -0700

    hal: Correct audio HAL library name
    - Ensure that the platform name is reflected in the audio HAL
      library name.
    
    Change-Id: I942d7c5b428cd1e73cdf598c74f5203c881a060a

--- hal/Android.mk
+++ hal/Android.mk
@@ -7,7 +7,7 @@ include $(CLEAR_VARS)
 LOCAL_ARM_MODE := arm
 
 AUDIO_PLATFORM := $(TARGET_BOARD_PLATFORM)
-ifneq ($(filter msm8974 msm8226,$(TARGET_BOARD_PLATFORM)),)
+ifneq ($(filter msm8974 msm8226 msm8084,$(TARGET_BOARD_PLATFORM)),)
   # B-family platform uses msm8974 code base
   AUDIO_PLATFORM = msm8974
 endif
@@ -32,7 +32,7 @@ LOCAL_C_INCLUDES += \
 	$(call include-path-for, audio-effects) \
 	$(LOCAL_PATH)/$(AUDIO_PLATFORM)
 
-LOCAL_MODULE := audio.primary.$(AUDIO_PLATFORM)
+LOCAL_MODULE := audio.primary.$(TARGET_BOARD_PLATFORM)
 
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 
