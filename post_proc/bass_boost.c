4926a88f272b6d72d2fdf128804f491e6e708e9b
Dhananjay Kumar <dhakumar@codeaurora.org>
2014-05-06 16:48:17 +0530

    post_proc: disable Bassboost and Virtualizer for USB_ACCESSORY
    USB_ACCESSORY is not intended to be applied with SA+ bassboost
    and virtualizer, so add into invalid devices list.
    
    CRs-Fixed: 659191
    Change-Id: If1f3421f9935ce624dc21895d9244d36d195b2bd

--- post_proc/bass_boost.c
+++ post_proc/bass_boost.c
@@ -156,6 +156,7 @@ int bassboost_set_device(effect_context_t *context, uint32_t device)
        (device == AUDIO_DEVICE_OUT_BLUETOOTH_A2DP_SPEAKER) ||
        (device == AUDIO_DEVICE_OUT_PROXY) ||
        (device == AUDIO_DEVICE_OUT_AUX_DIGITAL) ||
+       (device == AUDIO_DEVICE_OUT_USB_ACCESSORY) ||
        (device == AUDIO_DEVICE_OUT_ANLG_DOCK_HEADSET)) {
         if (offload_bassboost_get_enable_flag(&(bass_ctxt->offload_bass))) {
             offload_bassboost_set_enable_flag(&(bass_ctxt->offload_bass), false);
