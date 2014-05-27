83474359780add0faa3a1c1d883c404facccc7dd
Manish Dewangan <manish@codeaurora.org>
2014-05-27 18:12:42 +0530

    mm-audio: Fix 64 bit compilation warnings
    Fix compilation warnings seen with 64 bit lpdk builds
    
    Change-Id: Ia82c54f81531c31569a96b8cdde48b7f7fc7a4b7

--- mm-audio/aenc-qcelp13/qdsp6/src/omx_qcelp13_aenc.cpp
+++ mm-audio/aenc-qcelp13/qdsp6/src/omx_qcelp13_aenc.cpp
@@ -454,7 +454,7 @@ void omx_qcelp13_aenc::buffer_done_cb(OMX_BUFFERHEADERTYPE *bufHdr)
         pthread_mutex_lock(&in_buf_count_lock);
         m_qcelp13_pb_stats.ebd_cnt++;
         nNumInputBuf--;
-        DEBUG_DETAIL("EBD CB:: in_buf_len=%d nNumInputBuf=%d\n",\
+        DEBUG_DETAIL("EBD CB:: in_buf_len=%d nNumInputBuf=%d\n ebd_cnt=%d",\
                      m_qcelp13_pb_stats.tot_in_buf_len,
                      nNumInputBuf, m_qcelp13_pb_stats.ebd_cnt);
         pthread_mutex_unlock(&in_buf_count_lock);
@@ -710,7 +710,7 @@ loopback_out:
                 pThis->execute_output_omx_flush();
             } else
             {
-                DEBUG_DETAIL("Invalid command[%d]\n",p1);
+                DEBUG_DETAIL("Invalid command[%lu]\n",p1);
             }
         } else
         {
@@ -1026,7 +1026,7 @@ loopback_in:
                 pThis->execute_input_omx_flush();
             } else
             {
-                DEBUG_DETAIL("Invalid command[%d]\n",p1);
+                DEBUG_DETAIL("Invalid command[%lu]\n",p1);
             }
         }
         else
@@ -2132,7 +2132,7 @@ bool omx_qcelp13_aenc::execute_input_omx_flush()
                 (ident == OMX_COMPONENT_GENERATE_BUFFER_DONE))
             {
                 omx_buf = (OMX_BUFFERHEADERTYPE *) p2;
-                DEBUG_DETAIL("Flush:Input dataq=0x%x \n", omx_buf);
+                DEBUG_DETAIL("Flush:Input dataq=%p \n", omx_buf);
                 omx_buf->nFilledLen = 0;
                 buffer_done_cb((OMX_BUFFERHEADERTYPE *)omx_buf);
             }
@@ -2143,7 +2143,7 @@ bool omx_qcelp13_aenc::execute_input_omx_flush()
             {
                 omx_buf = (OMX_BUFFERHEADERTYPE *) p2;
                 omx_buf->nFilledLen = 0;
-                DEBUG_DETAIL("Flush:ctrl dataq=0x%x \n", omx_buf);
+                DEBUG_DETAIL("Flush:ctrl dataq=%p \n", omx_buf);
                 buffer_done_cb((OMX_BUFFERHEADERTYPE *)omx_buf);
             }
         } else
@@ -2211,7 +2211,7 @@ bool omx_qcelp13_aenc::execute_output_omx_flush()
                  (OMX_COMPONENT_GENERATE_FRAME_DONE == ident))
             {
                 omx_buf = (OMX_BUFFERHEADERTYPE *) p2;
-                DEBUG_DETAIL("Ouput Buf_Addr=%x TS[0x%x] \n",\
+                DEBUG_DETAIL("Ouput Buf_Addr=%p TS[0x%x] \n",\
                              omx_buf,nTimestamp);
                 omx_buf->nTimeStamp = nTimestamp;
                 omx_buf->nFilledLen = 0;
@@ -2224,7 +2224,7 @@ bool omx_qcelp13_aenc::execute_output_omx_flush()
             if (OMX_COMPONENT_GENERATE_FRAME_DONE == ident)
             {
                 omx_buf = (OMX_BUFFERHEADERTYPE *) p2;
-                DEBUG_DETAIL("Ouput Buf_Addr=%x TS[0x%x] \n", \
+                DEBUG_DETAIL("Ouput Buf_Addr=%p TS[0x%x] \n", \
                              omx_buf,nTimestamp);
                 omx_buf->nTimeStamp = nTimestamp;
                 omx_buf->nFilledLen = 0;
@@ -3705,7 +3705,7 @@ bool omx_qcelp13_aenc::search_input_bufhdr(OMX_BUFFERHEADERTYPE *buffer)
     temp = m_input_buf_hdrs.find_ele(buffer);
     if (buffer && temp)
     {
-        DEBUG_DETAIL("search_input_bufhdr %x \n", buffer);
+        DEBUG_DETAIL("search_input_bufhdr %p \n", buffer);
         eRet = true;
     }
     return eRet;
@@ -3727,7 +3727,7 @@ bool omx_qcelp13_aenc::search_output_bufhdr(OMX_BUFFERHEADERTYPE *buffer)
     temp = m_output_buf_hdrs.find_ele(buffer);
     if (buffer && temp)
     {
-        DEBUG_DETAIL("search_output_bufhdr %x \n", buffer);
+        DEBUG_DETAIL("search_output_bufhdr %p \n", buffer);
         eRet = true;
     }
     return eRet;
@@ -4039,7 +4039,7 @@ OMX_ERRORTYPE  omx_qcelp13_aenc::fill_this_buffer_proxy
     {
           DEBUG_PRINT("\nBefore Read..m_drv_fd = %d,\n",m_drv_fd);
           nReadbytes = read(m_drv_fd,buffer->pBuffer,output_buffer_size );
-          DEBUG_DETAIL("FTBP->Al_len[%d]buf[%p]size[%d]numOutBuf[%d]\n",\
+          DEBUG_DETAIL("FTBP->Al_len[%lu]buf[%p]size[%d]numOutBuf[%d]\n",\
                          buffer->nAllocLen,buffer->pBuffer,
                          nReadbytes,nNumOutputBuf);
       if (nReadbytes <= 0) {
