7e34495eda2ed48a137414fa7e427a8ed7a0eea2
Mingming Yin <mingming@codeaurora.org>
2014-06-12 16:05:49 -0700

    policy_hal: Initial change for the new libaudiopolicymanager
    - Upgrade policy_hal to use new AudioPolicyManager
      introduced by Google. The legacy AudioPolicyManagerBase
      class is replaced by AudioPolicyManager.
    - Customized AudioPolicyManager needs to implement everything
      from /frameworks/av/service/audiopolicy/AudioPolicyManager
      and add extended changes on top of it
    - This change implements stock AOSP AudioPolicyManager with no
      Additional changes.
    
    Change-Id: I56f7c575e60c51876fc5eda59b2eaa29d4e77639

--- policy_hal/Android.mk
+++ policy_hal/Android.mk
@@ -1,43 +1,26 @@
-ifeq ($(strip $(BOARD_USES_EXTN_AUDIO_POLICY_MANAGER)),true)
+ifneq ($(USE_LEGACY_AUDIO_POLICY), 1)
+ifeq ($(USE_CUSTOM_AUDIO_POLICY), 1)
 
-LOCAL_PATH := $(call my-dir)
+LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
-LOCAL_SRC_FILES := AudioPolicyManager.cpp
+LOCAL_SRC_FILES:= \
+    AudioPolicyManager.cpp
 
 LOCAL_SHARED_LIBRARIES := \
     libcutils \
     libutils \
     liblog
 
+LOCAL_C_INCLUDES := \
+    $(TOPDIR)frameworks/av/services/audiopolicy \
+
 LOCAL_STATIC_LIBRARIES := \
     libmedia_helper
 
-LOCAL_WHOLE_STATIC_LIBRARIES := \
-    libaudiopolicy_legacy
-
-LOCAL_MODULE := audio_policy.$(TARGET_BOARD_PLATFORM)
-LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
-LOCAL_MODULE_TAGS := optional
-
-ifeq ($(strip $(AUDIO_FEATURE_ENABLED_FM)),true)
-LOCAL_CFLAGS += -DAUDIO_EXTN_FM_ENABLED
-endif
-ifeq ($(strip $(AUDIO_FEATURE_ENABLED_PROXY_DEVICE)),true)
-LOCAL_CFLAGS += -DAUDIO_EXTN_AFE_PROXY_ENABLED
-endif
-ifeq ($(strip $(AUDIO_FEATURE_ENABLED_INCALL_MUSIC)),true)
-LOCAL_CFLAGS += -DAUDIO_EXTN_INCALL_MUSIC_ENABLED
-endif
-
-
-ifeq ($(strip $(TARGET_BOARD_PLATFORM)),msm8916)
-LOCAL_CFLAGS += -DVOICE_CONCURRENCY
-LOCAL_CFLAGS += -DWFD_CONCURRENCY
-endif
-
-
+LOCAL_MODULE:= libaudiopolicymanager
 
 include $(BUILD_SHARED_LIBRARY)
 
+endif
 endif
