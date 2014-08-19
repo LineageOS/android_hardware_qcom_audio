2471348a8426ee1671aa32a410e271717d0ffc26
Mingming Yin <mingming@codeaurora.org>
2014-08-18 17:06:00 -0700

    hal: fix speaker protection macro
    - use snd_device instead of SND_DEVICE_OUT_SPEAKER
      for audio_extn_get_spkr_prot_snd_device(snd_device)
      to avoid overwriting snd_device.
    
    Change-Id: I9e8a93610292351c9877fd5500511366c6da4048

--- hal/audio_extn/audio_extn.h
+++ hal/audio_extn/audio_extn.h
@@ -192,7 +192,7 @@ int32_t audio_extn_read_xml(struct audio_device *adev, uint32_t mixer_card,
 #define audio_extn_spkr_prot_stop_processing()     (0)
 #define audio_extn_spkr_prot_is_enabled() (false)
 #define audio_extn_spkr_prot_get_acdb_id(snd_device)         (-EINVAL)
-#define audio_extn_get_spkr_prot_snd_device(snd_device) (SND_DEVICE_OUT_SPEAKER)
+#define audio_extn_get_spkr_prot_snd_device(snd_device) (snd_device)
 #else
 void audio_extn_spkr_prot_init(void *adev);
 int audio_extn_spkr_prot_start_processing(snd_device_t snd_device);
