01418bf407aad26338bf07ca6c02808ea77b9e06
Ravi Kumar Alamanda <ralama@codeaurora.org>
2014-08-20 16:24:38 -0700

    hal: avoid usage of extern functions and tables
    If there is any difference in the signature of a function declared
    as extern, it will not be reported by the compiler and may result
    in unexpected results when executed.
    All the API functions should be declared in a header file.
    
    Change-Id: I89662e23da8118c3a9eac728b389498ed52e19c2

--- hal/audio_hw.h
+++ hal/audio_hw.h
@@ -58,7 +58,7 @@ typedef int snd_device_t;
  * Each usecase is mapped to a specific PCM device.
  * Refer to pcm_device_table[].
  */
-typedef enum {
+enum {
     USECASE_INVALID = -1,
     /* Playback usecases */
     USECASE_AUDIO_PLAYBACK_DEEP_BUFFER = 0,
@@ -106,7 +106,9 @@ typedef enum {
     USECASE_AUDIO_RECORD_AFE_PROXY,
 
     AUDIO_USECASE_MAX
-} audio_usecase_t;
+};
+
+const char * const use_case_table[AUDIO_USECASE_MAX];
 
 #define ARRAY_SIZE(a) (sizeof(a) / sizeof((a)[0]))
 
--- hal/voice.h
+++ hal/voice.h
@@ -42,6 +42,7 @@ struct audio_device;
 struct str_parms;
 struct stream_in;
 struct stream_out;
+typedef int audio_usecase_t;
 
 struct call_state {
     int current;
@@ -70,6 +71,9 @@ enum {
     INCALL_REC_DOWNLINK,
     INCALL_REC_UPLINK_AND_DOWNLINK,
 };
+
+int voice_start_usecase(struct audio_device *adev, audio_usecase_t usecase_id);
+int voice_stop_usecase(struct audio_device *adev, audio_usecase_t usecase_id);
 
 int voice_start_call(struct audio_device *adev);
 int voice_stop_call(struct audio_device *adev);
