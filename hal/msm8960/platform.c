11bbed6c0f760018d83bcd778466b4c9622faf7d
Ethan Chen <intervigil@gmail.com>
2014-09-05 13:01:26 -0700

    audio: Extend platform parser to allow device name aliasing
    * Supported sections now include device names
    * This allows for aliasing device names to a custom name
    
    Change-Id: I880e90a7e887f020517d89ba276199c700c0eeae

--- hal/msm8960/platform.c
+++ hal/msm8960/platform.c
@@ -209,7 +209,7 @@ static int pcm_device_table[AUDIO_USECASE_MAX][2] = {
 };
 
 /* Array to store sound devices */
-static const char * const device_table[SND_DEVICE_MAX] = {
+static char * device_table[SND_DEVICE_MAX] = {
     [SND_DEVICE_NONE] = "none",
     /* Playback sound devices */
     [SND_DEVICE_OUT_HANDSET] = "handset",
@@ -2581,4 +2581,19 @@ int platform_set_device_params(struct stream_out *out __unused,
                                   int param __unused, int value __unused)
 {
     return 0;
+}
+
+int platform_set_snd_device_name(snd_device_t device, const char *name)
+{
+    int ret = 0;
+
+    if ((device < SND_DEVICE_MIN) || (device >= SND_DEVICE_MAX)) {
+        ALOGE("%s:: Invalid snd_device = %d", __func__, device);
+        ret = -EINVAL;
+        goto done;
+    }
+
+    device_table[device] = strdup(name);
+done:
+    return ret;
 }
--- hal/msm8974/platform.c
+++ hal/msm8974/platform.c
@@ -267,7 +267,7 @@ static int pcm_device_table[AUDIO_USECASE_MAX][2] = {
 };
 
 /* Array to store sound devices */
-static const char * const device_table[SND_DEVICE_MAX] = {
+static char * device_table[SND_DEVICE_MAX] = {
     [SND_DEVICE_NONE] = "none",
     /* Playback sound devices */
     [SND_DEVICE_OUT_HANDSET] = "handset",
@@ -3640,5 +3640,20 @@ int platform_set_device_params(struct stream_out *out, int param, int value)
     mixer_ctl_set_array(ctl, set_values, ARRAY_SIZE(set_values));
 
 end:
+    return ret;
+}
+
+int platform_set_snd_device_name(snd_device_t device, const char *name)
+{
+    int ret = 0;
+
+    if ((device < SND_DEVICE_MIN) || (device >= SND_DEVICE_MAX)) {
+        ALOGE("%s:: Invalid snd_device = %d", __func__, device);
+        ret = -EINVAL;
+        goto done;
+    }
+
+    device_table[device] = strdup(name);
+done:
     return ret;
 }
