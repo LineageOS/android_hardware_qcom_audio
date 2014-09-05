f1ae5ef9d1b2a1b18744ef79a693b11533ca1d54
Ravi Kumar Alamanda <ralama@codeaurora.org>
2014-09-05 16:14:17 -0700

    hal: fix unused param warnings
    Fix unused param warnings.
    Take care of coding style issues.
    Remove unwanted code to set acdb_settings.
    Fix compilation issues on 8960 platform.
    
    Change-Id: Ifc72472b487abee83ff85e21fd324cee938973e9

--- hal/voice_extn/voice_extn.c
+++ hal/voice_extn/voice_extn.c
@@ -504,7 +504,7 @@ done:
     return ret;
 }
 
-int get_all_call_states_str(const struct audio_device *adev,
+static int get_all_call_states_str(const struct audio_device *adev,
                             char *value)
 {
     int ret = 0;
@@ -549,7 +549,7 @@ void voice_extn_get_parameters(const struct audio_device *adev,
         }
         str_parms_add_str(reply, AUDIO_PARAMETER_KEY_ALL_CALL_STATES, value);
     }
-    voice_extn_compress_voip_get_parameters(adev, query, reply);
+    voice_extn_compress_voip_get_parameters(query, reply);
 
     str = str_parms_to_str(reply);
     ALOGV_IF(str != NULL, "%s: exit: returns \"%s\"", __func__, str);
