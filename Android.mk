5c943cfd19eacb113a654eaf71f69446ab757216
Ethan Chen <intervigil@gmail.com>
2014-10-03 15:01:12 -0700

    audio: Use project pathmap
    Change-Id: I908f080105aad8ff686e409fd04ff730f09a86a4

--- Android.mk
+++ Android.mk
@@ -1,3 +1,5 @@
+ifeq ($(call my-dir),$(call project-path-for,qcom-audio))
+
 # TODO:  Find a better way to separate build configs for ADP vs non-ADP devices
 ifneq ($(TARGET_BOARD_AUTO),true)
   ifneq ($(filter msm8960 msm8226 msm8x26 msm8x84 msm8084 msm8992 msm8994 msm8996 msm8909 msm8952 msm8998 sdm845 sdm710,$(TARGET_BOARD_PLATFORM)),)
@@ -13,4 +15,6 @@ ifneq ($(TARGET_BOARD_AUTO),true)
       include $(MY_LOCAL_PATH)/post_proc/Android.mk
     endif
   endif
+endif
+
 endif
