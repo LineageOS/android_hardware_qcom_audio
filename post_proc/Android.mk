1fd7550e13b0c9e3ea7fa6898ca12bc628531d30
Ethan Chen <intervigil@gmail.com>
2014-10-03 15:01:12 -0700

    audio: Use project pathmap
    Change-Id: I908f080105aad8ff686e409fd04ff730f09a86a4

--- post_proc/Android.mk
+++ post_proc/Android.mk
@@ -65,7 +65,7 @@ LOCAL_MODULE_OWNER := qcom
 LOCAL_PROPRIETARY_MODULE := true
 
 LOCAL_C_INCLUDES := \
-        hardware/qcom/audio/hal \
+	$(call project-path-for,qcom-audio)/hal \
 	$(call include-path-for, audio-effects)
 
 LOCAL_HEADER_LIBRARIES += libhardware_headers
