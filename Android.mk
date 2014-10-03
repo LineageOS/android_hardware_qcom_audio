2dec5b8df92bd7cf4c22b0ca394d6097d854f8a6
Ethan Chen <intervigil@gmail.com>
2014-10-03 15:01:12 -0700

    audio: Use project pathmap
    Change-Id: I908f080105aad8ff686e409fd04ff730f09a86a4

--- Android.mk
+++ Android.mk
@@ -1,3 +1,5 @@
+ifeq ($(call my-dir),$(call project-path-for,qcom-audio)/default)
+
 ifneq ($(filter msm8960 msm8226 msm8x26 msm8974 msm8x74 msm8x84 msm8084,$(TARGET_BOARD_PLATFORM)),)
 
 MY_LOCAL_PATH := $(call my-dir)
@@ -9,6 +11,8 @@ include $(MY_LOCAL_PATH)/hal/Android.mk
 include $(MY_LOCAL_PATH)/voice_processing/Android.mk
 include $(MY_LOCAL_PATH)/visualizer/Android.mk
 include $(MY_LOCAL_PATH)/post_proc/Android.mk
+endif
+
 endif
 
 endif
