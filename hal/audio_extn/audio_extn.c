02c133d999f9cb9e861ef06f4534d511932b1f5c
Ashish Jain <ashishj@codeaurora.org>
2014-10-16 13:56:28 +0530

    hal: Update DS2 implementation to support ACDB based license mechanism
    -Add ACDB based licenese mechanism for Dolby DS2 implementation.
    -Add device to back end mapping for MSM8916
    
    Change-Id: I0de4bd67fbfc26c21c4751e32ae2c45278a226d0

--- hal/audio_extn/audio_extn.c
+++ hal/audio_extn/audio_extn.c
@@ -461,6 +461,7 @@ void audio_extn_set_parameters(struct audio_device *adev,
    audio_extn_listen_set_parameters(adev, parms);
    audio_extn_hfp_set_parameters(adev, parms);
    audio_extn_ddp_set_parameters(adev, parms);
+   audio_extn_ds2_set_parameters(adev, parms);
    audio_extn_customstereo_set_parameters(adev, parms);
 }
 
