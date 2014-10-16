22582e755cc01b823638cb316dcaa8bd59b633b2
Banajit Goswami <bgoswami@codeaurora.org>
2014-10-15 18:29:34 -0700

    hal: add support for thulium target
    Adding thulium target support in makefile
    
    Change-Id: Ibb33e9545a511ea85fc29cf56f72cd0b5d368e7e

--- hal/Android.mk
+++ hal/Android.mk
@@ -8,7 +8,7 @@ LOCAL_ARM_MODE := arm
 
 AUDIO_PLATFORM := $(TARGET_BOARD_PLATFORM)
 
-ifneq ($(filter msm8974 msm8226 msm8610 apq8084 msm8994,$(TARGET_BOARD_PLATFORM)),)
+ifneq ($(filter msm8974 msm8226 msm8610 apq8084 msm8994 thulium,$(TARGET_BOARD_PLATFORM)),)
   # B-family platform uses msm8974 code base
   AUDIO_PLATFORM = msm8974
   MULTIPLE_HW_VARIANTS_ENABLED := true
@@ -23,6 +23,9 @@ ifneq ($(filter apq8084,$(TARGET_BOARD_PLATFORM)),)
 endif
 ifneq ($(filter msm8994,$(TARGET_BOARD_PLATFORM)),)
   LOCAL_CFLAGS := -DPLATFORM_MSM8994
+endif
+ifneq ($(filter thulium,$(TARGET_BOARD_PLATFORM)),)
+  LOCAL_CFLAGS := -DPLATFORM_THULIUM
 endif
 endif
 
