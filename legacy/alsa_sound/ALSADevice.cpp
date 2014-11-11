CyanogenMod/android_hardware_qcom_audio-caf

alsa_sound: Support ES325 2Mic and Samsung CSD client
 * The dual mic feature on the Audience ES325 needs to be enabled via
   csd-client on the Galaxy S4. Add support for this.
 * Samsung is using a non-standard name for the functions in csd-client.
   Add support for this.

Change-Id: I764ead9ea17729055da701a423edec97d96b8319

alsa_sound/ALSADevice.cpp
@@ -198,6 +198,7 @@ static int adjustFlagsForCsd(int flags, const char *rxDevice)
            is the only adaptive mode CSD Client cares about */
         adjustedFlags &= ~(ANC_FLAG);
     }
+
     ALOGD("%s: current Rx device: %s, flags: %x, adjustedFlags: %x",
             __FUNCTION__, rxDevice, flags, adjustedFlags);
     return adjustedFlags;
@@ -603,6 +604,9 @@ void ALSADevice::switchDevice(alsa_handle_t *handle, uint32_t devices, uint32_t 
         if (csd_disable_device == NULL) {
             ALOGE("csd_client_disable_device is NULL");
         } else {
+#ifdef USE_ES325_2MIC
+            setMixerControl("ES325 2Mic Enable", 0, 0);
+#endif
             err = csd_disable_device();
             if (err < 0)
             {
@@ -757,9 +761,21 @@ void ALSADevice::switchDevice(alsa_handle_t *handle, uint32_t devices, uint32_t 
             if (csd_enable_device == NULL) {
                 ALOGE("csd_client_enable_device is NULL");
             } else {
+                int tmp_tx_id = tx_dev_id;
+
+#ifdef USE_ES325_2MIC
+                if (tx_dev_id == 4) {
+                    tmp_tx_id = 34;
+                    setMixerControl("VEQ Enable", 1, 0);
+                    setMixerControl("ES325 2Mic Enable", 1, 0);
+                } else {
+                    setMixerControl("ES325 2Mic Enable", 0, 0);
+                }
+#endif
+
                 int adjustedFlags = adjustFlagsForCsd(mDevSettingsFlag,
                         mCurRxUCMDevice);
-                err = csd_enable_device(rx_dev_id, tx_dev_id, adjustedFlags);
+                err = csd_enable_device(rx_dev_id, tmp_tx_id, adjustedFlags);
                 if (err < 0)
                 {
                     ALOGE("csd_client_disable_device failed, error %d", err);
@@ -2857,8 +2873,20 @@ void  ALSADevice::setCsdHandle(void* handle)
     csd_slow_talk = (int (*)(uint32_t, uint8_t))::dlsym(mcsd_handle,
                                                         "csd_client_slow_talk");
 #else
-    csd_start_voice = (int (*)())::dlsym(mcsd_handle,"csd_client_start_voice");
-    csd_stop_voice = (int (*)())::dlsym(mcsd_handle,"csd_client_stop_voice");
+    csd_start_voice = (int (*)())::dlsym(mcsd_handle,
+#ifdef SAMSUNG_CSDCLIENT
+            "csd_client_start_voice_og"
+#else
+            "csd_client_start_voice"
+#endif
+            );
+    csd_stop_voice = (int (*)())::dlsym(mcsd_handle,
+#ifdef SAMSUNG_CSDCLIENT
+            "csd_client_stop_voice_og"
+#else
+            "csd_client_stop_voice"
+#endif
+            );
     csd_volume = (int (*)(int))::dlsym(mcsd_handle,"csd_client_volume");
     csd_mic_mute = (int (*)(int))::dlsym(mcsd_handle,"csd_client_mic_mute");
     csd_wide_voice = (int (*)(uint8_t))::dlsym(mcsd_handle,"csd_client_wide_voice");

LineageOS/android_hardware_qcom_audio

legacy/alsa_sound/ALSADevice.cpp
@@ -195,6 +195,7 @@ static int adjustFlagsForCsd(int flags, const char *rxDevice)
            is the only adaptive mode CSD Client cares about */
         adjustedFlags &= ~(ANC_FLAG);
     }
+
     ALOGD("%s: current Rx device: %s, flags: %x, adjustedFlags: %x",
             __FUNCTION__, rxDevice, flags, adjustedFlags);
     return adjustedFlags;
@@ -625,6 +626,9 @@ void ALSADevice::switchDevice(alsa_handle_t *handle, uint32_t devices, uint32_t 
         if (csd_disable_device == NULL) {
             ALOGE("csd_client_disable_device is NULL");
         } else {
+#ifdef USE_ES325_2MIC
+            setMixerControl("ES325 2Mic Enable", 0, 0);
+#endif
             err = csd_disable_device();
             if (err < 0)
             {
@@ -801,9 +805,21 @@ void ALSADevice::switchDevice(alsa_handle_t *handle, uint32_t devices, uint32_t 
         if (csd_enable_device == NULL) {
             ALOGE("csd_client_enable_device is NULL");
         } else {
+            int tmp_tx_id = tx_dev_id;
+
+#ifdef USE_ES325_2MIC
+            if (tx_dev_id == 4) {
+                tmp_tx_id = 34;
+                setMixerControl("VEQ Enable", 1, 0);
+                setMixerControl("ES325 2Mic Enable", 1, 0);
+            } else {
+                setMixerControl("ES325 2Mic Enable", 0, 0);
+            }
+#endif
+
             int adjustedFlags = adjustFlagsForCsd(mDevSettingsFlag,
                     mCurRxUCMDevice);
-            err = csd_enable_device(rx_dev_id, tx_dev_id, adjustedFlags);
+            err = csd_enable_device(rx_dev_id, tmp_tx_dev_id, adjustedFlags);
             if (err < 0)
             {
                 ALOGE("csd_enable_device failed, error %d", err);
@@ -2874,9 +2890,19 @@ void  ALSADevice::setCsdHandle(void* handle)
     csd_enable_device_config = (int (*)(int,int))::dlsym(mcsd_handle,
                                                     "csd_client_enable_device_config");
     csd_start_voice = (int (*)(uint32_t))::dlsym(mcsd_handle,
-                                                 "csd_client_start_voice");
-    csd_stop_voice = (int (*)(uint32_t))::dlsym(mcsd_handle,
-                                                "csd_client_stop_voice");
+#ifdef SAMSUNG_CSDCLIENT
+            "csd_client_start_voice_og"
+#else
+            "csd_client_start_voice"
+#endif
+            );
+    csd_stop_voice = (int (*)())::dlsym(mcsd_handle,
+#ifdef SAMSUNG_CSDCLIENT
+            "csd_client_stop_voice_og"
+#else
+            "csd_client_stop_voice"
+#endif
+            );
     csd_volume = (int (*)(uint32_t, int))::dlsym(mcsd_handle,
                                                  "csd_client_volume");
     csd_mic_mute = (int (*)(uint32_t, int))::dlsym(mcsd_handle,
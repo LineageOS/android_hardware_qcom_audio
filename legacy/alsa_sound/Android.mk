5cb3501d69491426d472899d5a14aea50db23ccd
Steve Kondik <shade@chemlab.org>
2013-05-31 16:27:31 -0700

    alsa_sound: Support ES325 2Mic and Samsung CSD client
     * The dual mic feature on the Audience ES325 needs to be enabled via
       csd-client on the Galaxy S4. Add support for this.
     * Samsung is using a non-standard name for the functions in csd-client.
       Add support for this.
    
    Change-Id: I764ead9ea17729055da701a423edec97d96b8319

--- legacy/alsa_sound/Android.mk
+++ legacy/alsa_sound/Android.mk
@@ -93,6 +93,14 @@ ifeq ($(BOARD_HAVE_DOCK_USBAUDIO),true)
   LOCAL_CFLAGS += -DDOCK_USBAUDIO_ENABLED
 endif
 
+ifeq ($(BOARD_HAVE_AUDIENCE_ES325_2MIC),true)
+  LOCAL_CFLAGS += -DUSE_ES325_2MIC
+endif
+
+ifeq ($(BOARD_HAVE_SAMSUNG_CSDCLIENT),true)
+  LOCAL_CFLAGS += -DSAMSUNG_CSDCLIENT
+endif
+
 LOCAL_SRC_FILES := \
   AudioHardwareALSA.cpp         \
   AudioStreamOutALSA.cpp        \
