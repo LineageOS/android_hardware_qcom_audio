f9c7405417ff3360e660fa56d3e5bba4aae00f71
Ethan Chen <intervigil@gmail.com>
2014-11-12 13:01:26 -0800

    audio: Make CAF audio HALS platform specific
    Change-Id: Ie4d7a1a872c41269a280271bcd4cb14ee37cd050

--- Android.mk
+++ Android.mk
@@ -1,4 +1,4 @@
-ifeq ($(call my-dir),$(call project-path-for,qcom-audio)/legacy)
+ifeq ($(call my-dir),$(call project-path-for,qcom-audio)/$(TARGET_BOARD_PLATFORM))
 
 ifneq ($(filter msm8960 msm8226 msm8x26 msm8974 msm8x74,$(TARGET_BOARD_PLATFORM)),)
 
