e645be83ac629e6bde3ba00737199a7430254fd3
Ethan Chen <intervigil@gmail.com>
2014-11-12 13:01:35 -0800

    Revert "audio: Only allow legacy HAL"
    This reverts commit 949e8ddbcfe6f6663ae5f41ae4cde413592c469b.
    
    Change-Id: I752e9976ddf16ec53b4179194b42412f8bcf5f87

--- Android.mk
+++ Android.mk
@@ -6,6 +6,8 @@ MY_LOCAL_PATH := $(call my-dir)
 
 ifeq ($(BOARD_USES_LEGACY_ALSA_AUDIO),true)
 include $(MY_LOCAL_PATH)/legacy/Android.mk
+else
+include $(MY_LOCAL_PATH)/hal/Android.mk
 endif
 
 endif
