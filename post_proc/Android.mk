b3253ff33042f055021e665e0dfdf4b874c40932
Chirayu Desai <chirayudesai1@gmail.com>
2014-11-13 09:08:13 +0530

    audio: Use kernel headers
    Change-Id: I7a238b7c105d58eb6ec7794ec7a6af37f63c4cb0

--- post_proc/Android.mk
+++ post_proc/Android.mk
@@ -28,6 +28,9 @@ LOCAL_C_INCLUDES := \
 	external/tinyalsa/include \
 	$(call include-path-for, audio-effects)
 
+LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
+LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
+
 include $(BUILD_SHARED_LIBRARY)
 endif
 
@@ -54,6 +57,9 @@ LOCAL_MODULE:= libvolumelistener
 
 LOCAL_C_INCLUDES := \
 	$(call include-path-for, audio-effects)
+
+LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
+LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
 
 include $(BUILD_SHARED_LIBRARY)
 
