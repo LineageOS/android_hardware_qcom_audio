c113dc8c10d3220b976ee8e8a8c73454d92db090
Ethan Chen <intervigil@gmail.com>
2014-11-18 14:03:56 -0800

    audio: Use direct project pathmap
    Change-Id: I94be5eaddd7c87eb2357ac3c372d50bc0ccfcf5d

--- Android.mk
+++ Android.mk
@@ -1,4 +1,4 @@
-ifeq ($(call my-dir),$(call project-path-for,qcom-audio)/default)
+ifeq ($(call my-dir),$(call project-path-for,qcom-audio))
 
 ifneq ($(filter msm8960 msm8226 msm8x26 msm8974 msm8x74 msm8x84 msm8084 msm8992 msm8994,$(TARGET_BOARD_PLATFORM)),)
 
