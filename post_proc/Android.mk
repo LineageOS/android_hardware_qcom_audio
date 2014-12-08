3840f73dd3b76bdeb13cd1f87b49ad8db7eb44b9
Steve Kondik <steve@cyngn.com>
2014-12-08 04:02:28 -0800

    hal: Invert standard build flags
     * Always include these unless explicitly negated
    
    Change-Id: I22daf022570785a191caea95a0735c90a763f8ff

--- post_proc/Android.mk
+++ post_proc/Android.mk
@@ -3,7 +3,7 @@ LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
 
-ifeq ($(strip $(AUDIO_FEATURE_ENABLED_PROXY_DEVICE)),true)
+ifneq ($(strip $(AUDIO_FEATURE_ENABLED_PROXY_DEVICE)),false)
     LOCAL_CFLAGS += -DAFE_PROXY_ENABLED
 endif
 
