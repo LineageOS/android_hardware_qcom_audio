ea6d50be12983c4fe3dbc80ebee9008942e4a7ee
NoSpamDan <nospamdan11@gmail.com>
2015-01-11 05:38:59 +0100

    audio-caf: Add msm8660 as a valid target
    Change-Id: I38bde6ca7c6f431b50db858e27b3ff9ffa258f95

--- Android.mk
+++ Android.mk
@@ -1,6 +1,6 @@
 ifeq ($(call my-dir),$(call project-path-for,qcom-audio))
 
-ifneq ($(filter msm8960 msm8226 msm8x26 msm8974 msm8x74,$(TARGET_BOARD_PLATFORM)),)
+ifneq ($(filter msm8960 msm8660 msm8226 msm8x26 msm8974 msm8x74,$(TARGET_BOARD_PLATFORM)),)
 
 MY_LOCAL_PATH := $(call my-dir)
 
