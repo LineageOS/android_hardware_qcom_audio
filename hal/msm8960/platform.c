8f1cebea5cf59ea16237aa02bf4bbcdaddf8ffe8
Arne Coucheron <arco68@gmail.com>
2015-01-21 08:06:13 +0100

    hal: Use old API for setting voice volume and mute states
    A family kernels doesn't support the new API.
    
    Change-Id: Iaa0d7c0641bfb0cc66f70bef958e860b38a0864a

--- hal/msm8960/platform.c
+++ hal/msm8960/platform.c
@@ -1183,7 +1183,8 @@ int platform_set_voice_volume(void *platform, int volume)
     struct platform_data *my_data = (struct platform_data *)platform;
     struct audio_device *adev = my_data->adev;
     struct mixer_ctl *ctl;
-    const char *mixer_ctl_name = "Voice Rx Gain";
+    //const char *mixer_ctl_name = "Voice Rx Gain";
+    const char *mixer_ctl_name = "Voice Rx Volume";
     int vol_index = 0, ret = 0;
     uint32_t set_values[ ] = {0,
                               ALL_SESSION_VSID,
@@ -1201,8 +1202,10 @@ int platform_set_voice_volume(void *platform, int volume)
               __func__, mixer_ctl_name);
         return -EINVAL;
     }
-    ALOGV("Setting voice volume index: %d", set_values[0]);
-    mixer_ctl_set_array(ctl, set_values, ARRAY_SIZE(set_values));
+    //ALOGV("Setting voice volume index: %d", set_values[0]);
+    //mixer_ctl_set_array(ctl, set_values, ARRAY_SIZE(set_values));
+    ALOGV("Setting voice volume index: %d", vol_index);
+    mixer_ctl_set_value(ctl, 0, vol_index);
 
     if (my_data->csd != NULL) {
         ret = my_data->csd->volume(ALL_SESSION_VSID, volume);
@@ -1232,7 +1235,8 @@ int platform_set_mic_mute(void *platform, bool state)
         return -EINVAL;
     }
     ALOGV("Setting voice mute state: %d", state);
-    mixer_ctl_set_array(ctl, set_values, ARRAY_SIZE(set_values));
+    //mixer_ctl_set_array(ctl, set_values, ARRAY_SIZE(set_values));
+    mixer_ctl_set_value(ctl, 0, state);
 
     if (my_data->csd != NULL) {
         ret = my_data->csd->mic_mute(ALL_SESSION_VSID, state);
@@ -1276,7 +1280,8 @@ int platform_set_device_mute(void *platform, bool state, char *dir)
 
     ALOGV("%s: Setting device mute state: %d, mixer ctrl:%s",
           __func__,state, mixer_ctl_name);
-    mixer_ctl_set_array(ctl, set_values, ARRAY_SIZE(set_values));
+    //mixer_ctl_set_array(ctl, set_values, ARRAY_SIZE(set_values));
+    mixer_ctl_set_value(ctl, 0, state);
 
     return ret;
 }
