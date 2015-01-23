c0e2d91814bd1a35550817d22dee8d2088bf5cf2
Arne Coucheron <arco68@gmail.com>
2015-01-23 08:38:16 +0100

    hal: Adjust period size and count for legacy kernels
    Standard A family kernels have a default playback period size
    of 2048, so deep buffer period size need to be 512, because
    512 * 4 = 2048
    
    Any higher sizes will require kernel changes.
    
    Also, with higher values there's noticeably lag when adjusting
    the volume.
    
    Increase period count as well to match A family kernels.
    
    Change-Id: I65247bbebb1065edf5a8b6fdfea6532209eb6cf5

--- hal/msm8960/platform.h
+++ hal/msm8960/platform.h
@@ -154,9 +154,9 @@ enum {
  * We should take care of returning proper size when AudioFlinger queries for
  * the buffer size of an input/output stream
  */
-#define DEEP_BUFFER_OUTPUT_PERIOD_SIZE 960
-#define DEEP_BUFFER_OUTPUT_PERIOD_COUNT 4
-#define LOW_LATENCY_OUTPUT_PERIOD_SIZE 240
+#define DEEP_BUFFER_OUTPUT_PERIOD_SIZE 512
+#define DEEP_BUFFER_OUTPUT_PERIOD_COUNT 8
+#define LOW_LATENCY_OUTPUT_PERIOD_SIZE 256
 #define LOW_LATENCY_OUTPUT_PERIOD_COUNT 2
 
 #define HDMI_MULTI_PERIOD_SIZE  336
