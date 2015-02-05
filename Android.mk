d72422edd79ee882e14eb31f272ade0535f48072
Arne Coucheron <arco68@gmail.com>
2015-02-05 07:47:47 +0100

    hal: Remove hal_mpq
    Change-Id: I2f2e5ad220beb1889c366baefc2462a556f2e2f3

--- Android.mk
+++ Android.mk
@@ -1,17 +1,13 @@
 ifeq ($(call my-dir),$(call project-path-for,qcom-audio))
 
-ifneq ($(filter mpq8092 msm8960 msm8226 msm8x26 msm8610 msm8974 msm8x74 apq8084,$(TARGET_BOARD_PLATFORM)),)
+ifneq ($(filter msm8960 msm8226 msm8x26 msm8610 msm8974 msm8x74 apq8084,$(TARGET_BOARD_PLATFORM)),)
 
 MY_LOCAL_PATH := $(call my-dir)
 
 ifeq ($(BOARD_USES_LEGACY_ALSA_AUDIO),true)
 include $(MY_LOCAL_PATH)/legacy/Android.mk
 else
-ifneq ($(filter mpq8092,$(TARGET_BOARD_PLATFORM)),)
-include $(MY_LOCAL_PATH)/hal_mpq/Android.mk
-else
 include $(MY_LOCAL_PATH)/hal/Android.mk
-endif
 include $(MY_LOCAL_PATH)/voice_processing/Android.mk
 include $(MY_LOCAL_PATH)/mm-audio/Android.mk
 include $(MY_LOCAL_PATH)/policy_hal/Android.mk
