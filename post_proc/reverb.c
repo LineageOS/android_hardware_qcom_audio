57ef87e9f1ab2c8bf00b7250b324c2c7fd82990e
Weiyin Jiang <wjiang@codeaurora.org>
2015-03-23 00:54:14 +0800

    post_proc: add support for virtualizer capability query
    Add extra virtualizer interfaces to keep align with aosp design.
    Extended capabilities include:
    - Stub interface to allow querying speaker angles.
    - Force set output device as specific virtualize mode.
    - Query virtualize mode
    
    Stub function remains to be refined in sync with upcoming aosp
    changes.
    
    Change-Id: I3316f6d944db1c9954eda7643a5ce433defa1a6c
    CRs-Fixed: 810294

--- post_proc/reverb.c
+++ post_proc/reverb.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2013-2015, The Linux Foundation. All rights reserved.
  * Not a Contribution.
  *
  * Copyright (C) 2013 The Android Open Source Project
@@ -433,7 +433,7 @@ int reverb_get_parameter(effect_context_t *context, effect_param_t *p,
 }
 
 int reverb_set_parameter(effect_context_t *context, effect_param_t *p,
-                         uint32_t size)
+                         uint32_t size __unused)
 {
     reverb_context_t *reverb_ctxt = (reverb_context_t *)context;
     int voffset = ((p->psize - 1) / sizeof(int32_t) + 1) * sizeof(int32_t);
@@ -602,7 +602,7 @@ int reverb_start(effect_context_t *context, output_context_t *output)
     return 0;
 }
 
-int reverb_stop(effect_context_t *context, output_context_t *output)
+int reverb_stop(effect_context_t *context, output_context_t *output __unused)
 {
     reverb_context_t *reverb_ctxt = (reverb_context_t *)context;
 
