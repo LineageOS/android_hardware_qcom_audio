4f05e0711a67b5c85d95e5b08145f284d1357413
Ethan Chen <intervigil@gmail.com>
2015-03-26 22:17:05 -0700

    alsa_sound: Fix compilation for 5.1
    Change-Id: If40c1835a64f48f07d81d87cdb8dec4a92fff00a

--- legacy/alsa_sound/AudioHardwareALSA.cpp
+++ legacy/alsa_sound/AudioHardwareALSA.cpp
@@ -3337,6 +3337,31 @@ bool  AudioHardwareALSA::suspendPlaybackOnExtOut_l(uint32_t activeUsecase) {
     return NO_ERROR;
 }
 
+status_t AudioHardwareALSA::setMasterMute(bool muted) {
+    return INVALID_OPERATION;
+}
+
+int AudioHardwareALSA::createAudioPatch(unsigned int num_sources,
+        const struct audio_port_config *sources,
+        unsigned int num_sinks,
+        const struct audio_port_config *sinks,
+        audio_patch_handle_t *handle) {
+    return INVALID_OPERATION;
+}
+
+int AudioHardwareALSA::releaseAudioPatch(audio_patch_handle_t handle) {
+    return INVALID_OPERATION;
+}
+
+int AudioHardwareALSA::getAudioPort(struct audio_port *port) {
+    return INVALID_OPERATION;
+}
+
+int AudioHardwareALSA::setAudioPortConfig(
+        const struct audio_port_config *config) {
+    return INVALID_OPERATION;
+}
+
 }       // namespace android_audio_legacy
 
 #ifdef AUXPCM_BT_ENABLED
