ca67a02c51e37fb14e19e885d662362c7434636c
Sharad Sangle <assangle@codeaurora.org>
2015-05-28 16:15:16 +0530

    policy-hal: add custom audio policy
    - this change contains all the fixes went in
      audio policy made by QTI as below
    
     audio_policyhal: SSR support for compress offload
     Change-Id: Idc8f6690d7c06883dbfcf55b073d4456a8c27e5c
    
     audio: Fix playback of AAC clips in offload mode instead of deep buffer
     Change-Id: Ib946830bc2f6b9cec33c8f0943910e3aeeb6fa6e
    
     audio: primary desc check for sonification_respectful
     Change-Id: I5d7aef3b29791823c277452e71e0cd43f91b2ddd
    
     audio_policy: Fix for mute count mismatch for sonification strategy
     Change-Id: Id4f328e2f9fc6820cecfbd73df11c871f8587aad
    
     audio: execute setVoiceVolume for USB device during inCall usecase
     Change-Id: Ia4917807103d1cf406a03e1d6d3dada470401737
    
     audio_policy: handle in call sonification for attached outputs of duplicating output
     Change-Id: I91968b33c4d93b13aae421654b2740c46269d55e
    
     audio: Fix for Sound from handset leaks to speaker
     Change-Id: Id2fd20de7a279f74e38a9aefe91e17b36cc9da6f
    
     audio: Fix media server crash at the start of voice call
     Change-Id: Ibe2d19acbed2ca44ab193ae0b613fae899f02ebb
    
     audio_policy: handle incall sound notifications on non primary outputs
     Change-Id: I2eec3872a2dcd10750ac20f69791a5bb51b37d8b
    
     Audiopolicymanager: handle incall sonification without checking output refCount
     Change-Id: I0eab939b32fe47388e2515ab56154d540e9df55c
    
     audio: use FLAG_FAST for non-music stream on WFD
     Change-Id: Id2f5965c8f5623646b2adb2aa892a9929f3bc5bb
    
     audiopolicy: optimize voice call start latency
     Chnage-Id: I77cb2ddb5a9154ffd2ac1620351992686e580f8a
    
    Change-Id: I8a5991f09063f8e00b9a1308c3307c239f3ce487

--- policy_hal/Android.mk
+++ policy_hal/Android.mk
@@ -1,48 +1,35 @@
-ifeq ($(strip $(BOARD_USES_EXTN_AUDIO_POLICY_MANAGER)),true)
-
+ifneq ($(USE_LEGACY_AUDIO_POLICY), 1)
+ifeq ($(USE_CUSTOM_AUDIO_POLICY), 1)
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := AudioPolicyManager.cpp
 
+LOCAL_C_INCLUDES := $(TOPDIR)frameworks/av/services \
+                    $(TOPDIR)frameworks/av/services/audioflinger \
+                    $(call include-path-for, audio-effects) \
+                    $(call include-path-for, audio-utils) \
+                    $(TOPDIR)frameworks/av/services/audiopolicy/common/include \
+                    $(TOPDIR)frameworks/av/services/audiopolicy/engine/interface \
+                    $(TOPDIR)frameworks/av/services/audiopolicy \
+                    $(TOPDIR)frameworks/av/services/audiopolicy/common/managerdefinitions/include \
+                    $(call include-path-for, avextension)
+
+
 LOCAL_SHARED_LIBRARIES := \
     libcutils \
     libutils \
-    liblog
+    liblog \
+    libsoundtrigger \
+    libaudiopolicymanagerdefault \
+    libserviceutility
 
 LOCAL_STATIC_LIBRARIES := \
-    libmedia_helper
+    libmedia_helper \
 
-LOCAL_WHOLE_STATIC_LIBRARIES := \
-    libaudiopolicy_legacy
-
-LOCAL_MODULE := audio_policy.$(TARGET_BOARD_PLATFORM)
-LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
-LOCAL_MODULE_TAGS := optional
-
-ifeq ($(strip $(AUDIO_FEATURE_ENABLED_FM)),true)
-LOCAL_CFLAGS += -DAUDIO_EXTN_FM_ENABLED
-endif
-ifeq ($(strip $(AUDIO_FEATURE_ENABLED_PROXY_DEVICE)),true)
-LOCAL_CFLAGS += -DAUDIO_EXTN_AFE_PROXY_ENABLED
-endif
-ifeq ($(strip $(AUDIO_FEATURE_ENABLED_INCALL_MUSIC)),true)
-LOCAL_CFLAGS += -DAUDIO_EXTN_INCALL_MUSIC_ENABLED
-endif
-ifeq ($(strip $(AUDIO_FEATURE_ENABLED_HDMI_SPK)),true)
-LOCAL_CFLAGS += -DAUDIO_EXTN_HDMI_SPK_ENABLED
-endif
-
-
-ifeq ($(strip $(TARGET_BOARD_PLATFORM)),msm8916)
-LOCAL_CFLAGS += -DVOICE_CONCURRENCY
-LOCAL_CFLAGS += -DWFD_CONCURRENCY
-endif
-
-ifeq ($(strip $(AUDIO_FEATURE_ENABLED_MULTIPLE_TUNNEL)), true)
-LOCAL_CFLAGS += -DMULTIPLE_OFFLOAD_ENABLED
-endif
+LOCAL_MODULE := libaudiopolicymanager
 
 include $(BUILD_SHARED_LIBRARY)
 
+endif
 endif
