CyanogenMod/android_hardware_qcom_display

Add dummy symlinks to actual implementation
* Include paths expect headers to be at
  hardware/qcom/display-caf/$(TARGET_BOARD_PLATFORM), so create symlinks to
  the 8974 implementation at hardware/qcom/display-caf/msm8974.
* (cdesai): Symlink the Android.mk too, so that the build
  system can pick that up and build what's in here.

Change-Id: Ic089ef7f8d635db4de2f8d238f6d2c4e96d05799
Signed-off-by: Chirayu Desai <chirayudesai1@gmail.com>

Android.mk
@@ -1,0 +1,1 @@
+../msm8974/Android.mk

LineageOS/android_hardware_qcom_audio

  hardware/qcom/audio-caf/$(TARGET_BOARD_PLATFORM), so create symlinks to
  the 8994 implementation at hardware/qcom/audio-caf/msm8994.

Android.mk
@@ -1,0 +1,1 @@
+../msm8994/Android.mk