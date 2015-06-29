a74e68af4157b9dc1709efbf173f9933f9c9dda3
Steve Kondik <steve@cyngn.com>
2015-06-28 17:04:52 -0700

    msm8994: Remove bogus metadata
     * This is unnecessary and also wrong. Remove it to enable FLAC offload
       without the proprietary decoder.
    
    Change-Id: I057fee4662fc7800b6ec3b576f1a1efcb939183f

--- hal/audio_extn/audio_extn.h
+++ hal/audio_extn/audio_extn.h
@@ -69,14 +69,6 @@
 #define AUDIO_DEVICE_IN_FM_RX_A2DP (AUDIO_DEVICE_BIT_IN | 0x10000)
 #endif
 
-#ifndef QTI_FLAC_DECODER
-#define AUDIO_FORMAT_FLAC 0x19000000UL
-#define AUDIO_OFFLOAD_CODEC_FLAC_MIN_BLK_SIZE "music_offload_flac_min_blk_size"
-#define AUDIO_OFFLOAD_CODEC_FLAC_MAX_BLK_SIZE "music_offload_flac_max_blk_size"
-#define AUDIO_OFFLOAD_CODEC_FLAC_MIN_FRAME_SIZE "music_offload_flac_min_frame_size"
-#define AUDIO_OFFLOAD_CODEC_FLAC_MAX_FRAME_SIZE "music_offload_flac_max_frame_size"
-#endif
-
 #ifdef PCM_OFFLOAD_ENABLED_24
 #define PCM_OUTPUT_BIT_WIDTH (config->offload_info.bit_width)
 #else
