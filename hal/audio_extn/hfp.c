95333359b939d4e4b4a546d6f38e2f82258f8b31
Apoorv Raghuvanshi <apoorvr@codeaurora.org>
2015-07-06 15:07:14 -0700

    hal: Add codec EC reference support
    - We need to set codec EC reference instead of MSM/AFE
      EC ref for Native audio usecase along with other
      usecases running on 48K port. In this scenario we
      will run two AFE ports, one regular port and the
      other port running at 44.1KHz and mixing done in codec.
      So set appropriate codec EC reference.
    
    - One example of such usecase is VoIP over audio path
      and a 44.1KHz stream playback. But this change is generic
      enough to accommodate other possible usecases if any.
    
    CRs-Fixed: 877363
    Change-Id: Ib980e36d111c1671303abe2b19341568d9fd2331

--- hal/audio_extn/hfp.c
+++ hal/audio_extn/hfp.c
@@ -1,5 +1,5 @@
 /* hfp.c
-Copyright (c) 2012-2014, The Linux Foundation. All rights reserved.
+Copyright (c) 2012-2015, The Linux Foundation. All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are
@@ -271,7 +271,7 @@ static int32_t stop_hfp(struct audio_device *adev)
     }
 
     /* 2. Disable echo reference while stopping hfp */
-    platform_set_echo_reference(adev->platform, false);
+    platform_set_echo_reference(adev, false, uc_info->devices);
 
     /* 3. Get and set stream specific mixer controls */
     disable_audio_route(adev, uc_info);
