1521f8d1b815d9940e412a0d45dd0eaa49ba3b1d
Preetam Singh Ranawat <apranawat@codeaurora.org>
2015-07-23 17:58:53 +0530

    hal : Avoid use of small buf flag and bit width info.
     -small buf flag and bit width info are not member of offload into
      structure.
     -avoid use of small buf flag and bit width info, use PCM offload
      format.
    
    Change-Id: I1291e3d0ade43a0d7412e29abb2383c713d1e40f

--- hal/audio_hw.c
+++ hal/audio_hw.c
@@ -2847,7 +2847,7 @@ static int adev_open_output_stream(struct audio_hw_device *dev,
         else
             out->compr_config.codec->id =
                 get_snd_codec_id(config->offload_info.format);
-        if (audio_is_offload_pcm(config->offload_info.format)) {
+        if ((config->offload_info.format & AUDIO_FORMAT_MAIN_MASK) == AUDIO_FORMAT_PCM_OFFLOAD) {
             out->compr_config.fragment_size =
                platform_get_pcm_offload_buffer_size(&config->offload_info);
         } else if (audio_extn_dolby_is_passthrough_stream(out->flags)) {
