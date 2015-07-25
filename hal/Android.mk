251719e457a721b4f91a1b07dd51f4b8c899a251
ljzyal <ljzyal@gmail.com>
2015-07-25 00:41:56 -0700

    hal: Add support for Samsung dual-SIM implementation
    Change-Id: Iaacf9a5837a7391bdbce8c5ee6aae609e24dfb47

--- hal/Android.mk
+++ hal/Android.mk
@@ -103,6 +103,10 @@ ifneq ($(strip $(AUDIO_FEATURE_ENABLED_COMPRESS_VOIP)),false)
     LOCAL_CFLAGS += -DCOMPRESS_VOIP_ENABLED
     LOCAL_SRC_FILES += voice_extn/compress_voip.c
 endif
+ifeq ($(strip $(AUDIO_FEATURE_SAMSUNG_DUAL_SIM)),true)
+    LOCAL_CFLAGS += -DSAMSUNG_DUAL_SIM
+    LOCAL_SRC_FILES += voice_extn/sec_voice_extn.c
+endif
 endif
 
 ifeq ($(strip $(AUDIO_FEATURE_ENABLED_EXTN_FORMATS)),true)
