71f844addaecd6ae05f741c979a58d083f405dcf
Ethan Chen <intervigil@gmail.com>
2015-07-27 13:44:45 -0700

    audio: Update makefile for CM

--- Android.mk
+++ Android.mk
@@ -1,27 +1,11 @@
-ifneq ($(TARGET_PROVIDES_LIBAUDIO),true)
-ifneq ($(BOARD_USES_AUDIO_LEGACY),true)
-ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
+ifeq ($(call my-dir),$(call project-path-for,qcom-audio))
 
 AUDIO_HW_ROOT := $(call my-dir)
 
-ifeq ($(TARGET_BOARD_PLATFORM),msm8960)
-    include $(AUDIO_HW_ROOT)/alsa_sound/Android.mk
-    include $(AUDIO_HW_ROOT)/libalsa-intf/Android.mk
-endif
-ifeq ($(TARGET_BOARD_PLATFORM),msm7x27a)
-    include $(AUDIO_HW_ROOT)/msm7627a/Android.mk
-endif
-ifeq ($(TARGET_BOARD_PLATFORM),msm7x30)
-    include $(AUDIO_HW_ROOT)/msm7x30/Android.mk
-endif
-ifeq ($(TARGET_BOARD_PLATFORM),msm8660)
-    include $(AUDIO_HW_ROOT)/msm8660/Android.mk
-    include $(AUDIO_HW_ROOT)/mm-audio/Android.mk
-endif
-ifeq ($(TARGET_BOARD_PLATFORM),msm8960)
+include $(AUDIO_HW_ROOT)/$(TARGET_BOARD_PLATFORM)/Android.mk
+
+ifneq ($(filter msm8660,$(TARGET_BOARD_PLATFORM)),)
     include $(AUDIO_HW_ROOT)/mm-audio/Android.mk
 endif
 
-endif
-endif
 endif
