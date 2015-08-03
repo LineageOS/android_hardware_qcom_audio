e20788101caeb1aae4b8623e7644a02e72de9aec
Bruno Martins <bgcngm@gmail.com>
2015-08-03 22:52:51 +0100

    hal: Genericize dual-SIM voice_extn implementation
    * Add HTC hooks
    
    Change-Id: Id72f3af3590df70e744522f63f04b9a77af26220

--- hal/Android.mk
+++ hal/Android.mk
@@ -80,9 +80,13 @@ ifeq ($(strip $(AUDIO_FEATURE_ENABLED_MULTI_VOICE_SESSIONS)),true)
 ifneq ($(strip $(AUDIO_FEATURE_ENABLED_INCALL_MUSIC)),false)
     LOCAL_CFLAGS += -DINCALL_MUSIC_ENABLED
 endif
+ifeq ($(strip $(AUDIO_FEATURE_HTC_DUAL_SIM)),true)
+    LOCAL_CFLAGS += -DHTC_DUAL_SIM
+    LOCAL_SRC_FILES += voice_extn/msim_voice_extn.c
+endif
 ifeq ($(strip $(AUDIO_FEATURE_SAMSUNG_DUAL_SIM)),true)
     LOCAL_CFLAGS += -DSAMSUNG_DUAL_SIM
-    LOCAL_SRC_FILES += voice_extn/sec_voice_extn.c
+    LOCAL_SRC_FILES += voice_extn/msim_voice_extn.c
 endif
 endif
 
