ca36cb1c9009f16da60c75b18da381b6dc41f8a3
Shiv Maliyappanahalli <smaliyap@codeaurora.org>
2015-08-17 11:02:21 -0700

    Revert "hal: Fix voice session setup failure"
    Camera click tones are not playing while in voice calls and headset
    is connected since get_output_devices doesnt return a valid device.
    Fix by returning combo device speaker-and-headset when in voice call.
    
    The original issue reported in CR859796, seem to customer specific,
    as on internal platforms, there are no usecases such as
    "volte-call speaker-and-headset".
    
    This reverts commit 62d96d50dafae78818994425275a743b68d70881.
    
    Change-Id: Ic8146d8ed5ab7312fa40242c5fda00996a27c083

--- hal/msm8974/platform.c
+++ hal/msm8974/platform.c
@@ -2049,7 +2049,7 @@ snd_device_t platform_get_output_snd_device(void *platform, struct stream_out *o
         goto exit;
     }
 
-    if (popcount(devices) == 2 && !voice_is_in_call(adev)) {
+    if (popcount(devices) == 2) {
         if (devices == (AUDIO_DEVICE_OUT_WIRED_HEADPHONE |
                         AUDIO_DEVICE_OUT_SPEAKER)) {
             if (my_data->external_spk_1)
