8d0dee5a3aeb0b16ed8c4021463fcc5023b8e613
Roopesh Rajashekharaiah Nataraja <roopes@codeaurora.org>
2015-09-03 16:52:21 -0700

    audio : Disable compilation of mm-audio and post_proc for 8909w
    Do not compile mm-audio and post_proc audio hal modules for 8909w
    
    Change-Id: I379d7f7c0a756449dbbb8bd9b36e6b4d04837413

--- Android.mk
+++ Android.mk
@@ -11,11 +11,15 @@ else
 include $(MY_LOCAL_PATH)/hal/Android.mk
 endif
 include $(MY_LOCAL_PATH)/voice_processing/Android.mk
+ifneq ($(TARGET_SUPPORTS_WEARABLES),true)
 include $(MY_LOCAL_PATH)/mm-audio/Android.mk
+endif
 include $(MY_LOCAL_PATH)/policy_hal/Android.mk
 include $(MY_LOCAL_PATH)/visualizer/Android.mk
 include $(MY_LOCAL_PATH)/audiod/Android.mk
+ifneq ($(TARGET_SUPPORTS_WEARABLES),true)
 include $(MY_LOCAL_PATH)/post_proc/Android.mk
+endif
 endif
 
 endif
