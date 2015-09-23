e12b3969fbcf748b529e690f286d648cb1f49e4b
Ravi Kumar Alamanda <ralama@codeaurora.org>
2015-09-23 13:47:03 -0700

    hal: Add support for perf lock management
    Acquire perf lock before starting an input or output
    stream to optimize the cold start latency.
    
    Bug: 22537514
    
    Cherry-pick of CAF commit:
    390bcf3edac3e0e38cf8163d1297f1b669117fe0
    
    Change-Id: Iacedde8ff8531f8857e516e2671acd62197556a0

--- hal/audio_extn/audio_extn.c
+++ hal/audio_extn/audio_extn.c
@@ -2,7 +2,7 @@
  * Copyright (c) 2013-2014, The Linux Foundation. All rights reserved.
  * Not a Contribution.
  *
- * Copyright (C) 2013 The Android Open Source Project
+ * Copyright (C) 2015 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -23,6 +23,7 @@
 
 #include <stdlib.h>
 #include <errno.h>
+#include <dlfcn.h>
 #include <cutils/properties.h>
 #include <cutils/log.h>
 
@@ -461,3 +462,74 @@ int32_t audio_extn_read_xml(struct audio_device *adev, uint32_t mixer_card,
     return 0;
 }
 #endif /* AUXPCM_BT_ENABLED */
+
+
+#ifdef KPI_OPTIMIZE_ENABLED
+typedef int (*perf_lock_acquire_t)(int, int, int*, int);
+typedef int (*perf_lock_release_t)(int);
+
+static void *qcopt_handle;
+static perf_lock_acquire_t perf_lock_acq;
+static perf_lock_release_t perf_lock_rel;
+
+static int perf_lock_handle;
+char opt_lib_path[PROPERTY_VALUE_MAX] = {0};
+
+int perf_lock_opts[] = {0x101, 0x20E, 0x30E};
+
+int audio_extn_perf_lock_init(void)
+{
+    int ret = 0;
+    if (qcopt_handle == NULL) {
+        if (property_get("ro.vendor.extension_library",
+                         opt_lib_path, NULL) <= 0) {
+            ALOGE("%s: Failed getting perf property", __func__);
+            ret = -EINVAL;
+            goto err;
+        }
+        if ((qcopt_handle = dlopen(opt_lib_path, RTLD_NOW)) == NULL) {
+            ALOGE("%s: Failed to open perf handle", __func__);
+            ret = -EINVAL;
+            goto err;
+        } else {
+            perf_lock_acq = (perf_lock_acquire_t)dlsym(qcopt_handle,
+                                                       "perf_lock_acq");
+            if (perf_lock_acq == NULL) {
+                ALOGE("%s: Perf lock Acquire NULL", __func__);
+                ret = -EINVAL;
+                goto err;
+            }
+            perf_lock_rel = (perf_lock_release_t)dlsym(qcopt_handle,
+                                                       "perf_lock_rel");
+            if (perf_lock_rel == NULL) {
+                ALOGE("%s: Perf lock Release NULL", __func__);
+                ret = -EINVAL;
+                goto err;
+            }
+            ALOGD("%s: Perf lock handles Success", __func__);
+        }
+    }
+err:
+    return ret;
+}
+
+void audio_extn_perf_lock_acquire(void)
+{
+    if (perf_lock_acq) {
+        perf_lock_handle = perf_lock_acq(perf_lock_handle, 0, perf_lock_opts, 3);
+        ALOGV("%s: Perf lock acquired", __func__);
+    } else {
+        ALOGE("%s: Perf lock acquire error", __func__);
+    }
+}
+
+void audio_extn_perf_lock_release(void)
+{
+    if (perf_lock_rel && perf_lock_handle) {
+        perf_lock_rel(perf_lock_handle);
+        ALOGV("%s: Perf lock released", __func__);
+    } else {
+        ALOGE("%s: Perf lock release error", __func__);
+    }
+}
+#endif /* KPI_OPTIMIZE_ENABLED */
