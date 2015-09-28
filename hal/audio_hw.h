5b606869551fdb0eb533861c522d5a8333cd3d31
Shiv Maliyappanahalli <smaliyap@codeaurora.org>
2015-09-28 15:23:06 -0700

    hal: fix thread starvation
    Fix thread starvation issue where the capture or playback
    threads running in FIFO priority would constantly acquire
    the stream mutex preventing other threads to complete
    routing commands.
    
    CRs-Fixed: 909938
    
    Change-Id: I8d1befa0791ae9a3bce868c1c3ef262ff489f052

--- hal/audio_hw.h
+++ hal/audio_hw.h
@@ -161,6 +161,7 @@ struct stream_app_type_cfg {
 struct stream_out {
     struct audio_stream_out stream;
     pthread_mutex_t lock; /* see note below on mutex acquisition order */
+    pthread_mutex_t pre_lock; /* acquire before lock to avoid DOS by playback thread */
     pthread_cond_t  cond;
     struct pcm_config config;
     struct compr_config compr_config;
@@ -201,6 +202,7 @@ struct stream_out {
 struct stream_in {
     struct audio_stream_in stream;
     pthread_mutex_t lock; /* see note below on mutex acquisition order */
+    pthread_mutex_t pre_lock; /* acquire before lock to avoid DOS by record thread */
     struct pcm_config config;
     struct pcm *pcm;
     int standby;
