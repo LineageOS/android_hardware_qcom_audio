7020f43b125f71be0570919a2517dbbb4639dc5a
Ethan Chen <intervigil@gmail.com>
2015-10-15 15:14:48 -0700

    alsa_sound: Disable tunnel/LPA
    * Requires legacy AwesomePlayer, which has been deprecated for a while.
    
    Change-Id: Ia9c8d1b66631fc50714751d459045e6f3aac4ad3

--- legacy/alsa_sound/AudioHardwareALSA.cpp
+++ legacy/alsa_sound/AudioHardwareALSA.cpp
@@ -1433,7 +1433,7 @@ AudioHardwareALSA::openOutputStream(uint32_t devices,
          devices, *channels, *sampleRate, flags);
 
     status_t err = BAD_VALUE;
-//#ifdef QCOM_TUNNEL_LPA_ENABLED
+#ifdef QCOM_TUNNEL_LPA_ENABLED
     if (flags & (AUDIO_OUTPUT_FLAG_LPA | AUDIO_OUTPUT_FLAG_TUNNEL)) {
         int type = !(flags & AUDIO_OUTPUT_FLAG_LPA); //0 for LPA, 1 for tunnel
         AudioSessionOutALSA *out = new AudioSessionOutALSA(this, devices, *format, *channels,
@@ -1447,7 +1447,7 @@ AudioHardwareALSA::openOutputStream(uint32_t devices,
         if (status) *status = err;
         return out;
     }
-//#endif
+#endif
     AudioStreamOutALSA *out = 0;
     ALSAHandleList::iterator it;
 
