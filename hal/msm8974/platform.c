04a9b1da563f483478f4b12065dd9b930eafe3c8
Amit Shekhar <ashekhar@codeaurora.org>
2016-01-20 17:21:58 -0800

    hal: Fix 44.1Khz playback when native support is not enabled
    -Currently, backend sampling rate is incorrectly set to 44.1Khz - for
     24b/44.1Khz clips - when native support is not available. SLIM_0_RX
     doesn't support native playback. By setting the backend to 48Khz,
     audio playback works as expected.
    -Initialize and update return value before returning
    
    Change-Id: I35f05e78b02159ff046da6c0d70dcd53f6464c8e
    CRs-Fixed: 963693

--- hal/msm8974/platform.c
+++ hal/msm8974/platform.c
@@ -1888,8 +1888,8 @@ int platform_get_snd_device_bit_width(snd_device_t snd_device)
 
 int platform_set_native_support(bool codec_support)
 {
-    int ret = 0;
-    na_props.platform_na_prop_enabled = na_props.ui_na_prop_enabled
+    int ret = -1;
+    ret = na_props.platform_na_prop_enabled = na_props.ui_na_prop_enabled
         = codec_support;
     ALOGD("%s: na_props.platform_na_prop_enabled: %d", __func__,
            na_props.platform_na_prop_enabled);
@@ -3649,6 +3649,8 @@ int platform_set_codec_backend_cfg(struct audio_device* adev,
      * 24 bit playback - 48khz for stream sample rate less than 48khz
      * 24 bit playback - 96khz for sample rate range of 48khz to 96khz
      * 24 bit playback - 192khz for sample rate range of 96khz to 192 khz
+     * native support is present: 16/24 bit 44.1Khz clip uses backend at 44.1Khz
+     * native support is not present: 44.1Khz clips uses backend at 48Khz
      * Upper limit is inclusive in the sample rate range.
      */
     if (sample_rate !=
@@ -3778,6 +3780,10 @@ bool platform_check_codec_backend_cfg(struct audio_device* adev,
         if ((24 == bit_width) &&
             (usecase->stream.out->devices & AUDIO_DEVICE_OUT_SPEAKER)) {
             bit_width = (uint32_t)platform_get_snd_device_bit_width(SND_DEVICE_OUT_SPEAKER);
+            sample_rate = CODEC_BACKEND_DEFAULT_SAMPLE_RATE;
+        }
+        // 24 bit native clips must be played at 48Khz for non native backend
+        if ((24 == bit_width) && (OUTPUT_SAMPLING_RATE_44100 == sample_rate)) {
             sample_rate = CODEC_BACKEND_DEFAULT_SAMPLE_RATE;
         }
     }
--- hal/platform_info.c
+++ hal/platform_info.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014-2015, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2014-2016, The Linux Foundation. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -324,7 +324,7 @@ static void process_native_support(const XML_Char **attr)
     }
 
     if (platform_set_native_support(atoi((char *)attr[3])) < 0) {
-        ALOGE("%s: NATIVE_AUDIO_44 was not set!", __func__);
+        ALOGE("%s: native audio support flag couldn't be set!", __func__);
         goto done;
     }
 
