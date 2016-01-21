146e7cf58b4343d9f3a42a544631b15fc79e26f5
Shiv Maliyappanahalli <smaliyap@codeaurora.org>
2016-01-21 11:30:06 -0800

    hal: fix no audio issue in voice call
    No audio is observed in voice call -> voip -> voice call scenario
    since voice usecase is incorrectly stopped when audio mode
    MODE_IN_COMMUNICATION is set. This results in not setting the voice
    usecase if when the usecase transitions from voip -> voice.
    
    Fix by removing check for MODE_IN_COMMUNICATION in adev_set_mode().
    Also update the check for selecting voice devices in select_devices()
    to ensure that voice devices are still selected in
    voice call -> voip -> voice call scenario.
    
    Conflicts:
    	hal/audio_hw.c
    
    Change-Id: I74c9161282750ba5f78f0daddd7478c4823ff19c

--- hal/audio_hw.c
+++ hal/audio_hw.c
@@ -987,7 +987,7 @@ int select_devices(struct audio_device *adev, audio_usecase_t uc_id)
          * usecase. This is to avoid switching devices for voice call when
          * check_usecases_codec_backend() is called below.
          */
-        if (voice_is_in_call(adev) && adev->mode == AUDIO_MODE_IN_CALL) {
+        if (voice_is_in_call(adev) && adev->mode != AUDIO_MODE_NORMAL) {
             vc_usecase = get_usecase_from_list(adev,
                                                get_usecase_id_from_usecase_type(adev, VOICE_CALL));
             if ((vc_usecase) && ((vc_usecase->devices & AUDIO_DEVICE_OUT_ALL_CODEC_BACKEND) ||
@@ -3666,8 +3666,7 @@ static int adev_set_mode(struct audio_hw_device *dev, audio_mode_t mode)
         if (amplifier_set_mode(mode) != 0)
             ALOGE("Failed setting amplifier mode");
         adev->mode = mode;
-        if ((mode == AUDIO_MODE_NORMAL || mode == AUDIO_MODE_IN_COMMUNICATION) &&
-                voice_is_in_call(adev)) {
+        if ((mode == AUDIO_MODE_NORMAL) && voice_is_in_call(adev)) {
             adev->voice.is_in_call = false;
             voice_stop_call(adev);
             platform_set_gsm_mode(adev->platform, false);
