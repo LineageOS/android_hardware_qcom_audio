318d9612b5bd88cd757226a985681105cbfce352
Luca Stefani <luca.stefani.ge1@gmail.com>
2016-02-28 21:38:42 +0100

    audio: msm8916: Squashed audio amp commits
    commit e821aba3b7603328f5b54877ae49c968db9f041e
    Author: Scott Mertz <scott@cyngn.com>
    Date:   Fri Sep 11 12:13:08 2015 -0700
    
        hal: notify amplifier of parameter changes
    
        Change-Id: Iecc020c0347ae7c43d27183186e06dcefef7a0dd
    
    commit 338f1b9c00aaa3830b44f030c255f974e781519a
    Author: Dan Pasanen <dan.pasanen@gmail.com>
    Date:   Mon Aug 10 19:55:39 2015 -0500
    
        hal: only open the amplifier once
    
        Change-Id: Ie9bbff74123e90b71e95809a84dcb3bbe9ba82fe
    
    commit d79e906e7d34369a2444bc6827bfb0fb57dc2c55
    Author: Dan Pasanen <dan.pasanen@gmail.com>
    Date:   Mon Aug 10 19:47:43 2015 -0500
    
        hal: enable amplifier earlier
    
        Change-Id: Id876e8f836e3ce1ee5f8186ca9c0e6ef5f37182c
    
    commit dfa41b007c557c30c87842d67c3089b624b45253
    Author: Ethan Chen <intervigil@gmail.com>
    Date:   Fri Jul 3 21:35:30 2015 -0700
    
        hal: Notify amplifier of device enable/disable
    
        Change-Id: Ice808c9b55a9e3bc8bafe5ca3ff555377d38dd8f
    
    commit 44448ed866ca8d1fbcce558504151e8f1ea9e480
    Author: Ethan Chen <intervigil@gmail.com>
    Date:   Sun Jun 7 12:32:08 2015 -0700
    
        hal: Convert libaudioamp to audio_amplifier HAL
    
        Change-Id: I1d0f63a46989d1792d7a5e08d2bdb6344ebafa31
    
    commit cb832553f11b79acc16772bd3f930a9d38f748c5
    Author: Scott Mertz <scott@cyngn.com>
    Date:   Sat Jun 6 13:46:03 2015 -0700
    
        audio: add amplifier stream start/standby operations
    
        Change-Id: I5de7ad7a0467e7cf822c9c0870a755c03d05e884
    
    commit 1437a75bee9d016dc58e10220cd69ae4c1454f24
    Author: Daniel Hillenbrand <codeworkx@cyanogenmod.org>
    Date:   Thu May 23 10:10:00 2013 +0530
    
        hal: Support the audio amplifier hook
    
        * Original legacy HAL commit:
          Ib236598a5888b2af19bcfb81e285f644a0e84c0d
        * Example: http://review.cyanogenmod.org/38221
    
        Change-Id: Ic944a9f7059c78b79322dae9c787cdd8bb029cff
    
    Change-Id: I4ea9556a327b31ad2a39f182b8ac7114693f2ac6

--- hal/Android.mk
+++ hal/Android.mk
@@ -242,6 +242,7 @@ endif
 LOCAL_SHARED_LIBRARIES := \
         liblog \
         libcutils \
+        libhardware \
         libtinyalsa \
         libtinycompress_vendor \
         libaudioroute \
@@ -253,6 +254,7 @@ LOCAL_C_INCLUDES += \
         external/tinyalsa/include \
         external/tinycompress/include \
         external/expat/lib \
+        hardware/libhardware/include \
         system/media/audio_utils/include \
         $(call include-path-for, audio-route) \
         $(call include-path-for, audio-effects) \
