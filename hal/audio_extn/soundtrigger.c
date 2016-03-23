68d3c49601842a4000aa498e718827dc93585ca5
Steve Kondik <steve@cyngn.com>
2016-03-23 13:32:28 -0700

    audio: Hacks for sound trigger
    Change-Id: Ice58e83f731d45be77f69f2810426dd72cfb98f7

--- hal/audio_extn/soundtrigger.c
+++ hal/audio_extn/soundtrigger.c
@@ -26,16 +26,72 @@
 #include "audio_extn.h"
 #include "platform.h"
 #include "platform_api.h"
-#include "sound_trigger_prop_intf.h"
+//#include "sound_trigger_prop_intf.h"
 
 #define XSTR(x) STR(x)
 #define STR(x) #x
+
+struct sound_trigger_session_info {
+    int capture_handle;
+    struct pcm *pcm;
+    struct pcm_config config;
+};
 
 struct sound_trigger_info  {
     struct sound_trigger_session_info st_ses;
     bool lab_stopped;
     struct listnode list;
 };
+
+enum audio_event_type {
+    AUDIO_EVENT_CAPTURE_DEVICE_INACTIVE,
+    AUDIO_EVENT_CAPTURE_DEVICE_ACTIVE,
+    AUDIO_EVENT_PLAYBACK_STREAM_INACTIVE,
+    AUDIO_EVENT_PLAYBACK_STREAM_ACTIVE,
+    AUDIO_EVENT_STOP_LAB,
+    AUDIO_EVENT_SSR,
+    AUDIO_EVENT_NUM_ST_SESSIONS,
+    AUDIO_EVENT_READ_SAMPLES,
+    AUDIO_EVENT_DEVICE_CONNECT,
+    AUDIO_EVENT_DEVICE_DISCONNECT
+};
+
+enum sound_trigger_event_type {
+    ST_EVENT_SESSION_REGISTER,
+    ST_EVENT_SESSION_DEREGISTER
+};
+typedef enum sound_trigger_event_type sound_trigger_event_type_t;
+
+enum ssr_event_status {
+    SND_CARD_STATUS_OFFLINE,
+    SND_CARD_STATUS_ONLINE,
+    CPE_STATUS_OFFLINE,
+    CPE_STATUS_ONLINE
+};
+
+struct sound_trigger_event_info {
+    struct sound_trigger_session_info st_ses;
+};
+typedef struct sound_trigger_event_info sound_trigger_event_info_t;
+
+struct audio_read_samples_info {
+    struct sound_trigger_session_info *ses_info;
+    void *buf;
+    size_t num_bytes;
+};
+
+struct audio_event_info {
+    union {
+        enum ssr_event_status status;
+        int value;
+        struct sound_trigger_session_info ses_info;
+        struct audio_read_samples_info aud_info;
+    }u;
+};
+typedef struct audio_event_info audio_event_info_t;
+
+typedef int (*sound_trigger_hw_call_back_t)(enum audio_event_type,
+                                            struct audio_event_info*);
 
 struct sound_trigger_audio_device {
     void *lib_handle;
