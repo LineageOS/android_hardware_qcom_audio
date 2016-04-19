13dd5f3fbfea2082121d41b64a92ebfd0c400a62
kunleiz <kunleiz@codeaurora.org>
2016-04-19 10:28:42 +0800

    hal: fix no sound issue in voice call
    No audio is observed in voice call scenario since in_call state
    is not update when voice usecase is stopped. This results in not
    starting voice call usecase when setting up next call.
    
    Fix by setting in_call state as false in voice_stop_usecase(). This
    is to ensure that voice usecase is stopped and in_call state is update
    properly.
    
    CRs-Fixed: 996510
    Change-Id: I480bb14899d678a47df8a61fbdd208b75ef7b395

--- hal/voice.h
+++ hal/voice.h
@@ -81,7 +81,6 @@ int voice_set_parameters(struct audio_device *adev, struct str_parms *parms);
 void voice_get_parameters(struct audio_device *adev, struct str_parms *query,
                           struct str_parms *reply);
 void voice_init(struct audio_device *adev);
-bool voice_is_in_call(struct audio_device *adev);
 bool voice_is_in_call_rec_stream(struct stream_in *in);
 int voice_set_mic_mute(struct audio_device *dev, bool state);
 bool voice_get_mic_mute(struct audio_device *dev);
@@ -93,4 +92,5 @@ int voice_check_and_set_incall_music_usecase(struct audio_device *adev,
 int voice_check_and_stop_incall_rec_usecase(struct audio_device *adev,
                                             struct stream_in *in);
 void voice_update_devices_for_all_voice_usecases(struct audio_device *adev);
+bool voice_is_call_state_active(struct audio_device *adev);
 #endif //VOICE_H
