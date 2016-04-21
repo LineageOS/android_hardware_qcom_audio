1ea56685003f11e9fbc47877bff88352058cde72
Alexy Joseph <alexyj@codeaurora.org>
2016-04-20 18:44:56 -0700

    hal: Fix device selection during voice call
    During a voice call, when a device switch happens
    for any stream, we try to make sure that all
    streams sharing the same back end is re-routed
    to the new device. This was causing even
    devices that do not share the same codec backend
    to get re-routed to the new device. Fix this
    by checking for the device on which use case
    is running
    
    Change-Id: I101b79d3642fa1f8f32eeaf3731a5c7bd5364c72
    CRs-Fixed: 1006364

--- hal/audio_hw.c
+++ hal/audio_hw.c
@@ -654,8 +654,10 @@ static void check_usecases_codec_backend(struct audio_device *adev,
 
     list_for_each(node, &adev->usecase_list) {
         usecase = node_to_item(node, struct audio_usecase, list);
-        ALOGV("%s: curr device: %s, usecase device: %s", __func__,
+        ALOGV("%s: curr device: %s, usecase %s"
+              "usecase device: %s", __func__,
               platform_get_snd_device_name(snd_device),
+              use_case_table[usecase->id],
               platform_get_snd_device_name(usecase->out_snd_device));
         if (usecase->type != PCM_CAPTURE &&
             usecase != uc_info &&
@@ -873,12 +875,15 @@ int select_devices(struct audio_device *adev, audio_usecase_t uc_id)
          * be switched to new device when select_devices() is called for voice call
          * usecase. This is to avoid switching devices for voice call when
          * check_usecases_codec_backend() is called below.
+         * choose voice call device only if the use case device is
+         * also using the codec backend
          */
         if (voice_is_in_call(adev) && adev->mode != AUDIO_MODE_NORMAL) {
             vc_usecase = get_usecase_from_list(adev,
                                                get_usecase_id_from_usecase_type(adev, VOICE_CALL));
-            if ((vc_usecase) && ((vc_usecase->devices & AUDIO_DEVICE_OUT_ALL_CODEC_BACKEND) ||
-                (usecase->devices == AUDIO_DEVICE_IN_VOICE_CALL))) {
+            if (((vc_usecase) && ((vc_usecase->devices & AUDIO_DEVICE_OUT_ALL_CODEC_BACKEND) &&
+                                 (usecase->devices & AUDIO_DEVICE_OUT_ALL_CODEC_BACKEND))) ||
+                                (usecase->devices == AUDIO_DEVICE_IN_VOICE_CALL)) {
                 in_snd_device = vc_usecase->in_snd_device;
                 out_snd_device = vc_usecase->out_snd_device;
             }
