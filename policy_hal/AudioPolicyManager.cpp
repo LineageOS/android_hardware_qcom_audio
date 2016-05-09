9127146af5ecb3f321c4bfa7212395019904b1e1
Lalit Kansara <lkansara@codeaurora.org>
2016-05-09 19:12:01 +0530

    audiopolicy: fix compilation issue due to AOSP refresh
    Edit function signature of startSource() to reflect changes
    as per the latest AOSP refresh.
    
    Change-Id: Ife422dc44d0fd3b79b4b0363ba10a6bdbe772fee

--- policy_hal/AudioPolicyManager.cpp
+++ policy_hal/AudioPolicyManager.cpp
@@ -1098,6 +1098,7 @@ status_t AudioPolicyManagerCustom::stopSource(sp<AudioOutputDescriptor> outputDe
 status_t AudioPolicyManagerCustom::startSource(sp<AudioOutputDescriptor> outputDesc,
                                              audio_stream_type_t stream,
                                              audio_devices_t device,
+                                             const char *address,
                                              uint32_t *delayMs)
 {
     // cannot start playback of STREAM_TTS if any other output is being used
@@ -1160,7 +1161,7 @@ status_t AudioPolicyManagerCustom::startSource(sp<AudioOutputDescriptor> outputD
             }
         }
         uint32_t muteWaitMs;
-        muteWaitMs = setOutputDevice(outputDesc, device, force);
+        muteWaitMs = setOutputDevice(outputDesc, device, force, 0, NULL, address);
 
         // handle special case for sonification while in call
         if (isInCall()) {
