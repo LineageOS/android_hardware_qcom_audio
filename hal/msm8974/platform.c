d60805067418091f4a4ead52c38902596af47701
Ethan Chen <intervigil@gmail.com>
2016-05-11 22:27:12 -0700

    hal: Clean up from merge
    * Remove 8974-specific code where possible
    * Don't distinguish based on PLATFORM_8960, everything is PLATFORM_8960
      if using the A-family HAL.
    * Remove USECASE_AUDIO_RECORD_FM_VIRTUAL for 8960 platform, as it's not
      a valid codepath here.
    
    Change-Id: I1cef978b27d7ea87fa3fa7af1e121545dfda1505

--- hal/msm8974/platform.c
+++ hal/msm8974/platform.c
@@ -2056,7 +2056,7 @@ int platform_update_usecase_from_source(int source, int usecase)
             return USECASE_INCALL_REC_DOWNLINK;
         case AUDIO_SOURCE_VOICE_CALL:
             return USECASE_INCALL_REC_UPLINK_AND_DOWNLINK;
-#if 0 /* This is actually breaking FM playback/recording in MM */
+#if 0
         case AUDIO_SOURCE_FM_TUNER:
             return USECASE_AUDIO_RECORD_FM_VIRTUAL;
 #endif
