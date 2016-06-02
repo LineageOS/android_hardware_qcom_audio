a9abb20e12253aaf9f6b5266a3118878ef0cebc8
Haynes Mathew George <hgeorge@codeaurora.org>
2016-06-02 14:13:20 -0700

    audio: Fix memory leak
    Free command object queued with offload_thread_loop
    when driving instance is unavailable.
    
    Change-Id: I310a462ae8efe707658d941e88cb5ea59a9f8550
    CRs-Fixed: 1022145

--- hal/audio_hw.c
+++ hal/audio_hw.c
@@ -1494,6 +1494,7 @@ static void *offload_thread_loop(void *context)
 
         if (out->compr == NULL) {
             ALOGE("%s: Compress handle is NULL", __func__);
+            free(cmd);
             pthread_cond_signal(&out->cond);
             continue;
         }
