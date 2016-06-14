c3e8d36c8d4b111179437922d8b39eeb6b7a3359
Meng Wang <mwang@codeaurora.org>
2016-06-14 13:06:26 +0800

    hal: add support for MSM8976 skup device
    Add changes to support MMS8976 skup device with
    internal codec.Load the mixer paths for using
    MSM8976 skup device.
    
    CRs-Fixed: 1024644
    Change-Id: I6511d08f248bc891c7c95fa19b61cf50742af88f

--- hal/msm8916/platform.c
+++ hal/msm8916/platform.c
@@ -974,6 +974,13 @@ static void query_platform(const char *snd_card_name,
         msm_device_to_be_id = msm_device_to_be_id_internal_codec;
         msm_be_id_array_len  =
             sizeof(msm_device_to_be_id_internal_codec) / sizeof(msm_device_to_be_id_internal_codec[0]);
+    } else if (!strncmp(snd_card_name, "msm8976-skup-snd-card",
+                  sizeof("msm8976-skup-snd-card"))) {
+        strlcpy(mixer_xml_path, MIXER_XML_PATH_SKUN_CAJON,
+                sizeof(MIXER_XML_PATH_SKUN_CAJON));
+        msm_device_to_be_id = msm_device_to_be_id_internal_codec;
+        msm_be_id_array_len  =
+            sizeof(msm_device_to_be_id_internal_codec) / sizeof(msm_device_to_be_id_internal_codec[0]);
     } else {
         strlcpy(mixer_xml_path, MIXER_XML_PATH,
                 sizeof(MIXER_XML_PATH));
