2245647af936dd15c8ba073a0164a04e28d92d7b
Ricardo Cerqueira <ricardo@cyngn.com>
2016-06-20 00:41:47 +0100

    ssr: Fix building with OSS materials
    Change-Id: I466816af5e1258f61401edf3eaf37ed7a862292e

--- hal/audio_extn/ssr.c
+++ hal/audio_extn/ssr.c
@@ -36,7 +36,19 @@
 #include "audio_extn.h"
 #include "platform.h"
 #include "platform_api.h"
+#ifndef _OSS
 #include "surround_rec_interface.h"
+#else
+typedef struct {
+    const char *name;
+    char *(*get_param_fn)(void *h);
+} get_param_data_t;
+
+typedef struct {
+    const char *name;
+    void (*set_param_fn)(void *h, const char *val);
+} set_param_data_t;
+#endif
 
 #ifdef DYNAMIC_LOG_ENABLED
 #include <log_xml_parser.h>
