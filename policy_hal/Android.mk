f33e4cbf60001adfacf047f5487f72e748d3a3aa
Satya Krishna Pindiproli <satyak@codeaurora.org>
2016-06-21 17:56:14 +0530

    audiopolicy: add log to print XML policy config flag
    Add a log message to print the value of the flag that
    enables XML based audio policy configuration. This will
    help in determining the actual policy configuration file
    being loaded (whether XML or text based legacy config file).
    
    Change-Id: I476cc16e89438709bdd6ceea371eed6bdb7b62f4

--- policy_hal/Android.mk
+++ policy_hal/Android.mk
@@ -44,6 +44,10 @@ ifeq ($(strip $(AUDIO_FEATURE_ENABLED_FM_POWER_OPT)),true)
 LOCAL_CFLAGS += -DFM_POWER_OPT
 endif
 
+ifeq ($(USE_XML_AUDIO_POLICY_CONF), 1)
+LOCAL_CFLAGS += -DUSE_XML_AUDIO_POLICY_CONF
+endif
+
 LOCAL_CFLAGS += -Wno-error -fpermissive
 
 LOCAL_MODULE := libaudiopolicymanager
