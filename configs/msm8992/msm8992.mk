5fd1627fafe32c7b1c0e2df3ba6e2d2e8984d03a
Divya Narayanan Poojary <dnaray@codeaurora.org>
2016-06-28 18:56:59 +0530

    hal: move audio specific config files to HAL
    -Move all audio specific config files from device/qcom/msm8xxx
    to hardware/qcom/audio/configs/msm8xxx folder for
    msm8992 and msm8994 targets.
    -audio: configs: add target specific audio policy configuration XML.
    Add target specific audio policy configuration XML file for all targets
    with enhancements such as additional formats under compressed_offload
    output, new device ports such as proxy, line, fm etc., new outputs like
    direct_pcm, raw etc. as per the text based audio_policy.conf file and
    enable its usage by setting the flag USE_XML_AUDIO_POLICY_CONF to 1.
    
    Change-Id: I8a54672baa428d36492db8dd0fdfed8b1cb4a10d

--- configs/msm8992/msm8992.mk
+++ configs/msm8992/msm8992.mk
@@ -1,3 +1,52 @@
 #Audio Specific device overlays
 DEVICE_PACKAGE_OVERLAYS += hardware/qcom/audio/configs/common/overlay
 
+USE_XML_AUDIO_POLICY_CONF := 1
+
+# Audio configuration file
+ifeq ($(TARGET_USES_AOSP), true)
+PRODUCT_COPY_FILES += \
+    device/qcom/common/media/audio_policy.conf:system/etc/audio_policy.conf
+else
+PRODUCT_COPY_FILES += \
+    hardware/qcom/audio/configs/msm8992/audio_policy.conf:system/etc/audio_policy.conf
+endif
+
+PRODUCT_COPY_FILES += \
+    hardware/qcom/audio/configs/msm8992/audio_output_policy.conf:system/vendor/etc/audio_output_policy.conf \
+    hardware/qcom/audio/configs/msm8992/audio_effects.conf:system/vendor/etc/audio_effects.conf \
+    hardware/qcom/audio/configs/msm8992/mixer_paths.xml:system/etc/mixer_paths.xml \
+    hardware/qcom/audio/configs/msm8992/mixer_paths_i2s.xml:system/etc/mixer_paths_i2s.xml \
+    hardware/qcom/audio/configs/msm8992/audio_platform_info.xml:system/etc/audio_platform_info.xml \
+    hardware/qcom/audio/configs/msm8992/audio_platform_info_i2s.xml:system/etc/audio_platform_info_i2s.xml \
+    hardware/qcom/audio/configs/msm8992/sound_trigger_mixer_paths.xml:system/etc/sound_trigger_mixer_paths.xml \
+    hardware/qcom/audio/configs/msm8992/sound_trigger_platform_info.xml:system/etc/sound_trigger_platform_info.xml \
+    hardware/qcom/audio/configs/msm8992/aanc_tuning_mixer.txt:system/etc/aanc_tuning_mixer.txt
+
+#XML Audio configuration files
+ifeq ($(USE_XML_AUDIO_POLICY_CONF), 1)
+ifeq ($(TARGET_USES_AOSP), true)
+PRODUCT_COPY_FILES += \
+    $(TOPDIR)hardware/qcom/audio/configs/common/audio_policy_configuration.xml:/system/etc/audio_policy_configuration.xml
+else
+PRODUCT_COPY_FILES += \
+    $(TOPDIR)hardware/qcom/audio/configs/msm8992/audio_policy_configuration.xml:system/etc/audio_policy_configuration.xml
+endif
+endif
+
+# Listen configuration file
+PRODUCT_COPY_FILES += \
+    $(TOPDIR)hardware/qcom/audio/configs/msm8992/listen_platform_info.xml:system/etc/listen_platform_info.xml
+
+PRODUCT_PACKAGES += \
+    libqcomvisualizer \
+    libqcomvoiceprocessing \
+    libqcompostprocbundle
+
+# Reduce client buffer size for fast audio output tracks
+PRODUCT_PROPERTY_OVERRIDES += \
+    af.fast_track_multiplier=1
+
+# Low latency audio buffer size in frames
+PRODUCT_PROPERTY_OVERRIDES += \
+    audio_hal.period_size=192
