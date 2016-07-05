9a7cdb0e320db066383691b4b32df92117d519f4
Vidyakumar Athota <vathota@codeaurora.org>
2016-07-05 14:44:28 -0700

    configs: add graphite specific config file for msmcobalt
    Add graphite specific config file to update glink channel
    information for MSM to WDSP communication.
    
    Change-Id: I82d1fd9e63000fec453391836d6c3a600ec5c99c

--- configs/msmcobalt/msmcobalt.mk
+++ configs/msmcobalt/msmcobalt.mk
@@ -80,6 +80,7 @@ PRODUCT_COPY_FILES += \
     hardware/qcom/audio/configs/msmcobalt/sound_trigger_mixer_paths.xml:system/etc/sound_trigger_mixer_paths.xml \
     hardware/qcom/audio/configs/msmcobalt/sound_trigger_mixer_paths_wcd9330.xml:system/etc/sound_trigger_mixer_paths_wcd9330.xml \
     hardware/qcom/audio/configs/msmcobalt/sound_trigger_platform_info.xml:system/etc/sound_trigger_platform_info.xml \
+    hardware/qcom/audio/configs/msmcobalt/graphite_ipc_platform_info.xml:system/etc/graphite_ipc_platform_info.xml \
     hardware/qcom/audio/configs/msmcobalt/audio_platform_info.xml:system/etc/audio_platform_info.xml
 
 #XML Audio configuration files
