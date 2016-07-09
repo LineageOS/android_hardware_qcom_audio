bcbe6d99add7a83402c9eba5e5d7274bb08edf86
Grando F <aqdevmain@gmail.com>
2016-07-08 20:19:43 -0700

    Fix Headphone Mic for JF
    "Headset speaker output device and Speaker phone mic input device
    is an invalid device pair combination. Instead, select Handset mic
    input device for Headphones."
    
    Original Author: Narsinga Rao Chella <nrchella@codeaurora.org>
    https://source.codeaurora.org/quic/la/platform/hardware/qcom/audio/commit/?id=97db870693753a7d90036fc19b769dd7e21f6157
    Change-Id: I1bce425bd73d2f79b7dd78a7d5c701c43390b167

--- hal/msm8960/platform.c
+++ hal/msm8960/platform.c
@@ -1548,7 +1548,8 @@ snd_device_t platform_get_input_snd_device(void *platform, audio_devices_t out_d
                 goto exit;
             }
         }
-        if (out_device & AUDIO_DEVICE_OUT_EARPIECE) {
+        if (out_device & AUDIO_DEVICE_OUT_EARPIECE ||
+            out_device & AUDIO_DEVICE_OUT_WIRED_HEADPHONE) {
             if (out_device & AUDIO_DEVICE_OUT_EARPIECE &&
                 audio_extn_should_use_handset_anc(channel_count)) {
                 snd_device = SND_DEVICE_IN_AANC_HANDSET_MIC;
@@ -1568,7 +1569,6 @@ snd_device_t platform_get_input_snd_device(void *platform, audio_devices_t out_d
             else
                 snd_device = SND_DEVICE_IN_BT_SCO_MIC;
         } else if (out_device & AUDIO_DEVICE_OUT_SPEAKER ||
-                out_device & AUDIO_DEVICE_OUT_WIRED_HEADPHONE ||
                 out_device & AUDIO_DEVICE_OUT_LINE) {
             if (my_data->fluence_type != FLUENCE_NONE &&
                 my_data->fluence_in_voice_call &&
@@ -1727,9 +1727,10 @@ snd_device_t platform_get_input_snd_device(void *platform, audio_devices_t out_d
                 snd_device = SND_DEVICE_IN_SPEAKER_STEREO_DMIC;
             else
                 snd_device = SND_DEVICE_IN_SPEAKER_MIC;
-        } else if (out_device & AUDIO_DEVICE_OUT_WIRED_HEADPHONE ||
-                   out_device & AUDIO_DEVICE_OUT_LINE) {
+        } else if (out_device & AUDIO_DEVICE_OUT_LINE) {
             snd_device = SND_DEVICE_IN_SPEAKER_MIC;
+        } else if (out_device & AUDIO_DEVICE_OUT_WIRED_HEADPHONE) {
+            snd_device = SND_DEVICE_IN_HANDSET_MIC;
         } else if (out_device & AUDIO_DEVICE_OUT_BLUETOOTH_SCO_HEADSET) {
             if (my_data->btsco_sample_rate == SAMPLE_RATE_16KHZ)
                 snd_device = SND_DEVICE_IN_BT_SCO_MIC_WB;
