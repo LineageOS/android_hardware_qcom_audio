e6233d41a9955237aacdd608d8972ab35857712e
zaclimon <isaacpateau05@gmail.com>
2016-08-28 13:53:25 -0400

    hal: Ensure device_name is copied if empty
    For an unknown reason, an msm8960 device name gets empty even
    though the value is correct platform side. Fix this copying
    again directly if it's empty.
    
    We don't have to worry about checking if it's valid since a check
    has been performed before regarding it's validity.
    
    Change-Id: I19e6c6e2a1d445259eb5a519b502d368149dc7fa

--- hal/audio_hw.c
+++ hal/audio_hw.c
@@ -704,6 +704,16 @@ int enable_snd_device(struct audio_device *adev,
             goto on_error;
         }
 
+        /*
+         * For some unknown reason, the device name gets empty for
+         * msm8960 devices. Because we're sure that the sound device is valid,
+         * copy it manually and apply the correct route.
+        */
+
+        if (strlen(device_name) == 0) {
+            strcpy(device_name, platform_get_snd_device_name(snd_device));
+        }
+
         ALOGD("%s: snd_device(%d: %s)", __func__, snd_device, device_name);
 
         if (is_a2dp_device(snd_device) &&
@@ -771,6 +781,14 @@ int disable_snd_device(struct audio_device *adev,
             if (platform_get_snd_device_name_extn(adev->platform, snd_device, device_name) < 0 ) {
                 ALOGE(" %s: Invalid sound device returned", __func__);
                 return -EINVAL;
+            }
+
+            /*
+             * Same reason as in enable_snd_device()
+             */
+
+            if (strlen(device_name) == 0) {
+                strcpy(device_name, platform_get_snd_device_name(snd_device));
             }
 
             ALOGD("%s: snd_device(%d: %s)", __func__, snd_device, device_name);
