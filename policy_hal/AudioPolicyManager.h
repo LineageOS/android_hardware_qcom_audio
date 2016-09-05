5a9fd8b485d567ce3f88753797eb8f4bd1d22be2
Michael Bestas <mikeioannina@cyanogenmod.org>
2016-09-05 22:41:01 +0300

    Revert "policy_hal: Fix build with Clang"
    * Not needed if we disable wearable target
    
    This reverts commit 2040c84cccb011ff6f894f3f9bfb802218b834fa.
    
    RM-290
    
    Change-Id: Ibfec14b4bdb85a5d5fca4db41b9f72c36f371433
    (cherry picked from commit 7b2112b2c577da745b40d9be5a4affa20f7d6ead)

--- policy_hal/AudioPolicyManager.h
+++ policy_hal/AudioPolicyManager.h
@@ -91,7 +91,7 @@ protected:
 #else
          status_t checkAndSetVolume(audio_stream_type_t stream,
                                                    int index,
-                                                   const sp<AudioOutputDescriptor>& outputDesc,
+                                                   const sp<SwAudioOutputDescriptor>& outputDesc,
                                                    audio_devices_t device,
                                                    int delayMs = 0, bool force = false);
 #endif
