f13bdd27dbe29e64f87d126f11aa5b6bb3ab984b
Neil Agarwal <regalstreak@gmail.com>
2016-09-13 23:33:19 +0200

    DONOTMERGE: hax | Disable compiler warnings for now
    - Unused function errors in hardware/qcom/audio-caf/msm8952/hal/voice_extn/voice_extn.h and some other files
    
    Change-Id: Id0b2b5151a7a3981ac79d5cb5d9ba1a80f335e1b

--- hal/Android.mk
+++ hal/Android.mk
@@ -282,7 +282,7 @@ ifeq ($(strip $(AUDIO_FEATURE_ENABLED_PM_SUPPORT)),true)
     LOCAL_SHARED_LIBRARIES += libperipheral_client
 endif
 
-LOCAL_CFLAGS += -Wall -Werror
+LOCAL_CFLAGS += -Wall -Werror -Wno-unused-function
 LOCAL_COPY_HEADERS_TO   := mm-audio
 LOCAL_COPY_HEADERS      := audio_extn/audio_defs.h
 
