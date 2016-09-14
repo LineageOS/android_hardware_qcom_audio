c4b7b4643c140f55dba00a78e6e967a62864a8f1
Naresh Tanniru <ntanniru@codeaurora.org>
2016-09-14 11:25:37 +0530

    audiopolicy: Avoid spurious routing in APM::stopSource
    - On AudioOutput::stopSource, ignore routing calls
    on an output descriptor to the same device
    
    Change-Id: I10026d3d9147e45d3ed2b93160922bc3bb4bc2d0

--- policy_hal/AudioPolicyManager.cpp
+++ policy_hal/AudioPolicyManager.cpp
@@ -779,8 +779,8 @@ status_t AudioPolicyManagerCustom::stopSource(sp<AudioOutputDescriptor> outputDe
                         desc->isActive() &&
                         outputDesc->sharesHwModuleWith(desc) &&
                         (newDevice != desc->device())) {
-                        audio_devices_t dev = getNewOutputDevice(mOutputs.valueFor(curOutput), false
- /*fromCache*/);
+                        audio_devices_t dev = getNewOutputDevice(mOutputs.valueFor(curOutput), false /*fromCache*/);
+                        bool force = desc->device() != dev;
                         uint32_t delayMs;
                         if (dev == prevDevice) {
                             delayMs = 0;
@@ -789,7 +789,7 @@ status_t AudioPolicyManagerCustom::stopSource(sp<AudioOutputDescriptor> outputDe
                         }
                         setOutputDevice(desc,
                                     dev,
-                                    true,
+                                    force,
                                     delayMs);
                 }
             }
