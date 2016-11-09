f8647663afbde710e30aec32e88b5b320eb6f05c
Dan Pasanen <invisiblek@cyanogenmod.org>
2016-11-09 08:12:01 -0600

    post_proc: use pathmap for include path
    Change-Id: I094e5965b94c725e36b563cc2679aed8c671d8ab

--- post_proc/Android.mk
+++ post_proc/Android.mk
@@ -56,7 +56,7 @@ LOCAL_MODULE_RELATIVE_PATH := soundfx
 LOCAL_MODULE:= libvolumelistener
 
 LOCAL_C_INCLUDES := \
-        hardware/qcom/audio/hal \
+	$(call project-path-for,qcom-audio)/hal \
 	$(call include-path-for, audio-effects)
 
 LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
