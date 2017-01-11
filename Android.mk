c979d44fd646955516fd388489b2ee73aa326366
Dhananjay Kumar <dhakumar@codeaurora.org>
2017-01-11 21:22:16 +0530

    audio: correct dependencies for compilation with AOSP flag
    Add liblog to the dependency list for voice_processing and
    disable compilation of audiod when AOSP flag is set.
    
    Change-Id: Ie60559806a3ee02e6b84626ba2a3ee89844a1a7a

--- Android.mk
+++ Android.mk
@@ -14,8 +14,11 @@ include $(MY_LOCAL_PATH)/voice_processing/Android.mk
 include $(MY_LOCAL_PATH)/mm-audio/Android.mk
 include $(MY_LOCAL_PATH)/policy_hal/Android.mk
 include $(MY_LOCAL_PATH)/visualizer/Android.mk
-include $(MY_LOCAL_PATH)/audiod/Android.mk
 include $(MY_LOCAL_PATH)/post_proc/Android.mk
+endif
+
+ifneq ($(TARGET_USES_AOSP), true)
+include $(MY_LOCAL_PATH)/audiod/Android.mk
 endif
 
 endif
