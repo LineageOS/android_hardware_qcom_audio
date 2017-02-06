b5d96ce9b48c608ea26adad940a91b6729886c52
Preetam Singh Ranawat <apranawat@codeaurora.org>
2017-02-06 14:10:11 +0530

    hal: Add a2dp state check for device routing
    - During a2dp to sco transition multiple session
      open failures are seen due to a2dp start failure
    
    - Check a2dp streaming state with BT lib before
      routing audio on a2dp device.
      If a2dp is not ready for streaming, force drop
      audio packets for a2dp path similar to BT HAL
    
    Change-Id: If9fdd2184943c2e25a091067477cf0bcc5dce278

--- hal/audio_extn/audio_extn.h
+++ hal/audio_extn/audio_extn.h
@@ -206,7 +206,7 @@ bool audio_extn_usb_is_capture_supported();
 #define audio_extn_a2dp_set_handoff_mode(is_on)          (0)
 #define audio_extn_a2dp_get_apptype_params(sample_rate,bit_width)    (0)
 #define audio_extn_a2dp_get_encoder_latency()            (0)
-
+#define audio_extn_a2dp_is_ready()                       (0)
 #else
 void audio_extn_a2dp_init(void *adev);
 int audio_extn_a2dp_start_playback();
@@ -217,7 +217,7 @@ void audio_extn_a2dp_set_handoff_mode(bool is_on);
 void audio_extn_a2dp_get_apptype_params(uint32_t *sample_rate,
                                         uint32_t *bit_width);
 uint32_t audio_extn_a2dp_get_encoder_latency();
-
+bool audio_extn_a2dp_is_ready();
 #endif
 
 #ifndef SSR_ENABLED
