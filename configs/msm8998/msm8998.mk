5538d2be583f9d7a129f3c9343dc5cf610e9a1a5
vivek mehta <mvivek@codeaurora.org>
2017-02-18 23:36:57 -0800

    audio:config: enable audio HIDL component
    - add audio HIDL component.
    
    Change-Id: I4cb2891a3f241ac54830d5e76392ec7219912993

--- configs/msm8998/msm8998.mk
+++ configs/msm8998/msm8998.mk
@@ -230,3 +230,10 @@ audio.noisy.broadcast.delay=600
 #offload pausetime out duration to 3 secs to inline with other outputs
 PRODUCT_PROPERTY_OVERRIDES += \
 audio.offload.pstimeout.secs=3
+
+# for HIDL related packages
+PRODUCT_PACKAGES += \
+    android.hardware.audio@2.0-service \
+    android.hardware.audio@2.0-impl \
+    android.hardware.audio.effect@2.0-impl \
+    android.hardware.soundtrigger@2.0-impl
