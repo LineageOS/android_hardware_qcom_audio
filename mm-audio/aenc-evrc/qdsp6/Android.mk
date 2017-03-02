b3641a4cd241d0bfa7acd34afd48638e3eb0d494
Aniket Kumar Lata <alata@codeaurora.org>
2017-03-01 19:51:23 -0800

    hal: audio-test: Move omx tests mm-audio-internal
    Moving omx test applications for encoders and decoders to
    mm-audio-internal.
    
    CRs-Fixed: 2014832
    Change-Id: I8df3703dc84f7f277258b036bd6df87946af6efb

--- mm-audio/aenc-evrc/qdsp6/Android.mk
+++ mm-audio/aenc-evrc/qdsp6/Android.mk
@@ -42,33 +42,6 @@ LOCAL_ADDITIONAL_DEPENDENCIES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
 
 include $(BUILD_SHARED_LIBRARY)
 
-# ---------------------------------------------------------------------------------
-#             Make the apps-test (mm-aenc-omxevrc-test)
-# ---------------------------------------------------------------------------------
-
-include $(CLEAR_VARS)
-
-mm-evrc-enc-test-inc    := $(LOCAL_PATH)/inc
-mm-evrc-enc-test-inc    += $(LOCAL_PATH)/test
-mm-evrc-enc-test-inc    += $(TARGET_OUT_HEADERS)/mm-core/omxcore
-ifeq ($(strip $(TARGET_USES_QCOM_MM_AUDIO)),true)
-mm-evrc-enc-test-inc     += $(TARGET_OUT_HEADERS)/mm-audio/audio-alsa 
-endif
-LOCAL_MODULE            := mm-aenc-omxevrc-test
-LOCAL_MODULE_TAGS       := optional
-LOCAL_VENDOR_MODULE     := true
-LOCAL_CFLAGS            := $(libOmxEvrcEnc-def)
-LOCAL_C_INCLUDES        := $(mm-evrc-enc-test-inc)
-LOCAL_PRELINK_MODULE    := false
-LOCAL_SHARED_LIBRARIES  := libmm-omxcore
-LOCAL_SHARED_LIBRARIES  += libOmxEvrcEnc
-ifeq ($(strip $(TARGET_USES_QCOM_MM_AUDIO)),true)
-LOCAL_SHARED_LIBRARIES  += libaudioalsa
-endif
-LOCAL_SRC_FILES         := test/omx_evrc_enc_test.c
-
-include $(BUILD_EXECUTABLE)
-
 endif
 
 # ---------------------------------------------------------------------------------
