b134a6286cc5a5f366c79362faa1f4435e36a422
Vidyakumar Athota <vathota@codeaurora.org>
2017-03-16 11:11:31 -0700

    hal: send audio calibration for VI capture path
    Audio calibration is not sent for VI capture path because the
    input stream is NULL. This results failure in speaker thermal
    calibration. Send audio calibration with default app_type for
    VI capture path.
    
    Change-Id: Ic3c5d46b5de4f9b914a9fc880726d7658ce3ca8b

--- hal/audio_extn/utils.c
+++ hal/audio_extn/utils.c
@@ -1261,7 +1261,7 @@ void audio_extn_utils_send_audio_calibration(struct audio_device *adev,
         platform_send_audio_calibration(adev->platform, usecase,
                          usecase->stream.in->app_type_cfg.app_type,
                          usecase->stream.in->app_type_cfg.sample_rate);
-    } else if (type == PCM_HFP_CALL) {
+    } else if (type == PCM_HFP_CALL || type == PCM_CAPTURE) {
         /* when app type is default. the sample rate is not used to send cal */
         platform_send_audio_calibration(adev->platform, usecase,
                          platform_get_default_app_type_v2(adev->platform, usecase->type),
