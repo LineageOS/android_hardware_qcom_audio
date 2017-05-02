e75e9eeb7944f95ed37d7c35b6fc9b9ee1046d93
Kevin Rocard <krocard@google.com>
2017-05-02 16:38:39 -0700

    HAL: Align error code with HIDL wrapper
    Here is how the HIDL wrapper interprets status error codes:
    0         => Result::OK
    -EINVAL   => Result::INVALID_ARGUMENTS
    -ENODATA  => Result::INVALID_STATE
    -ENODEV   => Result::NOT_INITIALIZED
    -ENOSYS   => Result::NOT_SUPPORTED
    otherwise => Result::INVALID_STATE
    
    This is consistent with most of the hal.
    This patch changes the few places where it was not to unify the
    behaviours.
    
    Test: vts-tradefed run vts --module VtsHalAudioV2_0Target
    Test: call/play music/record/video...
    Bug: 36311550
    Change-Id: Id821d15ae3269cd2c9699924e2b174c7347a6d07
    Signed-off-by: Kevin Rocard <krocard@google.com>

--- hal/audio_hw.c
+++ hal/audio_hw.c
@@ -2623,7 +2623,7 @@ static int out_get_render_position(const struct audio_stream_out *stream,
         *dsp_frames = out->written;
         return 0;
     } else
-        return -EINVAL;
+        return -ENODATA;
 }
 
 static int out_add_audio_effect(const struct audio_stream *stream __unused,
@@ -2648,7 +2648,7 @@ static int out_get_presentation_position(const struct audio_stream_out *stream,
                                    uint64_t *frames, struct timespec *timestamp)
 {
     struct stream_out *out = (struct stream_out *)stream;
-    int ret = -EINVAL;
+    int ret = -ENODATA;
     unsigned long dsp_frames;
 
     /* below piece of code is not guarded against any lock because audioFliner serializes
@@ -3022,7 +3022,7 @@ static char* in_get_parameters(const struct audio_stream *stream,
 static int in_set_gain(struct audio_stream_in *stream __unused,
                        float gain __unused)
 {
-    return 0;
+    return -ENOSYS;
 }
 
 static ssize_t in_read(struct audio_stream_in *stream, void *buffer,
