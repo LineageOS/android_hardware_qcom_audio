b38e69cbc8b5ce7209471b46c75292025c57753d
Ben Romberger <bromberg@codeaurora.org>
2017-06-27 15:55:21 -0700

    audio: Remove policy hal directory
    Remove policy hal directory and move it to the new
    qcom-opensource audio project.
    
    Change-Id: I0c1d1845b90e4938194868f5ab317694d5722f0b

--- Android.mk
+++ Android.mk
@@ -14,7 +14,6 @@ include $(MY_LOCAL_PATH)/hal/Android.mk
 endif
 include $(MY_LOCAL_PATH)/voice_processing/Android.mk
 include $(MY_LOCAL_PATH)/mm-audio/Android.mk
-include $(MY_LOCAL_PATH)/policy_hal/Android.mk
 include $(MY_LOCAL_PATH)/visualizer/Android.mk
 include $(MY_LOCAL_PATH)/post_proc/Android.mk
 include $(MY_LOCAL_PATH)/qahw_api/Android.mk
