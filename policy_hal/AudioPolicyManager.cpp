1415c96fb9d19a757db3b4c2735071d9049c6e3d
XiNGRZ <chenxingyu92@gmail.com>
2017-06-28 23:14:21 +0800

    fix build error while we don't have an FM
    Change-Id: I54fa1f7b4862b6a3e88ac84151d42b5e259d6ded

--- policy_hal/AudioPolicyManager.cpp
+++ policy_hal/AudioPolicyManager.cpp
@@ -2304,9 +2304,11 @@ AudioPolicyManagerCustom::AudioPolicyManagerCustom(AudioPolicyClientInterface *c
     : AudioPolicyManager(clientInterface),
       mHdmiAudioDisabled(false),
       mHdmiAudioEvent(false),
-      mPrevPhoneState(0),
+#ifdef FM_POWER_OPT
       mPrevFMVolumeDb(0.0f),
-      mFMIsActive(false)
+      mFMIsActive(false),
+#endif
+      mPrevPhoneState(0)
 {
 
 #ifdef USE_XML_AUDIO_POLICY_CONF
