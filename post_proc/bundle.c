fa595db1f552aeccf0893bd3764c1c3c1872b6a4
Steven Moreland <smoreland@google.com>
2017-06-30 11:34:12 -0700

    Make libqcompostprocbundle build with the VNDK.
    When the vndk is enabled (BOARD_VNDK_VERSION=current),
    vendor libraries must only link against vendor variants
    in the build system. This means that they receive restricted
    sets of headers and that automatic global headers are restricted.
    
    Test: build libqcompostprocbundle with BOARD_VNDK_VERSION=current
    Bug: 37342627
    Change-Id: I213c4937f64e811d8018806982dc299c3e7e668a

--- post_proc/bundle.c
+++ post_proc/bundle.c
@@ -38,7 +38,9 @@
 #define LOG_TAG "offload_effect_bundle"
 //#define LOG_NDEBUG 0
 
+#include <pthread.h>
 #include <stdlib.h>
+
 #include <cutils/list.h>
 #include <cutils/log.h>
 #include <system/thread_defs.h>
