codeaurora-unoffical/platform-hardware-qcom-audio

hal: Add matrix limiter support
Matrix limiter is used to prevent saturation
when mixing multiple audio streams. It is
enabled when both the BD output flag is set
and the property audio.matrix.limiter.enable
is set to true.

Change-Id: Ibf06bd2444874ae555e98a26ed645f2455a3e1a3

hal/audio_extn/utils.c
@@ -117,6 +117,7 @@ const struct string_to_enum s_flag_name_to_enum_table[] = {
 #endif
     STRING_TO_ENUM(AUDIO_OUTPUT_FLAG_COMPRESS_PASSTHROUGH),
     STRING_TO_ENUM(AUDIO_OUTPUT_FLAG_TIMESTAMP),
+    STRING_TO_ENUM(AUDIO_OUTPUT_FLAG_BD10),
     STRING_TO_ENUM(AUDIO_OUTPUT_FLAG_INTERACTIVE),
     STRING_TO_ENUM(AUDIO_INPUT_FLAG_NONE),
     STRING_TO_ENUM(AUDIO_INPUT_FLAG_FAST),

LineageOS/android_hardware_qcom_audio

hal/audio_extn/utils.c
@@ -126,6 +126,7 @@ const struct string_to_enum s_flag_name_to_enum_table[] = {
     STRING_TO_ENUM(AUDIO_OUTPUT_FLAG_COMPRESS_PASSTHROUGH),
     STRING_TO_ENUM(AUDIO_OUTPUT_FLAG_TIMESTAMP),
     STRING_TO_ENUM(AUDIO_OUTPUT_FLAG_VOIP_RX),
+    STRING_TO_ENUM(AUDIO_OUTPUT_FLAG_BD),
     STRING_TO_ENUM(AUDIO_OUTPUT_FLAG_INTERACTIVE),
     STRING_TO_ENUM(AUDIO_INPUT_FLAG_NONE),
     STRING_TO_ENUM(AUDIO_INPUT_FLAG_FAST),