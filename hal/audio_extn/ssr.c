018f2890b536d12de442c474c1fa80f87d9c6381
Divya Narayanan Poojary <dnaray@codeaurora.org>
2017-09-27 15:50:37 +0530

    audio: Enable surround sound and WMA offload
    -Enable SSR and WMA_OFFLOAD flag
    -Fixed compilation issue, Update channel index used for
     surround sound recording as per latest channel mapping index
    
    Change-Id: I217d81a0defc2aca6bd1bf93cffdd1e17892e32d

--- hal/audio_extn/ssr.c
+++ hal/audio_extn/ssr.c
@@ -358,15 +358,14 @@ int audio_extn_ssr_set_usecase(struct stream_in *in,
                   audio_channel_mask_get_representation(in->channel_mask);
     *update_params = false;
 
-//if (audio_extn_ssr_check_usecase(in)) {
-
-  //    if (representation == AUDIO_CHANNEL_REPRESENTATION_INDEX) {
+    if (audio_extn_ssr_check_usecase(in)) {
+        if (representation == AUDIO_CHANNEL_REPRESENTATION_INDEX) {
             /* update params in case channel representation index.
              * on returning error, flinger will retry with supported representation passed
              */
-    /*      ALOGD("%s: SSR supports only channel representation position, channel_mask(%#x)"
+            ALOGD("%s: SSR supports only channel representation position, channel_mask(%#x)"
                               ,__func__, config->channel_mask);
-            config->channel_mask = AUDIO_CHANNEL_IN_5POINT1;
+            config->channel_mask = AUDIO_CHANNEL_IN_6;
             ret = 0;
             *update_params = true;
         } else {
@@ -377,7 +376,7 @@ int audio_extn_ssr_set_usecase(struct stream_in *in,
                 ALOGE("%s: Unable to start SSR record session", __func__);
             }
         }
-    }*/
+    }
     return ret;
 }
 
