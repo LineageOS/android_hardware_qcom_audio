ddd67d43d5aeeccd78cb4d36469efbb55f22998a
yidongh <yidongh@codeaurora.org>
2017-10-15 22:18:12 +0800

    hal: audio-test: remove code sections referring to audio-alsa
    audio-alsa is removed from mm-audio, reomve code sections
    referring to audio-alsa.
    
    CRs-Fixed: 2126971
    Change-Id: I50bd466a931a39846dc2fc5f136a38a9f64b2148

--- mm-audio/aenc-qcelp13/qdsp6/Android.mk
+++ mm-audio/aenc-qcelp13/qdsp6/Android.mk
@@ -54,7 +54,6 @@ mm-qcelp13-enc-test-inc    := $(LOCAL_PATH)/inc
 mm-qcelp13-enc-test-inc    += $(LOCAL_PATH)/test
 
 mm-qcelp13-enc-test-inc    += $(TARGET_OUT_HEADERS)/mm-core/omxcore
-mm-qcelp13-enc-test-inc    += $(TARGET_OUT_HEADERS)/mm-audio/audio-alsa
 LOCAL_MODULE            := mm-aenc-omxqcelp13-test
 LOCAL_MODULE_TAGS       := optional
 LOCAL_CFLAGS            := $(libOmxQcelp13Enc-def)
@@ -62,7 +61,6 @@ LOCAL_C_INCLUDES        := $(mm-qcelp13-enc-test-inc)
 LOCAL_PRELINK_MODULE    := false
 LOCAL_SHARED_LIBRARIES  := libmm-omxcore
 LOCAL_SHARED_LIBRARIES  += libOmxQcelp13Enc
-LOCAL_SHARED_LIBRARIES  += libaudioalsa
 LOCAL_SRC_FILES         := test/omx_qcelp13_enc_test.c
 
 include $(BUILD_EXECUTABLE)
