6f862824e13b1254e615920abbed21ec5c151194
Zhou Song <zhous@codeaurora.org>
2017-11-06 17:27:57 +0800

    hal: always allow afe proxy for WFD if USB tunnel mode is enabled
    Allow afe proxy consistently for WFD if the platforms support USB
    tunnel mode.
    
    CRs-Fixed: 2139116
    Change-Id: I0b8f59440b9d18426b8e15d41183928d912df77e

--- hal/audio_extn/audio_extn.h
+++ hal/audio_extn/audio_extn.h
@@ -216,6 +216,7 @@ int32_t audio_extn_get_afe_proxy_channel_count();
 #define audio_extn_usb_get_max_channels(p)                             (0)
 #define audio_extn_usb_get_max_bit_width(p)                            (0)
 #define audio_extn_usb_get_sup_sample_rates(t, s, l)                   (0)
+#define audio_extn_usb_is_tunnel_supported()                           (0)
 #else
 void audio_extn_usb_init(void *adev);
 void audio_extn_usb_deinit();
@@ -232,6 +233,7 @@ bool audio_extn_usb_is_capture_supported();
 int audio_extn_usb_get_max_channels(bool playback);
 int audio_extn_usb_get_max_bit_width(bool playback);
 int audio_extn_usb_get_sup_sample_rates(int type, uint32_t *sr, uint32_t l);
+bool audio_extn_usb_is_tunnel_supported();
 #endif
 
 #ifndef SPLIT_A2DP_ENABLED
