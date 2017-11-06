1b2aa2ac94a6c7b0274c43888a6d4450df84c5c9
Chih-Hung Hsieh <chh@google.com>
2017-11-06 11:39:27 -0800

    Use -Werror in hardware/qcom/audio
    * Suppress existing warnings.
    
    Bug: 66996870
    Test: build with WITH_TIDY=1
    Change-Id: I389b943471ccdcfc9b7db4411fa3973d77303a27

--- voice_processing/Android.mk
+++ voice_processing/Android.mk
@@ -11,6 +11,12 @@ LOCAL_VENDOR_MODULE := true
 LOCAL_SRC_FILES:= \
     voice_processing.c
 
+LOCAL_CFLAGS += \
+    -Wall \
+    -Werror \
+    -Wno-unused-function \
+    -Wno-unused-variable \
+
 LOCAL_C_INCLUDES += \
     $(call include-path-for, audio-effects)
 
