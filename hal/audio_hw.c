cfbf603c239009a83b478aa21a10ff19a88afc68
Wei Wang <wvw@google.com>
2017-11-21 14:51:20 -0800

    audio-hal: use powerHAL 1.2 to do performance hint
    1) Add PowerHAL 1.2 client
    2) call PowerHAL powerhint on msm8998 instead of perfd
    
    Test: Play audio and see powerhint handled
    Bug: 62041945
    Change-Id: I33ab8eaa7a3a3a953fb6531e0864b4ed62387ba2

--- hal/audio_hw.c
+++ hal/audio_hw.c
@@ -73,6 +73,7 @@
 #include "platform_api.h"
 #include <platform.h>
 #include "audio_extn.h"
+#include "audio_perf.h"
 #include "voice_extn.h"
 #include "audio_amplifier.h"
 #include "ultrasound.h"
@@ -2198,6 +2199,7 @@ int start_input_stream(struct stream_in *in)
     uc_info->out_snd_device = SND_DEVICE_NONE;
 
     list_add_tail(&adev->usecase_list, &uc_info->list);
+    audio_streaming_hint_start();
     audio_extn_perf_lock_acquire(&adev->perf_lock_handle, 0,
                                  adev->perf_lock_opts,
                                  adev->perf_lock_opts_size);
@@ -2275,12 +2277,14 @@ int start_input_stream(struct stream_in *in)
     }
 
 done_open:
+    audio_streaming_hint_end();
     audio_extn_perf_lock_release(&adev->perf_lock_handle);
     ALOGD("%s: exit", __func__);
 
     return ret;
 
 error_open:
+    audio_streaming_hint_end();
     audio_extn_perf_lock_release(&adev->perf_lock_handle);
     stop_input_stream(in);
 error_config:
@@ -2718,6 +2722,7 @@ int start_output_stream(struct stream_out *out)
     uc_info->out_snd_device = SND_DEVICE_NONE;
     list_add_tail(&adev->usecase_list, &uc_info->list);
 
+    audio_streaming_hint_start();
     audio_extn_perf_lock_acquire(&adev->perf_lock_handle, 0,
                                  adev->perf_lock_opts,
                                  adev->perf_lock_opts_size);
@@ -2885,6 +2890,7 @@ int start_output_stream(struct stream_out *out)
         }
     }
 
+    audio_streaming_hint_end();
     audio_extn_perf_lock_release(&adev->perf_lock_handle);
     ALOGD("%s: exit", __func__);
 
@@ -2896,6 +2902,7 @@ int start_output_stream(struct stream_out *out)
 
     return ret;
 error_open:
+    audio_streaming_hint_end();
     audio_extn_perf_lock_release(&adev->perf_lock_handle);
     stop_output_stream(out);
 error_config:
