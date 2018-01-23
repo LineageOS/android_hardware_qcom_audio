d81113313135a11f63dd5e3c139640797dc7c471
Soumil Shah <soumil_92@codeaurora.org>
2018-01-22 17:39:48 -0800

    HACK: Modify .mk files to avoid strict compiler errors
    Change-Id: I95dd11f96d8aaad31078b599eca1745be912b488

--- voice_processing/Android.mk
+++ voice_processing/Android.mk
@@ -22,4 +22,15 @@ LOCAL_SHARED_LIBRARIES += libdl
 
 LOCAL_CFLAGS += -fvisibility=hidden
 
+LOCAL_CFLAGS += -Wno-unused-variable
+LOCAL_CFLAGS += -Wno-sign-compare
+LOCAL_CFLAGS += -Wno-unused-parameter
+LOCAL_CFLAGS += -Wno-unused-label
+LOCAL_CFLAGS += -Wno-gnu-designator
+LOCAL_CFLAGS += -Wno-typedef-redefinition
+LOCAL_CFLAGS += -Wno-shorten-64-to-32
+LOCAL_CFLAGS += -Wno-tautological-compare
+LOCAL_CFLAGS += -Wno-unused-function
+LOCAL_CFLAGS += -Wno-unused-local-typedef
+
 include $(BUILD_SHARED_LIBRARY)
