55e4785f9abdf9b180da82310a56c8f6585bd45b
Abhishek Arpure <aarpure@codeaurora.org>
2018-01-31 21:29:03 +0530

    Bypass compilation errors due to compiler upgrade
    Bypass CLANG compilation errors by adding ignore flags
    
    Change-Id: I99317ca2ccf5ee953ca5796e01db63dd396d622d

--- post_proc/Android.mk
+++ post_proc/Android.mk
@@ -22,6 +22,17 @@ ifeq ($(strip $(AUDIO_FEATURE_ENABLED_HW_ACCELERATED_EFFECTS)),true)
 endif
 
 LOCAL_CFLAGS+= -O2 -fvisibility=hidden
+LOCAL_CFLAGS += -Wno-unused-variable
+LOCAL_CFLAGS += -Wno-sign-compare
+LOCAL_CFLAGS += -Wno-unused-parameter
+LOCAL_CFLAGS += -Wno-unused-label
+LOCAL_CFLAGS += -Wno-gnu-designator
+LOCAL_CFLAGS += -Wno-typedef-redefinition
+LOCAL_CFLAGS += -Wno-shorten-64-to-32
+LOCAL_CFLAGS += -Wno-tautological-compare
+LOCAL_CFLAGS += -Wno-unused-function
+LOCAL_CFLAGS += -Wno-unused-local-typedef
+LOCAL_CFLAGS += -Wno-format
 
 ifeq ($(strip $(AUDIO_FEATURE_ENABLED_DTS_EAGLE)), true)
     LOCAL_CFLAGS += -DDTS_EAGLE
