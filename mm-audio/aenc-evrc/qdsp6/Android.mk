c037c5e9507a675fb992a27a51ca87a68ab6d780
Arne Coucheron <arco68@gmail.com>
2018-02-17 12:08:15 +0100

    hal: Use LOCAL_VENDOR_MODULE
    Change-Id: I922ecc2bd9e70c2dd8eb79d00b8b5921ea797271

--- mm-audio/aenc-evrc/qdsp6/Android.mk
+++ mm-audio/aenc-evrc/qdsp6/Android.mk
@@ -26,6 +26,7 @@ libOmxEvrcEnc-inc       += $(TARGET_OUT_HEADERS)/mm-core/omxcore
 
 LOCAL_MODULE            := libOmxEvrcEnc
 LOCAL_MODULE_TAGS       := optional
+LOCAL_VENDOR_MODULE     := true
 LOCAL_CFLAGS            := $(libOmxEvrcEnc-def)
 LOCAL_C_INCLUDES        := $(libOmxEvrcEnc-inc)
 LOCAL_PRELINK_MODULE    := false
