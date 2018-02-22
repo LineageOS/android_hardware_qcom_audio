LineageOS/android_hardware_qcom_audio

commit f88cdc459be13c0c9e39e04ca4a8acc853a74fa3

Pull out commonly used constants to <audio_utils/clock.h>
Test: builds OK
Change-Id: Ia33722b53287ce0e52d04118e4f6fb856b466cf1

hal/audio_hw.c
@@ -99,8 +99,6 @@ static unsigned int configured_low_latency_capture_period_size =
 #define MMAP_PERIOD_COUNT_MAX 512
 #define MMAP_PERIOD_COUNT_DEFAULT (MMAP_PERIOD_COUNT_MAX)
 
-static const int64_t NANOS_PER_SECOND = 1000000000;
-
 /* This constant enables extended precision handling.
  * TODO The flag is off until more testing is done.
  */

commit 5a59dff461409983f5fe910aaa0b9b79a2064798

Fix overflow during 32-bit integer multiplication
Regression was introduced by commit f88cdc459be13c0c9e39e04ca4a8acc853a74fa3

Test: see bug
Bug: 73619529
Change-Id: I91c6f496e2f2a0b79cdac2fcf7e203c5186fc2f8

hal/audio_hw.c
@@ -2722,7 +2722,7 @@ static ssize_t out_write(struct audio_stream_out *stream, const void *buffer,
             }
             ALOGVV("%s: writing buffer (%zu bytes) to pcm device", __func__, bytes_to_write);
 
-            long ns = (frames * NANOS_PER_SECOND) / out->config.rate;
+            long ns = (frames * (int64_t) NANOS_PER_SECOND) / out->config.rate;
             request_out_focus(out, ns);
 
             bool use_mmap = is_mmap_usecase(out->usecase) || out->realtime;