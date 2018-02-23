0ee0d3d88e3b42be4bd8c64b228808ffb4ed9930
Banajit Goswami <bgoswami@codeaurora.org>
2018-02-23 09:54:30 -0800

    hal: add audio kernel header dependency
    Add dependency on audio kernel headers for cases where
    audio kernel drivers are compiled as DLKM.
    
    Change-Id: I65a45c3800df4f10481aca27be15f9be4a8a6438

--- post_proc/Android.mk
+++ post_proc/Android.mk
@@ -65,7 +65,8 @@ LOCAL_C_INCLUDES := \
 	$(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/techpack/audio/include \
         $(call include-path-for, audio-effects)
 
-ifneq ($(filter sdm670 qcs605,$(TARGET_BOARD_PLATFORM)),)
+ifneq ($(filter sdm670 qcs605 msmnile,$(TARGET_BOARD_PLATFORM)),)
+  LOCAL_HEADER_LIBRARIES := audio_kernel_headers
   LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/vendor/qcom/opensource/audio-kernel/include
   LOCAL_ADDITIONAL_DEPENDENCIES += $(BOARD_VENDOR_KERNEL_MODULES)
 endif
@@ -162,7 +163,8 @@ LOCAL_C_INCLUDES := \
         hardware/qcom/audio/hal/audio_extn \
         external/tinycompress/include
 
-ifneq ($(filter sdm670 qcs605,$(TARGET_BOARD_PLATFORM)),)
+ifneq ($(filter sdm670 qcs605 msmnile,$(TARGET_BOARD_PLATFORM)),)
+  LOCAL_HEADER_LIBRARIES := audio_kernel_headers
   LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/vendor/qcom/opensource/audio-kernel/include
   LOCAL_ADDITIONAL_DEPENDENCIES += $(BOARD_VENDOR_KERNEL_MODULES)
 endif
