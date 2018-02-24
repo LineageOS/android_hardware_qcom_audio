b8af9a6d83a74678bc5e49b046243ed34a9ce208
Banajit Goswami <bgoswami@codeaurora.org>
2018-02-24 12:57:55 -0800

    hal: add audio kernel header dependency only for msmnile
    Add audio kernel headers dependency only for msmnile target.
    
    Change-Id: Ie5e6116c4fadcf2b296cd56dbe8a1382d4a3508c

--- mm-audio/aenc-aac/qdsp6/Android.mk
+++ mm-audio/aenc-aac/qdsp6/Android.mk
@@ -40,8 +40,10 @@ LOCAL_SRC_FILES         += src/omx_aac_aenc.cpp
 LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
 LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/techpack/audio/include
 LOCAL_ADDITIONAL_DEPENDENCIES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
-ifneq ($(filter sdm670 qcs605 msmnile,$(TARGET_BOARD_PLATFORM)),)
+ifneq ($(filter msmnile,$(TARGET_BOARD_PLATFORM)),)
   LOCAL_HEADER_LIBRARIES := audio_kernel_headers
+endif
+ifneq ($(filter sdm670 qcs605 msmnile,$(TARGET_BOARD_PLATFORM)),)
   LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/vendor/qcom/opensource/audio-kernel/include
   LOCAL_ADDITIONAL_DEPENDENCIES += $(BOARD_VENDOR_KERNEL_MODULES)
 endif
