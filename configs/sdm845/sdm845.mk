4ee3ca3c6a3c3f17ade2b12d11ee50f9b6d18ac5
Dallas Delaney <ddelaney@codeaurora.org>
2018-04-05 16:51:32 -0700

    sdm845: Update sound trigger HIDL version to 2.1
    Sound trigger HIDL interface supports loading of more
    than 1MB sound models from 2.1 version. Update to use HIDL
    version 2.1.
    
    Change-Id: Iae4c277d7b5ac502331b6e9fcad214562b897fc0

--- configs/sdm845/sdm845.mk
+++ configs/sdm845/sdm845.mk
@@ -240,4 +240,4 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-service \
     android.hardware.audio@2.0-impl \
     android.hardware.audio.effect@2.0-impl \
-    android.hardware.soundtrigger@2.0-impl
+    android.hardware.soundtrigger@2.1-impl
