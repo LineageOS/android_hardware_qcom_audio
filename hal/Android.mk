f6bf6438ceaf886330a7396bfa160c61f63ef4e6
Michael Bestas <mkbestas@lineageos.org>
2018-04-16 22:24:09 +0300

    Revert "audio: msm8916: Squashed audio amp commits"
    * A cleaner audio_extn implementation will be picked
    
    This reverts commit 318d9612b5bd88cd757226a985681105cbfce352.
    
    Change-Id: I89384fc0b21492b8e5fe4d36ec617bf4a07429dd

--- hal/Android.mk
+++ hal/Android.mk
@@ -246,7 +246,6 @@ endif
 LOCAL_SHARED_LIBRARIES := \
         liblog \
         libcutils \
-        libhardware \
         libtinyalsa \
         libtinycompress_vendor \
         libaudioroute \
@@ -258,7 +257,6 @@ LOCAL_C_INCLUDES += \
         external/tinyalsa/include \
         external/tinycompress/include \
         external/expat/lib \
-        hardware/libhardware/include \
         system/media/audio_utils/include \
         $(call include-path-for, audio-route) \
         $(call include-path-for, audio-effects) \
