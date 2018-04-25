45b368ef860a36e92305933d2caa9a63e7815684
Paul Keith <javelinanddart@gmail.com>
2018-04-25 03:03:03 +0200

    hal: msim_voice_extn: Set msim_phone_type based on phone_type parameter
    * This is consistently cp1 on sim1 and cp2 on sim2, so drop all the
      confusing and unnecessary property_get calls, and just use the param
    
    Change-Id: I4a59ebd177d46b47e00067af7e6dce49d3e7e676

--- hal/voice_extn/msim_voice_extn.c
+++ hal/voice_extn/msim_voice_extn.c
@@ -33,14 +33,6 @@
 #define AUDIO_PARAMETER_VALUE_CP1 "cp1"
 #define AUDIO_PARAMETER_VALUE_CP2 "cp2"
 
-#ifdef HTC_DUAL_SIM
-#define RADIO_PREFER_NETWORK_SLOT0 "persist.radio.prefer.network"
-#define RADIO_PREFER_NETWORK_SLOT1 "persist.radio.prefer.nw.sub"
-#elif SAMSUNG_DUAL_SIM
-#define AUDIO_PROPERTY_SEC_VSID1 "gsm.current.vsid"
-#define AUDIO_PROPERTY_SEC_VSID2 "gsm.current.vsid2"
-#endif
-
 #define VOICE2_VSID 0x10DC1000
 
 #define VOICE2_SESS_IDX (VOICE_SESS_IDX + 1)
@@ -87,22 +79,7 @@ int msim_voice_extn_set_parameters(struct audio_device *adev __unused,
     ret = str_parms_get_str(parms, AUDIO_PARAMETER_KEY_PHONETYPE, value,
                             sizeof(value));
     if (ret >= 0) {
-#ifdef HTC_DUAL_SIM
-        voice_slot = property_get_int32(RADIO_PREFER_NETWORK_SLOT0) != 1 ? 0 : 1;
-        if (property_get_int32(RADIO_PREFER_NETWORK_SLOT0) ==
-                property_get_int32(RADIO_PREFER_NETWORK_SLOT1)) {
-            voice_slot = 0;
-        }
-        if (strcmp(value, AUDIO_PARAMETER_VALUE_CP2)) {
-            msim_phone_type = voice_slot == 0 ? 1 : 0;
-        } else {
-            msim_phone_type = voice_slot == 0 ? 0 : 1;
-        }
-#elif SAMSUNG_DUAL_SIM
-        msim_phone_type = property_get_int32(
-                strcmp(value, AUDIO_PARAMETER_VALUE_CP2) ?
-                AUDIO_PROPERTY_SEC_VSID1 : AUDIO_PROPERTY_SEC_VSID2) + 1;
-#endif
+        msim_phone_type = strcmp(value, AUDIO_PARAMETER_VALUE_CP2) ? 1 : 2;
         ALOGV("%s: phone_type: %d", __func__, msim_phone_type);
     }
 
