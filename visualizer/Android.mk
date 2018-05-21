e43a121ad58eda24658f9c247ba8123b3ed5c32d
Craig Tatlor <ctatlor97@gmail.com>
2018-05-21 17:45:11 +0100

    visualizer: Build with BOARD_VNDK_VERSION
    Adds missing libraries and headers.
    
    Change-Id: Ib5039945fd0d29cf28bd057e9cae3e780195beb3

--- visualizer/Android.mk
+++ visualizer/Android.mk
@@ -21,9 +21,13 @@ LOCAL_SRC_FILES:= \
 
 LOCAL_CFLAGS+= -O2 -fvisibility=hidden
 
+LOCAL_HEADER_LIBRARIES := \
+	libsystem_headers
+
 LOCAL_SHARED_LIBRARIES := \
 	libcutils \
 	liblog \
+	libhardware \
 	libtinyalsa
 
 LOCAL_MODULE_RELATIVE_PATH := soundfx
