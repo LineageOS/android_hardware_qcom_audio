6090b00ac9b196f603e5f126c1c6ca7021ff9df6
Lakshman Chaluvaraju <lchalu@codeaurora.org>
2018-07-25 18:09:15 +0530

    configs: MSM8953_64/8937_32go/8996: HAL compatibility for P upgrade
    HAL compatibility for P upgrade.
    Include the required product packages.
    
    Change-Id: I4de0c149628c1fd4b6e15786bcf3f6ef909968c9
    CRs-Fixed: 2272748

--- configs/msm8937/msm8937.mk
+++ configs/msm8937/msm8937.mk
@@ -240,4 +240,10 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-service \
     android.hardware.audio@2.0-impl \
     android.hardware.audio.effect@2.0-impl \
-    android.hardware.soundtrigger@2.0-impl
+    android.hardware.soundtrigger@2.0-impl \
+    android.hardware.audio@4.0 \
+    android.hardware.audio.common@4.0 \
+    android.hardware.audio.common@4.0-util \
+    android.hardware.audio@4.0-impl \
+    android.hardware.audio.effect@4.0 \
+    android.hardware.audio.effect@4.0-impl
