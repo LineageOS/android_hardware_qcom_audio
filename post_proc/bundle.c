dc9a485052efa066c5ac14c6d217405bf39103b9
Nicholas Lim <nicknitewolf@lineageos.org>
2018-08-09 14:12:39 +0800

    post_proc: Mark unused parameters with (void)
    Change-Id: Ia7864572f6144dbc31941d74d2150a3152676a86

--- post_proc/bundle.c
+++ post_proc/bundle.c
@@ -460,12 +460,15 @@ __attribute__ ((visibility ("default")))
 void offload_effects_bundle_get_parameters(struct str_parms *query,
                                            struct str_parms *reply)
 {
+    (void)query;
+    (void)reply;
     asphere_get_parameters(query, reply);
 }
 
 __attribute__ ((visibility ("default")))
 void offload_effects_bundle_set_parameters(struct str_parms *parms)
 {
+    (void)parms;
     asphere_set_parameters(parms);
 }
 
