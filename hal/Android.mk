a3d21989a15abc732da1a205581fb0ff86af9244
Rashed Abdel-Tawab <rashed@linux.com>
2018-08-21 21:59:07 -0700

    audio: Guard power@1.2 hints
    Not every device will implement these, so guard them behind
    AUDIO_FEATURE_ENABLED_PERF_HINTS
    
    Change-Id: If2f4e373f1ab2924d84b4a18ddce9f68e301e7c4

--- hal/Android.mk
+++ hal/Android.mk
@@ -437,8 +437,11 @@ ifeq ($(strip $(AUDIO_FEATURE_ENABLED_SND_MONITOR)), true)
     LOCAL_SRC_FILES += audio_extn/sndmonitor.c
 endif
 
+ifeq ($(strip $(AUDIO_FEATURE_ENABLED_PERF_HINTS)), true)
 LOCAL_SHARED_LIBRARIES += libbase libhidlbase libhwbinder libutils android.hardware.power@1.2 liblog
 LOCAL_SRC_FILES += audio_perf.cpp
+LOCAL_CFLAGS += -DPERF_HINTS_ENABLED
+endif
 
 LOCAL_HEADER_LIBRARIES += libhardware_headers
 
