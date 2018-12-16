2f2a7c19e314a6ce6c63f2261baf784d1f7835a0
sjllls <2012ji123@gmail.com>
2018-12-16 21:20:56 +0800

    Fix build error: unused-parameter

--- hal/Android.mk
+++ hal/Android.mk
@@ -260,6 +260,7 @@ ifeq ($(strip $(AUDIO_FEATURE_ENABLED_AUXPCM_BT)),true)
 endif
 
 LOCAL_CFLAGS += -Wall -Werror
+LOCAL_CLANG_CFLAGS += -Wno-unused-variable -Wno-missing-field-initializers -Wno-unused-parameter
 
 LOCAL_COPY_HEADERS_TO   := mm-audio
 LOCAL_COPY_HEADERS      := audio_extn/audio_defs.h
