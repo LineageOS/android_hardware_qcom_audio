2330032c946c49addd265b4c93686f264f3ffe4a
Aniket Kumar Lata <alata@codeaurora.org>
2019-02-20 22:25:30 -0800

    hal: Fix reconfigA2dp set parameter failure
    Fix reconfigA2dp set param failure in audio HAL.
    
    Change-Id: I478be1f0e34a5da1155e82803d03cd2fe3e963c9

--- hal/audio_hw.c
+++ hal/audio_hw.c
@@ -7296,7 +7296,7 @@ static int adev_set_parameters(struct audio_hw_device *dev, const char *kvpairs)
     audio_extn_qdsp_set_parameters(adev, parms);
 
     status = audio_extn_a2dp_set_parameters(parms, &a2dp_reconfig);
-    if (ret >= 0 && a2dp_reconfig) {
+    if (status >= 0 && a2dp_reconfig) {
         struct audio_usecase *usecase;
         struct listnode *node;
         list_for_each(node, &adev->usecase_list) {
