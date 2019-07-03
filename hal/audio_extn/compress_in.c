codeaurora-unoffical/platform-hardware-qcom-audio

hal: Add API support to stop input stream
Add API support to stop input stream.
Expose in_stop in HAL via QAHW so that clients may call it
when necessary.

Change-Id: I5b1112166453371ab4d79c31fa99fb1f6b3bff3f

hal/audio_extn/compress_in.c
@@ -180,7 +180,7 @@ size_t audio_extn_cin_get_buffer_size(struct stream_in *in)
     return sz;
 }
 
-int audio_extn_cin_start_input_stream(struct stream_in *in)
+int audio_extn_cin_open_input_stream(struct stream_in *in)
 {
     int ret = -EINVAL;
     struct audio_device *adev = in->dev;
@@ -209,12 +209,23 @@ void audio_extn_cin_stop_input_stream(struct stream_in *in)
 
     ALOGV("%s: in %p, cin_data %p", __func__, in, cin_data);
     if (cin_data->compr) {
+        compress_stop(cin_data->compr);
+    }
+}
+
+
+void audio_extn_cin_close_input_stream(struct stream_in *in)
+{
+    cin_private_data_t *cin_data = (cin_private_data_t *) in->cin_extn;
+
+    ALOGV("%s: in %p, cin_data %p", __func__, in, cin_data);
+    if (cin_data->compr) {
         compress_close(cin_data->compr);
         cin_data->compr = NULL;
     }
 }
 
-void audio_extn_cin_close_input_stream(struct stream_in *in)
+void audio_extn_cin_free_input_stream_resources(struct stream_in *in)
 {
     cin_private_data_t *cin_data = (cin_private_data_t *) in->cin_extn;
 
@@ -333,7 +344,7 @@ int audio_extn_cin_configure_input_stream(struct stream_in *in)
     return ret;
 
 err_config:
-    audio_extn_cin_close_input_stream(in);
+    audio_extn_cin_free_input_stream_resources(in);
     return ret;
 }
 #endif /* COMPRESS_INPUT_ENABLED end */

LineageOS/android_hardware_qcom_audio

hal/audio_extn/compress_in.c
@@ -179,7 +179,7 @@ size_t cin_get_buffer_size(struct stream_in *in)
     return sz;
 }
 
-int cin_start_input_stream(struct stream_in *in)
+int cin_open_input_stream(struct stream_in *in)
 {
     int ret = -EINVAL;
     struct audio_device *adev = in->dev;
@@ -208,12 +208,23 @@ void cin_stop_input_stream(struct stream_in *in)
 
     ALOGV("%s: in %p, cin_data %p", __func__, in, cin_data);
     if (cin_data->compr) {
+        compress_stop(cin_data->compr);
+    }
+}
+
+
+void cin_close_input_stream(struct stream_in *in)
+{
+    cin_private_data_t *cin_data = (cin_private_data_t *) in->cin_extn;
+
+    ALOGV("%s: in %p, cin_data %p", __func__, in, cin_data);
+    if (cin_data->compr) {
         compress_close(cin_data->compr);
         cin_data->compr = NULL;
     }
 }
 
-void cin_close_input_stream(struct stream_in *in)
+void cin_free_input_stream_resources(struct stream_in *in)
 {
     cin_private_data_t *cin_data = (cin_private_data_t *) in->cin_extn;
 
@@ -332,6 +343,6 @@ int cin_configure_input_stream(struct stream_in *in)
     return ret;
 
 err_config:
-    cin_close_input_stream(in);
+    cin_free_input_stream_resources(in);
     return ret;
 }