47509fc9c226af030bc05496b742fae2776e5f7c
Kunlei Zhang <kunleiz@codeaurora.org>
2019-08-16 16:41:28 +0800

    hal: Add snd card support for atoll qrd target
    Add snd card support for atoll qrd target.
    
    Change-Id: I313899fc441cf4cd496f436645e4d14a9cdb3176

--- hal/msm8974/platform.c
+++ hal/msm8974/platform.c
@@ -1680,7 +1680,9 @@ static void update_codec_type_and_interface(struct platform_data * my_data,
          !strncmp(snd_card_name, "atoll-wcd937x-snd-card",
                    sizeof("atoll-wcd937x-snd-card")) ||
          !strncmp(snd_card_name, "atoll-idp-snd-card",
-                   sizeof("atoll-idp-snd-card"))) {
+                   sizeof("atoll-idp-snd-card")) ||
+         !strncmp(snd_card_name, "atoll-qrd-snd-card",
+                   sizeof("atoll-qrd-snd-card"))) {
          ALOGI("%s: snd_card_name: %s",__func__,snd_card_name);
          my_data->is_internal_codec = true;
          my_data->is_slimbus_interface = false;
