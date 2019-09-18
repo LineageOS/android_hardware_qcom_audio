d2305b81fbe109bdb93db1b7f1276829ec5ce2e9
Sujin Panicker <spanic@codeaurora.org>
2019-09-18 18:45:03 +0530

    configs: Add speaker-safe mixer paths to all targets
    Add mixer paths for speaker-safe device for sdm845,
    sdm710, sdm660, & msm8998 targets.
    
    CRs-Fixed: 2528564
    Change-Id: Ie6686682f28075eaca6535e17011fb18477af80b

--- configs/sdm660/sdm660.mk
+++ configs/sdm660/sdm660.mk
@@ -112,6 +112,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.pro.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.pro.xml \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml
 
+
 #XML Audio configuration files
 ifeq ($(USE_XML_AUDIO_POLICY_CONF), 1)
 ifneq ($(TARGET_USES_AOSP_FOR_AUDIO), true)
