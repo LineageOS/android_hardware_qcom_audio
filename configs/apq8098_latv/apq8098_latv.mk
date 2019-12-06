b3368cd9dd66bc9373411b1526b097a626e36926
Manisha Agarwal <maniagar@codeaurora.org>
2019-12-06 19:01:07 +0530

    config: rename property violating sepolicy
    rename property to be inline with sepolicy rules
    
    Change-Id: I83cd5d3cd6f5f21bf2701151624bd0f255a1a823

--- configs/apq8098_latv/apq8098_latv.mk
+++ configs/apq8098_latv/apq8098_latv.mk
@@ -275,7 +275,7 @@ vendor.audio.volume.headset.gain.depcal=true
 
 #enable dualmic fluence for voice communication
 PRODUCT_PROPERTY_OVERRIDES += \
-persist.audio.fluence.voicecomm=true
+persist.vendor.audio.fluence.voicecomm=true
 
 # for HIDL related packages
 PRODUCT_PACKAGES += \
