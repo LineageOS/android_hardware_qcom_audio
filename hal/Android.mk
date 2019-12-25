c10dd306078c0634cd72fd3daaecf3e028bca1ff
flowertome <gesangtome@gmail.com>
2019-12-25 20:47:10 +0800

    audio: Fix compilation errors
    'LOCAL_CFLAGS' should be in 'include $(BUILD_SHARED_LIBRARY)',
     otherwise these flags to be passed in will have no effect.
    
    Change-Id: I4d46663b18762919445585cb0a92179ad9825952

--- hal/Android.mk
+++ hal/Android.mk
@@ -370,8 +370,6 @@ LOCAL_MODULE_OWNER := qti
 
 LOCAL_VENDOR_MODULE := true
 
-include $(BUILD_SHARED_LIBRARY)
-
 LOCAL_CFLAGS += -Wno-unused-variable
 LOCAL_CFLAGS += -Wno-sign-compare
 LOCAL_CFLAGS += -Wno-unused-parameter
@@ -382,6 +380,8 @@ LOCAL_CFLAGS += -Wno-shorten-64-to-32
 LOCAL_CFLAGS += -Wno-tautological-compare
 LOCAL_CFLAGS += -Wno-unused-function
 LOCAL_CFLAGS += -Wno-unused-local-typedef
+
+include $(BUILD_SHARED_LIBRARY)
 
 endif
 endif
