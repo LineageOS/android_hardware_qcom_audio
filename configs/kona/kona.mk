f9b8ec9a197936fb91485eb178c234e83f7380c2
Jaideep Sharma <jaidshar@codeaurora.org>
2020-01-28 15:05:49 +0530

    configs: enable audio hidl version 6.0
    Change-Id: I21810971c464a90f4c0ed7678c81f9245dafc8eb

--- configs/kona/kona.mk
+++ configs/kona/kona.mk
@@ -471,6 +471,15 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@5.0 \
     android.hardware.audio.effect@5.0-impl
 
+# enable audio hidl hal 6.0
+PRODUCT_PACKAGES += \
+    android.hardware.audio@6.0 \
+    android.hardware.audio.common@6.0 \
+    android.hardware.audio.common@6.0-util \
+    android.hardware.audio@6.0-impl \
+    android.hardware.audio.effect@6.0 \
+    android.hardware.audio.effect@6.0-impl
+
 # enable sound trigger hidl hal 2.2
 PRODUCT_PACKAGES += \
     android.hardware.soundtrigger@2.2-impl \
