27ba6e9141227c841a8e04d28e2859219d89c3c3
Bruno Martins <bgcngm@gmail.com>
2020-02-01 14:45:33 +0000

    audio_extn: Allow skipping compilation of audio HAL extensions
    In case a device uses a prebuilt HAL it is rather likely
    that the extensions must match it.
    
    Change-Id: I0cb05030c9c7619513810362960a2e2a3306b3aa

--- hal/audio_extn/Android.mk
+++ hal/audio_extn/Android.mk
@@ -1,3 +1,5 @@
+ifneq ($(strip $(TARGET_PROVIDES_AUDIO_EXTNS)),true)
+
 #--------------------------------------------
 #          Build SND_MONITOR LIB
 #--------------------------------------------
@@ -964,3 +966,4 @@ ifneq ($(filter kona lahaina holi,$(TARGET_BOARD_PLATFORM)),)
 LOCAL_SANITIZE := integer_overflow
 endif
 include $(BUILD_SHARED_LIBRARY)
+endif
