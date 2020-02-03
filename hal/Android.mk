96d99dc589a17700754c6da06fb4ac1b0b005fbf
Wei Wang <wvw@google.com>
2020-02-03 12:09:08 -0800

    Support Power HAL AIDL service
    Bug: 148798433
    Test: Build
    Change-Id: Ie14d90bf17faad68beb16a9916e2fab9627b1eb4

--- hal/Android.mk
+++ hal/Android.mk
@@ -377,6 +377,10 @@ ifeq ($(strip $(AUDIO_FEATURE_ENABLED_GCOV)),true)
 endif
 
 LOCAL_SHARED_LIBRARIES += libbase libhidlbase libutils android.hardware.power@1.2 liblog
+
+LOCAL_SHARED_LIBRARIES += android.hardware.power-ndk_platform
+LOCAL_SHARED_LIBRARIES += libbinder_ndk
+
 LOCAL_SRC_FILES += audio_perf.cpp
 
 ifeq ($(strip $(AUDIO_FEATURE_ENABLED_FM_TUNER_EXT)),true)
