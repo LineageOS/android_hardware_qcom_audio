689705b31476b9f69a7ecfe76f48dc0c57ebbc42
Sujin Panicker <spanic@codeaurora.org>
2020-08-13 12:07:32 +0530

    audio-hal: Add property for timecheck timeout duration.
    Timecheck timeout set to 20sec based on the property
    vendor.audio.hal.boot.timeout.ms before system is ready.
    It updates to default timeout of 5sec once the system
    is ready.
    Add this property to PRODUCT_PROPERTY_OVERRIDES
    
    Change-Id: I2987b8af09c40a5fbe292135734d93c597c99c3c

--- configs/msm8937/msm8937.mk
+++ configs/msm8937/msm8937.mk
@@ -226,7 +226,7 @@ vendor.audio.flac.sw.decoder.24bit=true
 
 #timeout crash duration set to 20sec before system is ready.
 #timeout duration updates to default timeout of 5sec once the system is ready.
-PRODUCT_PRODUCT_PROPERTIES += \
+PRODUCT_PROPERTY_OVERRIDES += \
 vendor.audio.hal.boot.timeout.ms=20000
 
 #Set AudioFlinger client heap size
