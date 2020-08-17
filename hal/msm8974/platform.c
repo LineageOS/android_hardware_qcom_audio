ba847df7b506bddd56b65b7e2491dd5dd24aa14c
Pig <pig.priv@gmail.com>
2020-08-17 21:04:55 +0800

    msm8974: platform: Append speaker to echo reference paths
     * Based on observing reverse engineering from davinci stock audio HAL.
    
    Change-Id: I223dddb05185878afab4c79bdcac9db0594b03e1

--- hal/msm8974/platform.c
+++ hal/msm8974/platform.c
@@ -1849,6 +1849,9 @@ void platform_set_echo_reference(struct audio_device *adev, bool enable,
         else if (out_device & AUDIO_DEVICE_OUT_EARPIECE)
             strlcat(ec_ref_mixer_path, " handset",
                     MIXER_PATH_MAX_LENGTH);
+        else if (out_device & AUDIO_DEVICE_OUT_SPEAKER)
+            strlcat(ec_ref_mixer_path, " speaker",
+                    MIXER_PATH_MAX_LENGTH);
         else if (out_device & AUDIO_DEVICE_OUT_WIRED_HEADPHONE)
             strlcat(ec_ref_mixer_path, " headphones",
                     MIXER_PATH_MAX_LENGTH);
