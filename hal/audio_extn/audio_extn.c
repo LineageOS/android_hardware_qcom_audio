a876622facff472ddff43bd053a62d22028f45dc
Zhou Song <zhous@codeaurora.org>
2020-09-15 11:20:57 +0800

    hal: redundant audio_extn callings clearance
    Remove redundant callings to audio_extn.
    
    Change-Id: Ia736d5f95b60d07213d297a61074ef27f4f67d7d

--- hal/audio_extn/audio_extn.c
+++ hal/audio_extn/audio_extn.c
@@ -6345,8 +6345,6 @@ void audio_extn_feature_init()
 void audio_extn_set_parameters(struct audio_device *adev,
                                struct str_parms *parms)
 {
-   bool a2dp_reconfig = false;
-
    audio_extn_set_aanc_noise_level(adev, parms);
    audio_extn_set_anc_parameters(adev, parms);
    audio_extn_set_fluence_parameters(adev, parms);
@@ -6355,9 +6353,7 @@ void audio_extn_set_parameters(struct audio_device *adev,
    audio_extn_sound_trigger_set_parameters(adev, parms);
    audio_extn_listen_set_parameters(adev, parms);
    audio_extn_ssr_set_parameters(adev, parms);
-   audio_extn_hfp_set_parameters(adev, parms);
    audio_extn_dts_eagle_set_parameters(adev, parms);
-   audio_extn_a2dp_set_parameters(parms, &a2dp_reconfig);
    audio_extn_ddp_set_parameters(adev, parms);
    audio_extn_ds2_set_parameters(adev, parms);
    audio_extn_customstereo_set_parameters(adev, parms);
