c442c545ade7b87c09c6c5d8fa8177d57ce6799e
Pig <pig.priv@gmail.com>
2020-09-19 08:57:38 +0800

    audio: Drop LOCAL_COPY_HEADERS usage
    LOCAL_COPY_HEADERS is deprecated, so remove all its usages
    to avoid build warnings.
    
    Change-Id: I4c52508e9109c5f448653fd5613a8991d28bbb2e

---
+++ qahw/Android.mk
@@ -0,0 +1,37 @@
+ifneq ($(AUDIO_USE_STUB_HAL), true)
+ifeq ($(strip $(BOARD_SUPPORTS_QAHW)),true)
+
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := libqahwwrapper
+LOCAL_MODULE_TAGS := optional
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/inc
+
+LOCAL_SRC_FILES := \
+    src/qahw.c \
+    src/qahw_effect.c
+
+LOCAL_SHARED_LIBRARIES := \
+    liblog \
+    libcutils \
+    libhardware \
+    libdl
+
+LOCAL_CFLAGS += -Wall -Werror
+
+LOCAL_PRELINK_MODULE    := false
+LOCAL_VENDOR_MODULE     := true
+
+include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := libqahw_headers
+LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)/inc
+LOCAL_PROPRIETARY_MODULE := true
+
+include $(BUILD_HEADER_LIBRARY)
+endif
+endif
