532ded7c1fb1eab2f608a41c87f73602862dc44f
Pig <pig.priv@gmail.com>
2020-09-19 08:57:38 +0800

    audio: Remove LOCAL_COPY_HEADERS usage
    Change-Id: I4c52508e9109c5f448653fd5613a8991d28bbb2e

--- qahw/Android.mk
+++ qahw/Android.mk
@@ -26,11 +26,7 @@ LOCAL_SHARED_LIBRARIES := \
     libdl
 
 LOCAL_CFLAGS += -Wall -Werror
-
-LOCAL_COPY_HEADERS_TO   := mm-audio/qahw/inc
-LOCAL_COPY_HEADERS      := inc/qahw.h
-LOCAL_COPY_HEADERS      += inc/qahw_effect_api.h
-
+LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)
 LOCAL_PROPRIETARY_MODULE := true
 LOCAL_VENDOR_MODULE     := true
 
--- qahw_api/Android.mk
+++ qahw_api/Android.mk
@@ -29,18 +29,7 @@ LOCAL_SHARED_LIBRARIES := \
     libqahwwrapper
 
 LOCAL_CFLAGS += -Wall -Werror
-
-LOCAL_COPY_HEADERS_TO   := mm-audio/qahw_api/inc
-LOCAL_COPY_HEADERS      := inc/qahw_defs.h
-LOCAL_COPY_HEADERS      += inc/qahw_api.h
-LOCAL_COPY_HEADERS      += inc/qahw_effect_audiosphere.h
-LOCAL_COPY_HEADERS      += inc/qahw_effect_bassboost.h
-LOCAL_COPY_HEADERS      += inc/qahw_effect_environmentalreverb.h
-LOCAL_COPY_HEADERS      += inc/qahw_effect_equalizer.h
-LOCAL_COPY_HEADERS      += inc/qahw_effect_presetreverb.h
-LOCAL_COPY_HEADERS      += inc/qahw_effect_virtualizer.h
-LOCAL_COPY_HEADERS      += inc/qahw_effect_visualizer.h
-
+LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)
 LOCAL_VENDOR_MODULE     := true
 
 ifneq ($(filter kona lahaina holi,$(TARGET_BOARD_PLATFORM)),)
