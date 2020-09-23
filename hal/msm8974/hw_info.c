f8cf3fe77a93bdb7bd0de4d5894fc68bfc0db04f
Meng Wang <quic_mengw@quicinc.com>
2020-09-23 16:34:36 +0800

    hal: add xrfusion-ult snd card
    Add xrfusion-ult sound card to the platform.
    
    Change-Id: Ie351fba5abc0bb0574fdde6c74f312b939f3df81

--- hal/msm8974/hw_info.c
+++ hal/msm8974/hw_info.c
@@ -1,6 +1,8 @@
 /*
  * Copyright (c) 2013-2020, The Linux Foundation. All rights reserved.
  *
+ * Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
  * met:
@@ -558,6 +560,14 @@ static void update_hardware_info_kona(
                  sizeof("kona-qrd-snd-card"))) {
         strlcpy(hw_info->name, "kona", sizeof(hw_info->name));
         hw_info->is_stereo_spkr = false;
+    } else if (!strncmp(snd_card_name, "kona-xrfusion-ult-snd-card",
+                 sizeof("kona-xrfusion-ult-snd-card"))) {
+        strlcpy(hw_info->name, "kona", sizeof(hw_info->name));
+        hw_info->is_stereo_spkr = true;
+    } else if (!strncmp(snd_card_name, "kona-xrfusionult-snd-card",
+                 sizeof("kona-xrfusionult-snd-card"))) {
+        strlcpy(hw_info->name, "kona", sizeof(hw_info->name));
+        hw_info->is_stereo_spkr = true;
     } else if (!strncmp(snd_card_name, "lito-qrd-snd-card",
                  sizeof("lito-qrd-snd-card"))) {
         strlcpy(hw_info->name, "lito", sizeof(hw_info->name));
