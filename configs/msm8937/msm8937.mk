2ea62268d38483c89b3c9ec56506c8ef5723765d
Sujin Panicker <spanic@codeaurora.org>
2020-10-07 11:39:03 +0530

    config: Add legacy specific default/common audio policy conf file
    Legacy SPs(8953 & 8937) does not support split A2DP.
    BT registers with AOSP A2DP HAL rather than with
    vendor BT HAL as the common audio policy conf has only
    support for split A2dp and a2dp module(for BT input).
    Changes is to support vendor BT HAL and remove split A2DP
    by localizing audio policy conf files for these targets
    rather than using common audio policy conf file. Same
    applies for USB Hal as well.
    
    Remove unsupported mixport profiles from policy conf
    for these SPs.
    
    Change-Id: Iba83b88480e998fae0c246b5f176ec27d1834578

--- configs/msm8937/msm8937.mk
+++ configs/msm8937/msm8937.mk
@@ -109,7 +109,7 @@ PRODUCT_COPY_FILES += \
     $(TOPDIR)vendor/qcom/opensource/audio-hal/primary-hal/configs/msm8937/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/audio_policy_configuration.xml
 endif
 PRODUCT_COPY_FILES += \
-    $(TOPDIR)vendor/qcom/opensource/audio-hal/primary-hal/configs/common/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
+    $(TOPDIR)vendor/qcom/opensource/audio-hal/primary-hal/configs/msm8937/audio_policy_configuration_common.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
     $(TOPDIR)vendor/qcom/opensource/audio-hal/primary-hal/configs/common/bluetooth_qti_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_audio_policy_configuration.xml \
     $(TOPDIR)frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
     $(TOPDIR)frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
