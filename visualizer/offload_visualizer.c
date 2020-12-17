67ddab604b833889c45c56ba9518a861b34b996e
Patrick Lai <plai@codeaurora.org>
2020-12-16 22:08:39 -0800

    audio-hal: visualizer: fix wrong specifier in print statements
    read_status is defined to be ssize_t aka long. Existing specifier
    is meant for size_t type. Update specifier in the print statement
    to reflect the right type.
    
    Change-Id: Ida39a3ee5e6b4b0d3255bfef95601890afd80709

--- visualizer/offload_visualizer.c
+++ visualizer/offload_visualizer.c
@@ -424,7 +424,7 @@ void *capture_thread_loop(void *arg)
         pthread_mutex_lock(&lock);
 
         if (read_status > 0) {
-            ALOGD("%s: pal_stream_read success no_of_bytes_read = %zu",
+            ALOGD("%s: pal_stream_read success no_of_bytes_read = %ld",
                     __func__, read_status );
 
             struct listnode *out_node;
@@ -444,7 +444,7 @@ void *capture_thread_loop(void *arg)
                 }
             }
         } else {
-            ALOGW("%s: pal_stream_read failed with read status %zu",
+            ALOGW("%s: pal_stream_read failed with read status %ld",
                 __func__, read_status);
         }
     }
