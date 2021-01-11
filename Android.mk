5f0b8e91253369075843d6ce3c6635b5cc4191e6
Phani Kumar Uppalapati <phaniu@codeaurora.org>
2021-01-10 23:59:50 -0800

    ahal: Enable post proc compilation
    Change-Id: I3ef8e2bad4e30569bb33f8451e64943445a34313

--- Android.mk
+++ Android.mk
@@ -16,7 +16,7 @@ endif
 include $(MY_LOCAL_PATH)/voice_processing/Android.mk
 #include $(MY_LOCAL_PATH)/mm-audio/Android.mk
 include $(MY_LOCAL_PATH)/visualizer/Android.mk
-#include $(MY_LOCAL_PATH)/post_proc/Android.mk
+include $(MY_LOCAL_PATH)/post_proc/Android.mk
 include $(MY_LOCAL_PATH)/qahw/Android.mk
 include $(MY_LOCAL_PATH)/qahw_api/Android.mk
 endif
