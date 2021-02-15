703612b8c5b807c7e5db7808d6fc945d2c79dca4
Jeongik Cha <jeongik@google.com>
2021-02-15 17:48:31 +0900

    Specify version for aidl_interface explicitly
    Bug: 150578172
    Test: m
    Change-Id: I9714cd4416fe0ce23e57cd7db162bfb45b0f84d5

--- hal/Android.mk
+++ hal/Android.mk
@@ -368,7 +368,7 @@ endif
 
 LOCAL_SHARED_LIBRARIES += libbase libhidlbase libutils android.hardware.power@1.2 liblog
 
-LOCAL_SHARED_LIBRARIES += android.hardware.power-ndk_platform
+LOCAL_SHARED_LIBRARIES += android.hardware.power-V1-ndk_platform
 LOCAL_SHARED_LIBRARIES += libbinder_ndk
 
 LOCAL_SRC_FILES += audio_perf.cpp
