fd1a63af8039fbf0c9198cfe1d7010ee00ae6e9f
Shiv Maliyappanahalli <smaliyap@codeaurora.org>
2021-03-18 21:04:24 -0700

    add ACD models to product packages
    Add ACD sound models to product packages.
    
    Change-Id: Ie33924a33a1ee24ed833326468d3638a0d8d6d9d

--- configs/lahaina/lahaina.mk
+++ configs/lahaina/lahaina.mk
@@ -154,6 +154,9 @@ PRODUCT_PACKAGES += IDP_workspaceFileXml.qwsp
 PRODUCT_PACKAGES += QRD_acdb_cal.acdb
 PRODUCT_PACKAGES += QRD_workspaceFileXml.qwsp
 PRODUCT_PACKAGES += libfmpal
+PRODUCT_PACKAGES += event.eai
+PRODUCT_PACKAGES += music.eai
+PRODUCT_PACKAGES += speech.eai
 
 ifneq ($(strip $(TARGET_USES_RRO)), true)
 #Audio Specific device overlays
