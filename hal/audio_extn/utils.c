1253c51c02430dab413c054454ede3d788d05db4
Bruno Martins <bgcngm@gmail.com>
2021-03-28 18:28:24 +0100

    [SQUASH][DNM] Merge tag 'LA.UM.9.3.r1-01500-sdm845.0' of https://source.codeaurora.org/quic/la/platform/hardware/qcom/audio into lineage-18.1-caf-sdm845
        "LA.UM.9.3.r1-01500-sdm845.0"
    
        * tag 'LA.UM.9.3.r1-01500-sdm845.0' of https://source.codeaurora.org/quic/la/platform/hardware/qcom/audio:
          configs : add usb_surround_sound input profile for usb capture
          audio-hal: fix a2dp mute issue
          hal: sampling rate mismatch during a2dp+spk combo usecase.
          hal: route use cases properly to SCO device when it arrives
          hal: don't mute stream with combo device during a2dpsuspended
          hal: switch to current active device when a2dp suspended
          AHAL: raise the priority for configuration of voip usecase
          hal: re-route VOIP TX path for enabling correct VOIP EC port
          hal: deadlock with pause and SSR for compressed offload
          hal: notify input/output streams of snd card status upon request
          hal: mute stream and switch to speaker during a2dp suspend
          hal: fix trailing sound heard during a2dp codec switch
          AHAL: setting 4 channel mapping info for proxy device
          HAL: Modified condition to avoid audio Tx path for legacy targets
          configs: Remove reset value for ADC MUX
          hal: re-enable voice call if disable during device switch
    
    Change-Id: I9c3e22c02c66d92efad414f8cdd07e9287151b21

--- hal/audio_extn/utils.c
+++ hal/audio_extn/utils.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014-2020, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2014-2021, The Linux Foundation. All rights reserved.
  * Not a Contribution.
  *
  * Copyright (C) 2014 The Android Open Source Project
@@ -1159,9 +1159,6 @@ int audio_extn_utils_get_app_sample_rate_for_device(
 
         if (usecase->id == USECASE_AUDIO_PLAYBACK_VOIP) {
             usecase->stream.out->app_type_cfg.sample_rate = usecase->stream.out->sample_rate;
-        } else if (compare_device_type(&usecase->stream.out->device_list,
-                                       AUDIO_DEVICE_OUT_SPEAKER)) {
-            usecase->stream.out->app_type_cfg.sample_rate = DEFAULT_OUTPUT_SAMPLING_RATE;
         } else if ((snd_device == SND_DEVICE_OUT_HDMI ||
                     snd_device == SND_DEVICE_OUT_USB_HEADSET ||
                     snd_device == SND_DEVICE_OUT_DISPLAY_PORT) &&
@@ -1180,9 +1177,7 @@ int audio_extn_utils_get_app_sample_rate_for_device(
             (usecase->stream.out->sample_rate < OUTPUT_SAMPLING_RATE_44100)) {
             /* Reset to default if no native stream is active*/
             usecase->stream.out->app_type_cfg.sample_rate = DEFAULT_OUTPUT_SAMPLING_RATE;
-        } else if (usecase->out_snd_device == SND_DEVICE_OUT_BT_A2DP ||
-                   usecase->out_snd_device == SND_DEVICE_OUT_SPEAKER_AND_BT_A2DP ||
-                   usecase->out_snd_device == SND_DEVICE_OUT_SPEAKER_SAFE_AND_BT_A2DP) {
+        } else if (snd_device == SND_DEVICE_OUT_BT_A2DP) {
                  /*
                   * For a2dp playback get encoder sampling rate and set copp sampling rate,
                   * for bit width use the stream param only.
@@ -1190,6 +1185,9 @@ int audio_extn_utils_get_app_sample_rate_for_device(
                    audio_extn_a2dp_get_enc_sample_rate(&usecase->stream.out->app_type_cfg.sample_rate);
                    ALOGI("%s using %d sample rate rate for A2DP CoPP",
                         __func__, usecase->stream.out->app_type_cfg.sample_rate);
+        } else if (compare_device_type(&usecase->stream.out->device_list,
+                                       AUDIO_DEVICE_OUT_SPEAKER)) {
+            usecase->stream.out->app_type_cfg.sample_rate = DEFAULT_OUTPUT_SAMPLING_RATE;
         }
         audio_extn_btsco_get_sample_rate(snd_device, &usecase->stream.out->app_type_cfg.sample_rate);
         sample_rate = usecase->stream.out->app_type_cfg.sample_rate;
