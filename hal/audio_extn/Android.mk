63fe95280d4e61601e5d7c4562c442ab4f5d6921
Aditya Bavanari <abavanar@codeaurora.org>
2021-08-18 16:47:11 +0530

    hal: battery_listener: Update health interface version to 2.1
    Add changes to make battery listener invoke health interface
    version 2.1 instead of the current 2.0 version
    which is deprecated now.
    
    Change-Id: Icbcb81128881a590ded25f9aaa46c51889f69319

--- hal/audio_extn/Android.mk
+++ hal/audio_extn/Android.mk
@@ -97,6 +97,7 @@ LOCAL_CFLAGS := \
 LOCAL_SHARED_LIBRARIES := \
     android.hardware.health@1.0 \
     android.hardware.health@2.0 \
+    android.hardware.health@2.1 \
     android.hardware.power@1.2 \
     libaudioutils \
     libbase \
