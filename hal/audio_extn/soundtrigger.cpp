31c71645ba149f72374dc7fc4540d9919c6efc6b
Ankit Mishra <quic_ankmis@quicinc.com>
2022-02-04 12:06:14 +0530

    HAL: Add NULL checks for KW issues
    Add null check for st_dev and st session list.
    Add null check after memory allocation for pal_volume.
    
    Change-Id: I3cca48fbf37889544acf3793304b457eee4e7dc1

--- hal/audio_extn/soundtrigger.cpp
+++ hal/audio_extn/soundtrigger.cpp
@@ -25,6 +25,38 @@
  * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
  * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
+ * Changes from Qualcomm Innovation Center are provided under the following license:
+ * Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted (subject to the limitations in the
+ * disclaimer below) provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *
+ *     * Redistributions in binary form must reproduce the above
+ *       copyright notice, this list of conditions and the following
+ *       disclaimer in the documentation and/or other materials provided
+ *       with the distribution.
+ *
+ *     * Neither the name of Qualcomm Innovation Center, Inc. nor the names of its
+ *       contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE
+ * GRANTED BY THIS LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT
+ * HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+ * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+ * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 #define LOG_TAG "AHAL: soundtrigger"
 /* #define LOG_NDEBUG 0 */
@@ -427,11 +459,13 @@ void audio_extn_sound_trigger_deinit(std::shared_ptr<AudioDevice> adev)
     struct listnode *node, *temp;
 
     AHAL_INFO("Enter");
-    list_for_each_safe(node, temp, &st_dev->st_ses_list) {
-        st_ses_info = node_to_item(node, struct sound_trigger_info, list);
-        if (st_ses_info) {
-            list_remove(&st_ses_info->list);
-            free(st_ses_info);
+    if (st_dev) {
+        list_for_each_safe(node, temp, &st_dev->st_ses_list) {
+            st_ses_info = node_to_item(node, struct sound_trigger_info, list);
+            if (st_ses_info) {
+                list_remove(&st_ses_info->list);
+                free(st_ses_info);
+            }
         }
     }
 
