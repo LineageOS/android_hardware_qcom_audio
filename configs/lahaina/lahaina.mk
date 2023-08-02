87cf41cc2ca85b484b0bc1de9d6c10d15ecc83cc
Hari Krishna Chowdary Yennana <quic_hyennnan@quicinc.com>
2023-08-02 12:31:16 +0530

    configs: Rb3Gen2: Install STT meta test app
    - Install STT meta test app binary file for RB3Gen2 Target.
    
    Change-Id: I64a026b9ca1d9006cfd5480c614cc17a4144b439

--- configs/lahaina/lahaina.mk
+++ configs/lahaina/lahaina.mk
@@ -115,9 +115,13 @@ AUDIO_WRAPPER += libqahwwrapper
 AUDIO_HAL_TEST_APPS := hal_play_test
 AUDIO_HAL_TEST_APPS += hal_rec_test
 
+#STT META Test App
+AUDIO_STT_META := stt_meta_extract
+
 PRODUCT_PACKAGES += $(AUDIO_HARDWARE)
 PRODUCT_PACKAGES += $(AUDIO_WRAPPER)
 PRODUCT_PACKAGES += $(AUDIO_HAL_TEST_APPS)
+PRODUCT_PACKAGES += $(AUDIO_STT_META)
 PRODUCT_PACKAGES += ftm_test_config_lahaina-qrd-snd-card
 PRODUCT_PACKAGES += ftm_test_config_lahaina-hdk-snd-card
 PRODUCT_PACKAGES += ftm_test_config_lahaina-hhg-snd-card
