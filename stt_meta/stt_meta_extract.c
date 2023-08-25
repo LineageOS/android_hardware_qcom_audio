7a7508c2d9c00231d42cb57e252816cfe51c3240
Hari Krishna Chowdary Yennana <quic_hyennnan@quicinc.com>
2023-08-25 12:12:35 +0530

    hal: update copyrights in DOA source track files
    - Add missing BSD markings from stt files.
    
    Change-Id: I0cf318ee4450ceaca4e32f9ec87ee3a612c55822
    (cherry picked from commit ba30519e6bd7581b0c36067c67b97e1f13dd28ed)

--- stt_meta/stt_meta_extract.c
+++ stt_meta/stt_meta_extract.c
@@ -1,6 +1,5 @@
 /*
 * Copyright (c) 2015-2019, The Linux Foundation. All rights reserved.
-* Copyright (c) 2021,2023 Qualcomm Innovation Center, Inc. All rights reserved.
 *
 * Redistribution and use in source and binary forms, with or without
 * modification, are permitted provided that the following conditions are
@@ -26,6 +25,10 @@
 * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
 * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
 * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*
+ * Changes from Qualcomm Innovation Center are provided under the following license:
+ * Copyright (c) 2023 Qualcomm Innovation Center, Inc. All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause-Clear
 */
 
 /* Test app to capture STT data using ctls from kernel */
