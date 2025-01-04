6671c1e01a23bda044fc53dc7fa251381f330ed1
Michael Bestas <mkbestas@lineageos.org>
2025-01-04 08:51:13 +0200

    audio: Remove disabled HW accelerated effects
    Change-Id: Id3b565877c919bdbddf9a5a2591f14e240783ae4

--- post_proc/bundle.h
+++ post_proc/bundle.h
@@ -27,9 +27,6 @@
 /* Retry for delay for mixer open */
 #define RETRY_NUMBER 10
 #define RETRY_US 500000
-#ifdef HW_ACCELERATED_EFFECTS
-#define EFFECT_CMD_HW_ACC 20
-#endif
 #define MIXER_CARD 0
 #define SOUND_CARD 0
 
