d3f471281e32dc8acc6180a865af5e4915bb817f
Michael Bestas <mkbestas@lineageos.org>
2025-01-04 08:15:02 +0200

    audio: Remove unsupported QAHW
    Change-Id: I9a225774b0cabde16c85188cb693187228ecf50c

--- Android.mk
+++ Android.mk
@@ -19,8 +19,6 @@ include $(MY_LOCAL_PATH)/voice_processing/Android.mk
 include $(MY_LOCAL_PATH)/mm-audio/Android.mk
 include $(MY_LOCAL_PATH)/visualizer/Android.mk
 include $(MY_LOCAL_PATH)/post_proc/Android.mk
-include $(MY_LOCAL_PATH)/qahw/Android.mk
-include $(MY_LOCAL_PATH)/qahw_api/Android.mk
 ifeq ($(AUDIO_FEATURE_ENABLED_STT_SUPPORT),true)
 include $(MY_LOCAL_PATH)/stt_meta/Android.mk
 endif
