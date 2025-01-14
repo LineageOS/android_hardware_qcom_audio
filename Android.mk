9ffeb6acc779914c073cc5293717df347bfdfe10
Michael Bestas <mkbestas@lineageos.org>
2025-01-14 23:31:43 +0200

    soundfx: Convert to blueprint
    Change-Id: Icf72d9c2211ae95741344e0051c8de6726f7d44b

--- Android.mk
+++ Android.mk
@@ -3,8 +3,5 @@ ifneq ($(TARGET_BOARD_AUTO),true)
   ifneq ($(filter msm8996 msm8998 sdm845 sdm710,$(TARGET_BOARD_PLATFORM)),)
     MY_LOCAL_PATH := $(call my-dir)
     include $(MY_LOCAL_PATH)/hal/Android.mk
-    include $(MY_LOCAL_PATH)/voice_processing/Android.mk
-    include $(MY_LOCAL_PATH)/visualizer/Android.mk
-    include $(MY_LOCAL_PATH)/post_proc/Android.mk
   endif
 endif
