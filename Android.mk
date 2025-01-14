d3e3742d34b8a1078599ab1b5418a39679917c48
Michael Bestas <mkbestas@lineageos.org>
2025-01-14 23:31:43 +0200

    soundfx: Convert to blueprint & remove unused effects
    Change-Id: Icf72d9c2211ae95741344e0051c8de6726f7d44b

--- Android.mk
+++ Android.mk
@@ -14,9 +14,6 @@ else
 include $(MY_LOCAL_PATH)/hal/Android.mk
 endif
 include $(MY_LOCAL_PATH)/hal/audio_extn/Android.mk
-include $(MY_LOCAL_PATH)/voice_processing/Android.mk
-include $(MY_LOCAL_PATH)/visualizer/Android.mk
-include $(MY_LOCAL_PATH)/post_proc/Android.mk
 endif
 
 endif
