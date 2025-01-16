8992eba638f92643d7e9114a722bf8a6aa5ed57a
Michael Bestas <mkbestas@lineageos.org>
2025-01-16 11:02:48 +0200

    hal: Convert primary HAL to blueprint
    Change-Id: Id369bcedf537addfbb55551874edb888538af13c

--- Android.mk
+++ Android.mk
@@ -2,7 +2,6 @@ ifeq ($(call my-dir),$(call project-path-for,qcom-audio))
 
 MY_LOCAL_PATH := $(call my-dir)
 
-include $(MY_LOCAL_PATH)/hal/Android.mk
 include $(MY_LOCAL_PATH)/hal/audio_extn/Android.mk
 
 endif
