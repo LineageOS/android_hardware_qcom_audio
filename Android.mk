ff2a142eaba6d3beaa3245bec847e05f62c1c86b
Michael Bestas <mkbestas@lineageos.org>
2025-01-16 11:02:48 +0200

    hal: Convert primary HAL to blueprint
    Change-Id: Id369bcedf537addfbb55551874edb888538af13c

--- Android.mk
+++
@@ -1,7 +0,0 @@
-# TODO:  Find a better way to separate build configs for ADP vs non-ADP devices
-ifneq ($(TARGET_BOARD_AUTO),true)
-  ifneq ($(filter msm8996 msm8998 sdm845 sdm710,$(TARGET_BOARD_PLATFORM)),)
-    MY_LOCAL_PATH := $(call my-dir)
-    include $(MY_LOCAL_PATH)/hal/Android.mk
-  endif
-endif
