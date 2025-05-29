152d77cc179e2f825a1696a87084e1f7259bd3d1
Yi Zhang <zhanyi@qti.qualcomm.com>
2025-05-29 16:54:05 +0800

    audio-hal: Add audio configuration for AIO
    add new xml files for AIO BAR device.
    
    Change-Id: Ib0c03605fbf033bf7471e19b28af7c4e2372d6d4
    Signed-off-by: Yi Zhang <zhanyi@qti.qualcomm.com>

--- hal/msm8974/platform.c
+++ hal/msm8974/platform.c
@@ -16,8 +16,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- * Changes from Qualcomm Innovation Center are provided under the following license:
- * Copyright (c) 2023-2024 Qualcomm Innovation Center, Inc. All rights reserved.
+ *Changes from Qualcomm Technologies, Inc. are provided under the following license:
+ *Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
  * SPDX-License-Identifier: BSD-3-Clause-Clear
  */
 
@@ -81,6 +81,7 @@
 #define PLATFORM_INFO_XML_PATH_YUPIK_QRD "audio_platform_info_yupikqrd.xml"
 #define PLATFORM_INFO_XML_PATH_YUPIK_IDP "audio_platform_info_yupikidp.xml"
 #define PLATFORM_INFO_XML_PATH_YUPIK_IDPRB3 "audio_platform_info_yupikidprb3.xml"
+#define PLATFORM_INFO_XML_PATH_YUPIK_IDPAIO "audio_platform_info_yupikidpaio.xml"
 #define PLATFORM_INFO_XML_PATH_SCUBA_IDP "audio_platform_info_scubaidp.xml"
 #define PLATFORM_INFO_XML_PATH_SCUBA_QRD "audio_platform_info_scubaqrd.xml"
 #define PLATFORM_INFO_XML_PATH_SA8295_ADP "audio_platform_info_sa8295.xml"
@@ -1969,6 +1970,8 @@ static void update_codec_type_and_interface(struct platform_data * my_data,
                    sizeof("lahaina-yupikidp-snd-card")) ||
          !strncmp(snd_card_name, "lahaina-yupikidprb3-snd-card",
                    sizeof("lahaina-yupikidprb3-snd-card")) ||
+         !strncmp(snd_card_name, "lahaina-yupikidpaio-snd-card",
+                   sizeof("lahaina-yupikidpaio-snd-card")) ||
          !strncmp(snd_card_name, "lahaina-yupikqrd-snd-card",
                    sizeof("lahaina-yupikqrd-snd-card")) ||
          !strncmp(snd_card_name, "kona-qrd-snd-card",
@@ -3686,6 +3689,10 @@ void *platform_init(struct audio_device *adev)
     } else if (!strncmp(snd_card_name, "lahaina-yupikidprb3-snd-card",
                sizeof("lahaina-yupikidprb3-snd-card"))) {
         platform_info_init(get_xml_file_path(PLATFORM_INFO_XML_PATH_YUPIK_IDPRB3),
+            my_data, PLATFORM);
+    } else if (!strncmp(snd_card_name, "lahaina-yupikidpaio-snd-card",
+               sizeof("lahaina-yupikidpaio-snd-card"))) {
+        platform_info_init(get_xml_file_path(PLATFORM_INFO_XML_PATH_YUPIK_IDPAIO),
             my_data, PLATFORM);
     } else if (!strncmp(snd_card_name, "lahaina-yupikqrd-snd-card",
                sizeof("lahaina-yupikqrd-snd-card"))) {
