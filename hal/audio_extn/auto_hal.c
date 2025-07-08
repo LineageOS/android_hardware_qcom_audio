881a67a6a717a86e7c1feb63f8a45dfe70c1b950
Srinivasa Reddy M N <quic_c_mullan@quicinc.com>
2025-07-08 15:28:01 +0530

    auto-hal: Fix the mute issue
    Fixed the Mute is not happened on "Mute" button pressed from kitchen sink.
    
    Change-Id: I31e10db1492b9c1317a4dc495fcd155404ca2c3e
    Signed-off-by: Srinivasa Reddy M N <quic_c_mullan@quicinc.com>

--- hal/audio_extn/auto_hal.c
+++ hal/audio_extn/auto_hal.c
@@ -27,7 +27,7 @@
  * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
  * Changes from Qualcomm Innovation Center are provided under the following license:
- * Copyright (c) 2023 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2023-2025 Qualcomm Innovation Center, Inc. All rights reserved.
  * SPDX-License-Identifier: BSD-3-Clause-Clear
  */
 #define LOG_TAG "auto_hal_extn"
@@ -54,6 +54,7 @@
 #define MAX_VOLUME 1.995262
 #define DSP_MAX_VOLUME 0x2000
 #define DUCKED_VOLUME 0.0035
+#define MUTE_VOLUME 0.0000
 
 enum {
     AUDIO_DEVICE_DUCKED = 0,
@@ -892,7 +893,7 @@ static void auto_hal_set_mute_duck_state(struct audio_device *adev,
                         ALOGD("%s: Muting BUS device %s", __func__, ptr);
                         out->muted = true;
                         if (out && out->compr)
-                              auto_hal_out_set_compr_volume(out, DUCKED_VOLUME, DUCKED_VOLUME);
+                              auto_hal_out_set_compr_volume(out, MUTE_VOLUME, MUTE_VOLUME);
                         break;
 
                     case AUDIO_DEVICE_UNMUTED:
