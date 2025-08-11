4e29049441bba16926707bcffb09c4e491049a2b
Yumi Yukimura <me.cafebabe@gmail.com>
2025-08-11 18:23:00 +0800

    audio-extn: Set parameter to `void*` for the function passed into pthread_create()
    Change-Id: I272f3796c223e677ea43bec03fce80aa27447a94

--- hal/audio_extn/cirrus_playback.c
+++ hal/audio_extn/cirrus_playback.c
@@ -165,13 +165,13 @@ struct pcm_config pcm_config_cirrus_tx = {
 static struct cirrus_playback_session handle;
 
 #ifdef CIRRUS_FACTORY_CALIBRATION
-static void *audio_extn_cirrus_calibration_thread();
+static void *audio_extn_cirrus_calibration_thread(void*);
 #else
-static void *audio_extn_cirrus_config_thread();
+static void *audio_extn_cirrus_config_thread(void*);
 #endif
 
 #ifdef ENABLE_CIRRUS_DETECTION
-static void *audio_extn_cirrus_failure_detect_thread();
+static void *audio_extn_cirrus_failure_detect_thread(void*);
 #endif
 
 void spkr_prot_init(void *adev, spkr_prot_init_config_t spkr_prot_init_config_val) {
@@ -476,7 +476,7 @@ exit:
     return ret;
 }
 
-static void *audio_extn_cirrus_calibration_thread() {
+static void *audio_extn_cirrus_calibration_thread(void*) {
     struct audio_device *adev = handle.adev_handle;
     struct audio_usecase *uc_info_rx = NULL;
     int ret = 0;
@@ -576,7 +576,7 @@ exit:
 }
 
 #else
-static void *audio_extn_cirrus_config_thread(void) {
+static void *audio_extn_cirrus_config_thread(void*) {
     struct audio_device *adev = handle.adev_handle;
     struct crus_sp_ioctl_header header;
     struct cirrus_cal_result_t result;
@@ -659,7 +659,7 @@ exit:
 #endif
 
 #ifdef ENABLE_CIRRUS_DETECTION
-void *audio_extn_cirrus_failure_detect_thread() {
+void *audio_extn_cirrus_failure_detect_thread(void*) {
     struct audio_device *adev = handle.adev_handle;
     struct crus_sp_ioctl_header header;
     struct mixer_ctl *ctl = NULL;
--- hal/audio_extn/spkr_protection.c
+++ hal/audio_extn/spkr_protection.c
@@ -1133,7 +1133,7 @@ static void spkr_calibrate_wait()
     pthread_mutex_unlock(&handle.cal_wait_cond_mutex);
 }
 
-static void* spkr_calibration_thread()
+static void* spkr_calibration_thread(void*)
 {
     unsigned long sec = 0;
     int t0;
@@ -1906,7 +1906,7 @@ static int get_spkr_prot_v_vali_param(int cal_fd __unused, int *status __unused,
 }
 #endif
 
-static void* spkr_v_vali_thread()
+static void* spkr_v_vali_thread(void*)
 {
     int ret = 0;
     struct audio_device *adev = handle.adev_handle;
